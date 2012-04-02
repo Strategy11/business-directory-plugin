d8804d7d2ae83591e126c42f102fae36e032a178
Jorge A. Torres <jtorresh@gmail.com>
2012-04-02 08:44:31 -0500

    add some API methods to support a basic settings API

--- api.php
+++ api.php
@@ -41,3 +41,19 @@ function wpbdp_get_page_link($name='main') {
 	if ($name == 'add-listing')
 		return add_query_arg('action', 'submitlisting', get_permalink($main_page_id));
 }
+
+/* Settings API */
+function wpbdp_settings_api() {
+	global $wpbdp;
+	return $wpbdp->settings;
+}
+
+function wpbdp_get_option($key) {
+	global $wpbdp;
+	return $wpbdp->settings->get($key);
+}
+
+function wpbdp_set_option($key, $value) {
+	global $wpbdp;
+	return $wpbdp->settings->set($key, $value);
+}
