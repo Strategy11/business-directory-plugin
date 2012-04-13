808945fd0343d0c2318d42b9905661863ecd1095
Jorge A. Torres <jtorresh@gmail.com>
2012-04-12 23:07:37 -0500

    minor admin header template fixes

--- admin/admin-pages.php
+++ admin/admin-pages.php
@@ -3,11 +3,11 @@ function wpbdp_admin_sidebar() {
     return wpbdp_render_page(WPBDP_PATH . 'admin/templates/sidebar.tpl.php');
 }
 
-function wpbdp_admin_header($title_=null, $id=null) {
+function wpbdp_admin_header($title_=null, $id=null, $h2items=array()) {
 	global $title;
 
 	if (!$title_) $title_ = $title;
-    return wpbdp_render_page(WPBDP_PATH . 'admin/templates/header.tpl.php', array('page_title' => $title_, 'page_id' => $id));
+    return wpbdp_render_page(WPBDP_PATH . 'admin/templates/header.tpl.php', array('page_title' => $title_, 'page_id' => $id, 'h2items' => $h2items));
 }
 
 
--- admin/templates/header.tpl.php
+++ admin/templates/header.tpl.php
@@ -1,6 +1,14 @@
 <div class="wrap wpbdp-admin <?php echo isset($page_id) ? 'wpbdp-page-' . $page_id : ''; ?>">
 	<div id="icon-edit-pages" class="icon32"></div>
-		<h2><?php echo isset($page_title) ? $page_title : __("Business Directory Plugin","WPBDM"); ?></h2>
+		<h2>
+			<?php echo isset($page_title) ? $page_title : __("Business Directory Plugin","WPBDM"); ?>
+
+			<?php if ($h2items): ?>
+				<?php foreach ($h2items as $item): ?>
+					<a href="<?php echo $item[1]; ?>" class="add-new-h2"><?php echo $item[0]; ?></a>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</h2>
 		
 		<?php echo $sidebar = wpbdp_admin_sidebar(); ?>
 
