d731431a3ddc5de058aa71114cef8d5e301a3611
Jorge A. Torres <jtorresh@gmail.com>
2012-04-28 17:54:45 -0500

    restrict admin menu to administrators. closes #90.

--- admin/wpbdp-admin.class.php
+++ admin/wpbdp-admin.class.php
@@ -85,10 +85,12 @@ class WPBDP_Admin {
                          'wpbusdirman_uninstall');
 
         // just a little hack
-        global $submenu;
-        $submenu['wpbusdirman.php'][0][0] = _x('Main Menu', 'admin menu', 'WPBDM');
-        $submenu['wpbusdirman.php'][5][2] = admin_url(sprintf('edit.php?post_type=%s&wpbdmfilter=%s', wpbdp()->get_post_type(), 'pendingupgrade'));
-        $submenu['wpbusdirman.php'][6][2] = admin_url(sprintf('edit.php?post_type=%s&wpbdmfilter=%s', wpbdp()->get_post_type(), 'unpaid'));
+        if (current_user_can('activate_plugins')) {
+            global $submenu;
+            $submenu['wpbusdirman.php'][0][0] = _x('Main Menu', 'admin menu', 'WPBDM');
+            $submenu['wpbusdirman.php'][5][2] = admin_url(sprintf('edit.php?post_type=%s&wpbdmfilter=%s', wpbdp()->get_post_type(), 'pendingupgrade'));
+            $submenu['wpbusdirman.php'][6][2] = admin_url(sprintf('edit.php?post_type=%s&wpbdmfilter=%s', wpbdp()->get_post_type(), 'unpaid'));
+        }
     }
 
     function add_listing_metabox() {
