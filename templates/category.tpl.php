8489dc20ca410ef09714d86e7794503e7e73db4a
Jorge A. Torres <jtorresh@gmail.com>
2012-09-18 12:10:20 -0500

    make category view use 'businessdirectory-listings' to match the standards

--- templates/category.tpl.php
+++ templates/category.tpl.php
@@ -6,26 +6,6 @@
 
     <h2 class="category-name"><?php echo esc_attr($category->name); ?></h2>    
 
-    <?php if (!$stickies && !have_posts()): ?>
-        <?php _ex("No listings found in category.", 'templates', "WPBDM"); ?>
-    <?php else: ?>
-        <div class="listings">
-            <?php echo $stickies; ?>
+    <?php echo wpbdp_render('businessdirectory-listings', array('excludebuttons' => true)); ?>
 
-            <?php while (have_posts()): the_post(); ?>
-                <?php echo wpbdp_render_listing(null, 'excerpt'); ?>
-            <?php endwhile; ?>
-
-            <div class="wpbdp-pagination">
-            <?php if (function_exists('wp_pagenavi')) : ?>
-                    <?php wp_pagenavi(); ?>
-            <?php elseif (function_exists('wp_paginate')): ?>
-                    <?php wp_paginate(); ?>
-            <?php else: ?>
-                <span class="prev"><?php next_posts_link(_x('&laquo; Older Entries', 'templates', 'WPBDM')); ?></span>
-                <span class="next"><?php previous_posts_link(_x('Newer Entries &raquo;', 'templates', 'WPBDM')); ?></span>
-            <?php endif; ?>
-            </div>
-        </div>
-    <?php endif; ?>
 </div>
