697db6334130e1f54fbc09cc661d8c872211b14f
Jorge A. Torres <jtorresh@gmail.com>
2013-02-06 18:16:50 -0500

    remove additional deprecated templates
    related to #281

--- deprecated/templates/businessdirectory-single.tpl.php
+++ deprecated/templates/businessdirectory-single.tpl.php
@@ -3,7 +3,7 @@
 <?php the_post(); ?>
 
 <div id="content">
-    <?php // Customize the output of this function using the template "businessdirectory-listing.tpl.php" ?>
+    <?php // Customize the output of this function using the template "businessdirectory-listing.tpl.php"; ?>
     <?php echo wpbdp_render_listing(null, 'single'); ?>
 </div>
 
