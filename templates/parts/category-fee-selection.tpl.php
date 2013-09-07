a3625ba7c91a1b633a30a2fd0b1b914fb77663e5
Jorge A. Torres <j@jorgetorres.co>
2013-09-07 15:50:08 -0500

    Minor style fixes to fee selection table
    Closes #542

--- templates/parts/category-fee-selection.tpl.php
+++ templates/parts/category-fee-selection.tpl.php
@@ -2,17 +2,17 @@
 	<?php if ( isset( $state->categories) && ( count( $state->categories ) > 1 ) ): ?><h4><?php echo sprintf( _x( '"%s" fee options', 'templates', 'WPBDM' ), $category->name ); ?></h4><?php endif; ?>
 	<table class="fee-options">
 		<thead>
-			<th></th>
-			<th><?php echo _x( 'Fee', 'templates', 'WPBDM' ); ?></th>
-			<th><?php echo _x( 'Price', 'templates', 'WPBDM' ); ?></th>					
-			<th><?php echo _x( 'Duration', 'templates', 'WPBDM' ); ?></th>
-			<th><?php echo _x( 'Images Allowed', 'templates', 'WPBDM' ); ?></th>
+			<th class="fee-selection"></th>
+			<th class="fee-label"><?php echo _x( 'Fee', 'templates', 'WPBDM' ); ?></th>
+			<th class="fee-amount"><?php echo _x( 'Price', 'templates', 'WPBDM' ); ?></th>					
+			<th class="fee-duration"><?php echo _x( 'Duration', 'templates', 'WPBDM' ); ?></th>
+			<th class="fee-images"><?php echo _x( 'Images Allowed', 'templates', 'WPBDM' ); ?></th>
 			<?php // do_action( 'wpbdp_fee_selection_extra_headers' ); ?>
 		</thead>
 		<tbody>
 			<?php $i = 0; foreach ( $fees as &$fee ): ?>					
 			<tr class="fee-option fee-id-<?php echo $fee->id; ?>">
-				<td>
+				<td class="fee-selection">
 					<?php $fee_selected = ( !isset( $state->fees[ $category->term_id ] ) && $i == 0 ) || ( isset( $state->fees[ $category->term_id ] ) && $state->fees[ $category->term_id ] == $fee->id ) ? true : false; ?>
 					<input type="radio" id="wpbdp-fees-radio-<?php echo $fee->id; ?>" name="fees[<?php echo $category->term_id; ?>]" value="<?php echo $fee->id; ?>" <?php echo $fee_selected ? 'checked="checked"' : ''; ?> />
 				</td>
