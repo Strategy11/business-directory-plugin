a2be7b2eb353abf396a603f68f733848fae32745
Jorge A. Torres <j@jorgetorres.co>
2014-06-24 23:29:11 -0500

    Fix shortcode detection for resource loading Closes #887.

--- core/utils.php
+++ core/utils.php
@@ -367,7 +367,6 @@ function wpbdp_get_term_name( $id_or_slug, $taxonomy = WPBDP_CATEGORY_TAX, $fiel
 
 function wpbdp_has_shortcode( &$content, $shortcode ) {
     $check = has_shortcode( $content, $shortcode );
-    $check = false;
 
     if ( ! $check ) {
         // Sometimes has_shortcode() fails so we try another approach.
