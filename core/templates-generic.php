9a668bf9601154a62e599bef4130b381f69cbcbe
Jorge A. Torres <j@jorgetorres.co>
2014-07-18 17:46:09 -0500

    Some refactoring
    Related to #842.

---
+++ core/templates-generic.php
@@ -0,0 +1,99 @@
+<?php
+
+function wpbdp_locate_template($template, $allow_override=true, $try_defaults=true) {
+    $template_file = '';
+
+    if (!is_array($template))
+        $template = array($template);
+
+    if ($allow_override) {
+        $search_for = array();
+
+        foreach ($template as $t) {
+            $search_for[] = $t . '.tpl.php';
+            $search_for[] = $t . '.php';
+            $search_for[] = 'single/' . $t . '.tpl.php';
+            $search_for[] = 'single/' . $t . '.php';
+        }
+
+        $template_file = locate_template($search_for);
+    }
+
+    if (!$template_file && $try_defaults) {
+        foreach ($template as $t) {
+            $template_path = WPBDP_TEMPLATES_PATH . '/' . $t . '.tpl.php'; 
+            
+            if (file_exists($template_path)) {
+                $template_file = $template_path;
+                break;
+            }
+        }
+    }
+
+    return $template_file;
+}
+
+function wpbdp_render($template, $vars=array(), $allow_override=true) {
+    $vars = wp_parse_args($vars, array(
+        '__page__' => array(
+            'class' => array(),
+            'content_class' => array(),
+            'before_content' => '')));
+    $template_name = is_array( $template ) ? $template[0] : $template;
+    $vars = apply_filters('wpbdp_template_vars', $vars, $template_name);
+    return apply_filters( "wpbdp_render_{$template_name}", wpbdp_render_page(wpbdp_locate_template($template, $allow_override), $vars, false) );
+}
+
+function wpbdp_render_msg($msg, $type='status') {
+    $html = '';
+    $html .= sprintf('<div class="wpbdp-msg %s">%s</div>', $type, $msg);
+    return $html;
+}
+
+function _wpbdp_template_mode($template) {
+    if ( wpbdp_locate_template(array('businessdirectory-' . $template, 'wpbusdirman-' . $template), true, false) )
+        return 'template';
+    return 'page';
+}
+
+/**
+ * Displays a reCAPTCHA field using the configured settings.
+ * @return string HTML for the reCAPTCHA field.
+ * @since 3.4.2
+ */
+function wpbdp_recaptcha() {
+    if ( ! wpbdp_get_option( 'recaptcha-on' ) )
+        return '';
+
+    $public_key = wpbdp_get_option( 'recaptcha-public-key' );
+º
+    if ( ! $public_key )
+        return '';
+
+    if ( ! function_exists( 'recaptcha_get_html' ) )
+        require_once( WPBDP_PATH . 'vendors/recaptcha/recaptchalib.php' );
+
+    return recaptcha_get_html( $public_key );
+}
+
+/**
+ * Validates reCAPTCHA input.
+ * @return boolean TRUE if validation succeeded, FALSE otherwise.
+ * @since 3.4.2
+ */
+function wpbdp_recaptcha_check_answer( &$error_msg = null ) {
+    if ( ! wpbdp_get_option( 'recaptcha-on' ) )
+        return true;
+
+    $private_key = wpbdp_get_option( 'recaptcha-private-key' );
+
+    if ( ! $private_key )
+        return true;
+
+    if ( ! function_exists( 'recaptcha_check_answer' ) )
+        require_once( WPBDP_PATH . 'vendors/recaptcha/recaptchalib.php' );
+
+    $resp = recaptcha_check_answer( $private_key, $_SERVER['REMOTE_ADDR'], $_POST['recaptcha_challenge_field'], $_POST['recaptcha_response_field'] );
+
+    return $resp->is_valid;
+}
---
+++ core/templates-listings.php
@@ -0,0 +1,209 @@
+<?php
+
+/**
+ * Displays a single listing view taking into account all of the theme overrides.
+ * @param mixed $listing_id listing object or listing id to display.
+ * @param string $view 'single' for single view or 'excerpt' for summary view.
+ * @return string HTML output.
+ */
+function wpbdp_render_listing($listing_id=null, $view='single', $echo=false) {
+    if (is_object($listing_id)) $listing_id = $listing_id->ID;
+
+    global $post;
+    $listings_api = wpbdp_listings_api();
+
+    if ($listing_id)  {
+        $args = array( 'post_type' => WPBDP_POST_TYPE, 'p' => $listing_id );
+
+        if ( !isset( $_GET['preview'] ) )
+            $args['post_status'] = 'publish';
+
+        query_posts( $args );
+
+        if (have_posts()) the_post();
+    }
+
+    if (!$post || $post->post_type != WPBDP_POST_TYPE) {
+        return '';
+    }
+
+    if ($view == 'excerpt')
+        $html = _wpbdp_render_excerpt();
+    else
+        $html = _wpbdp_render_single();
+
+    if ($listing_id)
+        wp_reset_query();
+
+    if ($echo)
+        echo $html;
+
+    return $html;
+}
+
+function _wpbdp_render_single() {
+    global $post;
+
+    $html = '';
+
+    $sticky_status = wpbdp_listings_api()->get_sticky_status($post->ID);
+
+    $html .= sprintf( '<div id="wpbdp-listing-%d" class="wpbdp-listing wpbdp-listing-single %s %s %s" itemscope itemtype="http://schema.org/LocalBusiness">',
+                      $post->ID,
+                      'single',
+                      $sticky_status,
+                      apply_filters( 'wpbdp_listing_view_css', '', $post->ID ) );
+    $html .= apply_filters('wpbdp_listing_view_before', '', $post->ID, 'single');
+    $html .= wpbdp_capture_action('wpbdp_before_single_view', $post->ID);
+
+    $sticky_tag = '';
+    if ($sticky_status == 'sticky')
+        $sticky_tag = sprintf('<div class="stickytag"><img src="%s" alt="%s" border="0" title="%s"></div>',
+                        WPBDP_URL . 'core/images/featuredlisting.png',
+                        _x('Featured Listing', 'templates', 'WPBDM'),
+                        the_title(null, null, false));
+
+    $d = WPBDP_ListingFieldDisplayItem::prepare_set( $post->ID, 'listing' );
+    $listing_fields = implode( '', WPBDP_ListingFieldDisplayItem::walk_set( 'html', $d->fields ) );
+    $social_fields = implode( '', WPBDP_ListingFieldDisplayItem::walk_set( 'html', $d->social ) );
+
+    // images
+    $thumbnail_id = wpbdp_listings_api()->get_thumbnail_id($post->ID);
+    $images = wpbdp_listings_api()->get_images($post->ID);
+    $extra_images = array();
+
+    if ( wpbdp_get_option( 'allow-images' ) ) {
+        foreach ($images as $img) {
+            // create thumbnail of correct size if needed (only in single view to avoid consuming server resources)
+            _wpbdp_resize_image_if_needed( $img->ID );
+
+            if ($img->ID == $thumbnail_id) continue;
+
+            $full_image_data = wp_get_attachment_image_src( $img->ID, 'wpbdp-large', false );
+            $full_image_url = $full_image_data[0];
+
+            $extra_images[] = sprintf('<a href="%s" class="thickbox" data-lightbox="wpbdpgal" target="_blank">%s</a>',
+                                        $full_image_url,
+                                        wp_get_attachment_image( $img->ID, 'wpbdp-thumb', false, array(
+                                            'class' => 'wpbdp-thumbnail size-thumbnail',
+                                            'alt' => the_title(null, null, false),
+                                            'title' => the_title(null, null, false)
+                                        ) ));
+        }
+    }
+
+    $vars = array(
+        'actions' => wpbdp_render('parts/listing-buttons', array('listing_id' => $post->ID, 'view' => 'single'), false),
+        'is_sticky' => $sticky_status == 'sticky',
+        'sticky_tag' => $sticky_tag,
+        'title' => get_the_title(),
+        'main_image' => wpbdp_get_option( 'allow-images' ) ? wpbdp_listing_thumbnail( null, 'link=picture&class=wpbdp-single-thumbnail' ) : '',
+        'listing_fields' => apply_filters('wpbdp_single_listing_fields', $listing_fields, $post->ID),
+        'fields' => $d->fields,
+        'listing_id' => $post->ID,
+        'extra_images' => $extra_images
+    );
+    $vars = apply_filters( 'wpbdp_listing_template_vars', $vars, $post->ID );
+    $vars = apply_filters( 'wpbdp_single_template_vars', $vars, $post->ID );
+
+    $html .= wpbdp_render('businessdirectory-listing', $vars, true);
+
+    $social_fields = apply_filters('wpbdp_single_social_fields', $social_fields, $post->ID);
+    if ($social_fields)
+        $html .= '<div class="social-fields cf">' . $social_fields . '</div>';
+
+    $html .= apply_filters('wpbdp_listing_view_after', '', $post->ID, 'single');
+    $html .= wpbdp_capture_action('wpbdp_after_single_view', $post->ID);
+
+    $show_contact_form = apply_filters('wpbdp_show_contact_form', wpbdp_get_option('show-contact-form'), $post->ID);
+    if ($show_contact_form) {
+        $html .= '<div class="contact-form">';
+        $html .= wpbdp_listing_contact_form();
+        $html .= '</div>';
+    }
+
+    if (wpbdp_get_option('show-comment-form')) {
+        $html .= '<div class="comments">';
+
+        ob_start();
+        comments_template(null, true);
+        $html .= ob_get_contents();
+        ob_end_clean();
+
+        $html .= '</div>';
+    }
+
+    $html .= '</div>';
+
+    return $html;
+}
+
+function _wpbdp_render_excerpt() {
+    global $post;
+    static $counter = 0;
+
+    $sticky_status = wpbdp_listings_api()->get_sticky_status($post->ID);
+
+    $html = '';
+    $html .= sprintf('<div id="wpbdp-listing-%d" class="wpbdp-listing excerpt wpbdp-listing-excerpt %s %s %s cf">',
+                     $post->ID,
+                     $sticky_status,
+                     ($counter & 1) ? 'odd':  'even',
+                     apply_filters( 'wpbdp_excerpt_view_css', '', $post->ID ) );
+    $html .= wpbdp_capture_action('wpbdp_before_excerpt_view', $post->ID);
+
+    $d = WPBDP_ListingFieldDisplayItem::prepare_set( $post->ID, 'excerpt' );
+    $listing_fields = implode( '', WPBDP_ListingFieldDisplayItem::walk_set( 'html', $d->fields ) );
+    $social_fields = implode( '', WPBDP_ListingFieldDisplayItem::walk_set( 'html', $d->social ) );
+
+
+    $vars = array(
+        'is_sticky' => $sticky_status == 'sticky',
+        'thumbnail' => ( wpbdp_get_option( 'allow-images' ) && wpbdp_get_option( 'show-thumbnail' ) ) ? wpbdp_listing_thumbnail( null, 'link=listing&class=wpbdmthumbs wpbdp-excerpt-thumbnail' ) : '',
+        'title' => get_the_title(),
+        'listing_fields' => apply_filters('wpbdp_excerpt_listing_fields', $listing_fields, $post->ID),
+        'fields' => $d->fields,
+        'listing_id' => $post->ID
+    );
+    $vars = apply_filters( 'wpbdp_listing_template_vars', $vars, $post->ID );
+    $vars = apply_filters( 'wpbdp_excerpt_template_vars', $vars, $post->ID );
+
+    $html .= wpbdp_render('businessdirectory-excerpt', $vars, true);
+
+    $social_fields = apply_filters('wpbdp_excerpt_social_fields', $social_fields, $post->ID);
+    if ($social_fields)
+        $html .= '<div class="social-fields cf">' . $social_fields . '</div>';
+
+    $html .= wpbdp_capture_action('wpbdp_after_excerpt_view', $post->ID);
+    $html .= wpbdp_render('parts/listing-buttons', array('listing_id' => $post->ID, 'view' => 'excerpt'), false);
+    $html .= '</div>';
+
+    $counter++;
+
+    return $html;
+}
+
+function wpbdp_latest_listings($n=10, $before='<ul>', $after='</ul>', $before_item='<li>', $after_item = '</li>') {
+    $n = max(intval($n), 0);
+
+    $posts = get_posts(array(
+        'post_type' => WPBDP_POST_TYPE,
+        'post_status' => 'publish',
+        'numberposts' => $n,
+        'orderby' => 'date'
+    ));
+
+    $html = '';
+
+    $html .= $before;
+
+    foreach ($posts as $post) {
+        $html .= $before_item;
+        $html .= sprintf('<a href="%s">%s</a>', get_permalink($post->ID), get_the_title($post->ID));
+        $html .= $after_item;
+    }
+
+    $html .= $after;
+
+    return $html;
+}
