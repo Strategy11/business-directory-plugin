b5630141cf8e344cd3581b9a94c56c7491af159d
Jorge A. Torres <j@jorgetorres.co>
2014-08-14 09:05:42 -0500

    Improve registration url behavior (#979)

--- templates/parts/login-required.tpl.php
+++ templates/parts/login-required.tpl.php
@@ -6,21 +6,23 @@
 <?php wp_login_form(); ?>
 
 <?php
-$registration_url = wpbdp_get_option( 'registration-url', '' );
+$registration_url = trim( wpbdp_get_option( 'registration-url', '' ) );
 
-if ( empty( $registration_url ) && function_exists( 'wp_registration_url' ) ) {
-    $registration_url = wp_registration_url();
-} else if ( empty( $registration_url ) ) {
-    $registration_url = site_url( 'wp-login.php?action=register', 'login' );
+if ( ! $registration_url && get_option( 'users_can_register' ) ) {
+    if ( function_exists( 'wp_registration_url' ) ) {
+        $registration_url = wp_registration_url();
+    } else {
+        $registration_url = site_url( 'wp-login.php?action=register', 'login' );
+    }
 }
 
 $current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$registration_url = add_query_arg( array( 'redirect_to' => urlencode( $current_url ) ), $registration_url );
+$registration_url = $registration_url ? add_query_arg( array( 'redirect_to' => urlencode( $current_url ) ), $registration_url ) : '';
 $lost_password_url = add_query_arg( 'redirect_to', urlencode( $current_url ), wp_lostpassword_url() );
 ?>
 
 <p>
-    <?php if (get_option('users_can_register')): ?>
+    <?php if ( $registration_url ): ?>
     <a href="<?php echo esc_url( $registration_url ); ?>"><?php _ex( 'Not yet registered?', 'templates', 'WPBDM' ); ?></a> | 
     <?php endif; ?>
     <a href="<?php echo esc_url( $lost_password_url ); ?>"><?php _ex( 'Lost your password?', 'templates', 'WPBDM' ); ?></a>
