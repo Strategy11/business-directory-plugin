a413b6453fe429dea2b81ed18b72d4884fdea784
Jorge A. Torres <j@jorgetorres.co>
2014-09-11 09:09:45 -0500

    Add new SEO class to handle SEO hacks

---
+++ core/seo.php
@@ -0,0 +1,39 @@
+<?php
+
+class WPBDP_SEO {
+
+    public static function is_wp_seo_enabled() {
+        return defined( 'WPSEO_VERSION' ) ? true : false;
+    }
+
+    public static function listing_title( $listing_id ) {
+        if ( self::is_wp_seo_enabled() ) {
+            global $wpseo_front;
+
+            $title = $wpseo_front->get_content_title( get_post( $listing_id ) );
+            $title = esc_html( strip_tags( stripslashes( apply_filters( 'wpseo_title', $title ) ) ) );
+
+            return $title;
+        }
+
+        return get_the_title( $listing_id );
+    }
+
+    public static function listing_og_description( $listing_id ) {
+        if ( self::is_wp_seo_enabled() ) {
+            global $wpseo_front;
+            global $post;
+
+            $prev_post = $post;
+            $post = get_post( $listing_id );
+            $desc = $wpseo_front->metadesc( false );
+            $post = $prev_post;
+
+            return $desc;
+        }
+
+        $listing = WPBDP_Listing::get( $listing_id );
+        return $listing->get_field_value( 'excerpt' );
+    }
+
+}
