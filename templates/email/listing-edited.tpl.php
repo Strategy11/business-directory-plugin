c1a3ddf5159dee5de8de8797a1f719839803de44
Jorge A. Torres <j@jorgetorres.co>
2014-10-08 16:24:41 -0500

    Add listing edit notification email

---
+++ templates/email/listing-edited.tpl.php
@@ -0,0 +1,19 @@
+<?php
+    _ex( 'A listing in the directory has been edited recently. Listing details can be found below.', 'emails', 'WPBDM' );
+?>
+
+----
+
+<?php _ex('ID', 'notify email', 'WPBDM'); ?>: <?php echo $listing->get_id(); ?>
+
+
+<?php _ex('Title', 'notify email', 'WPBDM'); ?>: <?php echo $listing->get_title(); ?>
+
+
+<?php _ex('URL', 'notify email', 'WPBDM'); ?>: <?php echo $listing->is_published() ? $listing->get_permalink() : _x( '(not published yet)', 'notify email', 'WPBDM' ); ?>
+
+
+<?php _ex('Categories', 'notify email', 'WPBDM'); ?>: <?php foreach ( $listing->get_categories( 'all' ) as $category ): ?><?php echo $category->name; ?> / <?php endforeach; ?>
+
+
+<?php _ex('Posted By', 'notify email', 'WPBDM'); ?>: <?php echo $listing->get_author_meta( 'user_login' ); ?> (<?php echo $listing->get_author_meta( 'user_email' ); ?>)
