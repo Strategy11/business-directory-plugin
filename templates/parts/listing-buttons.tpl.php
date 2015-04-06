9c9ec7366d4d1ed166c223ddb095ac84e5d27681
Jorge A. Torres <j@jorgetorres.co>
2015-04-06 14:22:01 -0500

    Use links instead of buttons for listing actions (excerpts)

--- templates/parts/listing-buttons.tpl.php
+++ templates/parts/listing-buttons.tpl.php
@@ -13,5 +13,13 @@
      <input type="button" value="<?php echo __('← Back to Directory', 'WPBDM'); ?>" onclick="window.location.href = '<?php echo wpbdp_get_page_link('main'); ?>'" class="wpbdp-hide-on-mobile button back-to-dir" />
      <input type="button" value="←" onclick="window.location.href = '<?php echo wpbdp_get_page_link('main'); ?>'" class="wpbdp-show-on-mobile button back-to-dir" />
     <?php endif; ?>
-<?php elseif ($view == 'excerpt'): ?><?php if (wpbdp_user_can('view', $listing_id)): ?><input type="button" value="<?php _ex('View', 'templates', 'WPBDM'); ?>" class="button view-listing" onclick="window.location.href = '<?php the_permalink(); ?>' " /><?php endif; ?><?php if (wpbdp_user_can('edit', $listing_id)): ?><form action="<?php echo wpbdp_get_page_link('editlisting', $listing_id); ?>" method="POST"><input type="submit" name="" value="<?php _ex('Edit', 'templates', 'WPBDM'); ?>" class="edit-listing" /></form><?php endif; ?><?php if (wpbdp_user_can('delete', $listing_id)): ?><form action="<?php echo wpbdp_get_page_link('deletelisting', $listing_id); ?>" method="POST"><input type="submit" name="" value="<?php _ex('Delete', 'templates', 'WPBDM'); ?>" class="delete-listing" data-confirmation-message="<?php _ex('Are you sure you wish to delete this listing?', 'templates', 'WPBDM'); ?>" /></form><?php endif; ?><?php endif; ?>
+<?php elseif ($view == 'excerpt'): ?>
+<a class="button view-listing" href="<?php the_permalink(); ?>"><?php _ex('View', 'templates', 'WPBDM'); ?></a>
+<a class="button edit-listing" href="<?php the_permalink(); ?>"><?php _ex('Edit', 'templates', 'WPBDM'); ?></a>
+<a class="button delete-listing" href="<?php the_permalink(); ?>"><?php _ex('Delete', 'templates', 'WPBDM'); ?></a>
+
+<!--<?php if (wpbdp_user_can('view', $listing_id)): ?><input type="button" value="<?php _ex('View', 'templates', 'WPBDM'); ?>" class="button view-listing" onclick="window.location.href = '<?php the_permalink(); ?>' " /><?php endif; ?>-->
+<!--<?php if (wpbdp_user_can('edit', $listing_id)): ?><form action="<?php echo wpbdp_get_page_link('editlisting', $listing_id); ?>" method="POST"><input type="submit" name="" value="<?php _ex('Edit', 'templates', 'WPBDM'); ?>" class="edit-listing" /></form><?php endif; ?>-->
+<!--<?php if (wpbdp_user_can('delete', $listing_id)): ?><form action="<?php echo wpbdp_get_page_link('deletelisting', $listing_id); ?>" method="POST"><input type="submit" name="" value="<?php _ex('Delete', 'templates', 'WPBDM'); ?>" class="delete-listing" data-confirmation-message="<?php _ex('Are you sure you wish to delete this listing?', 'templates', 'WPBDM'); ?>" /></form><?php endif; ?>-->
+<?php endif; ?>
 </div>
