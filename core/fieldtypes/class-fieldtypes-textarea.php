e35c91f854c6b6012874e8e0dc0cba407cdfd4c6
Jorge A. Torres <j@jorgetorres.co>
2015-11-26 22:13:44 -0500

    Support WYSIWYG editor for content fields (on the frontend) Closes #1584

--- core/fieldtypes/class-fieldtypes-textarea.php
+++ core/fieldtypes/class-fieldtypes-textarea.php
@@ -17,12 +17,27 @@ class WPBDP_FieldTypes_TextArea extends WPBDP_Form_Field_Type {
             return $wpbdp->formfields->get_field_type( 'textfield' )->render_field_inner( $field, $value, $context, $extra, $field_settings );
         }
 
-        return sprintf('<textarea id="%s" name="%s" class="intextarea textarea %s">%s</textarea>',
-                       'wpbdp-field-' . $field->get_id(),
-                       'listingfields[' . $field->get_id() . ']',
-                       $field->is_required() ? 'required' : '',
-                       $value ? esc_attr( $value ) : '' );
+        $html  = '';
 
+        if ( 'content' == $field->get_association() && $field->data( 'allow_html' ) && $field->data( 'wysiwyg_editor' ) ) {
+            ob_start();
+            wp_editor( $value ? $value: '',
+                       'wpbdp-field-' . $field->get_id(),
+                       array( 'textarea_name' => 'listingfields[' . $field->get_id() . ']',
+                              'drag_drop_upload' => false,
+                              'media_buttons' => false,
+                              'quicktags' => false  ) );
+            $html .= ob_get_contents();
+            ob_end_clean();
+        } else {
+            $html .= sprintf('<textarea id="%s" name="%s" class="intextarea textarea %s">%s</textarea>',
+                             'wpbdp-field-' . $field->get_id(),
+                             'listingfields[' . $field->get_id() . ']',
+                             $field->is_required() ? 'required' : '',
+                             $value ? esc_attr( $value ) : '' );
+        }
+
+        return $html;
     }
 
     public function get_supported_associations() {
@@ -39,6 +54,9 @@ class WPBDP_FieldTypes_TextArea extends WPBDP_Form_Field_Type {
             $settings['allow_shortcodes'][] = _x( 'Allow WordPress shortcodes in this field?', 'form-fields admin', 'WPBDM' );
             $settings['allow_shortcodes'][] = '<input type="checkbox" value="1" name="field[allow_shortcodes]" ' . ( $field && $field->data( 'allow_shortcodes' ) ? ' checked="checked"' : '' ) . ' />';
 
+            $settings['wysiwyg_editor'][] = _x( 'Display a WYSIWYG editor on the frontend?', 'form-fields admin', 'WPBDM' );
+            $settings['wysiwyg_editor'][] = '<input type="checkbox" value="1" name="field[wysiwyg_editor]" ' . ( $field && $field->data( 'wysiwyg_editor' ) ? ' checked="checked"' : '' ) . ' />';
+
             //$desc  = _x( 'Useful for integrating with some plugins.', 'form-fields admin', 'WPBDM' ) . '<br />';
             $desc = _x( '<b>Advanced users only!</b> Unless you\'ve been told to change this, don\'t switch it unless you know what you\'re doing.', 'form-fields admin', 'WPBDM' );
             $settings['allow_filters'][] = _x( 'Apply "the_content" filter before displaying this field?', 'form-fields admin', 'WPBDM' );
@@ -52,6 +70,7 @@ class WPBDP_FieldTypes_TextArea extends WPBDP_Form_Field_Type {
         $field->set_data( 'allow_html', isset( $_POST['field']['allow_html'] ) ? (bool) intval( $_POST['field']['allow_html'] ) : false );
         $field->set_data( 'allow_filters', isset( $_POST['field']['allow_filters'] ) ? (bool) intval( $_POST['field']['allow_filters'] ) : false );
         $field->set_data( 'allow_shortcodes', isset( $_POST['field']['allow_shortcodes'] ) ? (bool) intval( $_POST['field']['allow_shortcodes'] ) : false );
+        $field->set_data( 'wysiwyg_editor', isset( $_POST['field']['wysiwyg_editor'] ) ? (bool) intval( $_POST['field']['wysiwyg_editor'] ) : false );
     }
 
     public function get_field_html_value( &$field, $post_id ) {
@@ -66,14 +85,19 @@ class WPBDP_FieldTypes_TextArea extends WPBDP_Form_Field_Type {
         if ( 'content' == $field->get_association() ) {
             if ( $field->data( 'allow_filters' ) ) {
                 $value = apply_filters( 'the_content', $value );
-            } elseif ( $field->data( 'allow_shortcodes' ) ) {
-                global $post;
-                // Try to protect us from sortcodes messing things for us.
-                $current_post = $post;
-                $value = do_shortcode( nl2br( $value ) );
-                $post = $current_post;
             } else {
-                $value = nl2br( $value );
+                if ( ! $field->data( 'wysiwyg_editor' ) ) {
+                    // Do not modify HTML output from WYSIWYG editor.
+                    $value = nl2br( $value );
+                }
+
+                if ( $field->data( 'allow_shortcodes' ) ) {
+                    global $post;
+                    // Try to protect us from sortcodes messing things for us.
+                    $current_post = $post;
+                    $value = do_shortcode( nl2br( $value ) );
+                    $post = $current_post;
+                }
             }
         } else {
             $value = nl2br( $value );
