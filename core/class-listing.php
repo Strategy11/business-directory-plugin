09e645d2f5e33bafe3882ba9292f00b8e287ca74
Jorge A. Torres <j@jorgetorres.co>
2016-04-13 20:04:35 -0500

    Improve get_sticky_status() in Listing to take plans into account Related to #1886

--- core/class-listing.php
+++ core/class-listing.php
@@ -561,8 +561,21 @@ class WPBDP_Listing {
     /**
      * @since 3.6.9
      */
-    public function get_sticky_status() {
+    public function get_sticky_status( $consider_plans = true ) {
         $sticky_status = get_post_meta( $this->id, '_wpbdp[sticky]', true );
+
+        if ( $sticky_status )
+            return $sticky_status;
+
+        if ( $consider_plans ) {
+            global $wpdb;
+
+            $has_sticky_plan = (bool) $wpdb->get_var( $wpdb->prepare( "SELECT 1 AS x FROM {$wpdb->prefix}wpbdp_listing_fees WHERE listing_id = %d AND sticky = %d", $this->id, 1 ) );
+
+            if ( $has_sticky_plan )
+                $sticky_status = 'sticky';
+        }
+
         return $sticky_status ? $sticky_status : 'normal';
     }
 
