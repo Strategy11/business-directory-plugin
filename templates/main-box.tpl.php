73e7c9b8df7ce6aba12053f2d8df04a87aa1bfaa
Jorge A. Torres <j@jorgetorres.co>
2016-05-01 09:01:59 -0500

    Add some basic styles to main search box (#1843)

--- templates/main-box.tpl.php
+++ templates/main-box.tpl.php
@@ -1,12 +1,16 @@
 <div id="wpbdp-main-box">
 
-<form>
+<div class="main-fields box-row">
+    <form>
     <label for="wpbdp-main-box-keyword-field">Find listings for:</label>
     <input type="text" id="wpbdp-main-box-keyword-field" />
     <input type="submit" />
     <a href="#">Advanced Search</a>
-</form>
+    </form>
+</div>
 
-<?php wpbdp_the_main_links(); ?>
+<div class="box-row with-separator">
+    <?php wpbdp_the_main_links(); ?>
+</div>
 
 </div>
