1e61016de6861d92917cf0112daff59a87fbe3e4
Jorge A. Torres <j@jorgetorres.co>
2016-05-09 15:52:56 -0500

    Update quick search integrations

--- core/helpers/class-search-helper.php
+++ core/helpers/class-search-helper.php
@@ -6,34 +6,159 @@
 class WPBDP__Search_Helper {
 
     public $mode = '';
-    public $hints = array();
-    public $fields = array();
-    public $args = array();
     public $location = '';
+    public $keywords = array();
+    public $fields = array();
+    public $plan = array();
 
-    public $tax_query = array();
+    public $aliases = array();
+    public $query_pieces;
+
+    public $resultset = array();
 
 
-    public function __construct( $args, $mode = 'AND' ) {
-        foreach ( $args as $field_id => $value ) {
-            $f = WPBDP_Form_Field::get( $field_id );
+    public function __construct( $args ) {
+        $defaults = array( 'query' => '',
+                           'location' => '',
+                           'fields' => array(),
+                           'mode' => 'advanced' );
+        $args = wp_parse_args( $args, $defaults );
+        $args = apply_filters( 'wpbdp_search_args', $args );
 
-            if ( ! $f )
-                unset( $this->args[ $field_id ] );
+        $this->mode = $args['mode'];
+        $this->location = $args['location'];
 
-            $this->args[ $field_id ] = wpbdp_get_option( 'quick-search-enable-performance-tricks' ) ? array( $args[ $field_id ] ) : array_map( 'trim', explode( ' ', $args[ $field_id ] ) );
-            $this->fields[ $field_id ] = $f;
+        if ( 'advanced' == $this->mode ) {
+            foreach ( $args['query'] as $field_id => $keywords ) {
+                $f = WPBDP_Form_Field::get( $field_id );
+
+                if ( ! $f )
+                    continue;
+
+                $this->keywords[ $field_id ] = wpbdp_get_option( 'quick-search-enable-performance-tricks' ) ? array( $keywords ) : array_map( 'trim', explode( ' ', $keywords ) );
+                $this->fields[ $field_id ] = $f;
+            }
+        } elseif ( 'quick-search' == $this->mode ) {
+            $this->keywords = wpbdp_get_option( 'quick-search-enable-performance-tricks' ) ? array( $args['query'] ) : array_map( 'trim', explode( ' ', $args['query'] ) );
+
+            foreach ( $args['fields'] as $f ) {
+                if ( is_object( $f ) ) {
+                    $this->fields[ $f->get_id() ] = $f;
+                } else {
+                    if ( $field = WPBDP_Form_Field::get( $f ) )
+                        $this->fields[ $field->get_id() ] = $field;
+                }
+            }
         }
 
-        $this->mode = $mode;
+        do_action_ref_array( 'wpbdp_search_after_init', array( $this ) );
+
     }
 
-    public function set_location( $location ) {
-        $this->location = $location;
+    public function execute() {
+        global $wpdb;
+
+        if ( $this->resultset )
+            return;
+
+        $this->plan = $this->build_plan();
+        $this->plan = apply_filters( 'wpbdp_search_plan', $this->plan, $this );
+
+        $execution = array();
+
+        if ( 'quick-search' == $this->mode ) {
+            foreach ( $this->plan as $keyword => $fields ) {
+                $execution[ $keyword ] = array();
+
+                foreach ( $fields as $field_query ) {
+                    $f = $this->fields[ $field_query['field'] ];
+                    $execution[ $keyword ][] = $f->configure_search( $keyword, $this );
+                }
+            }
+        }
+
+        $query_pieces = array( 'where' => ' ',
+                               'join' => '',
+                               'orderby' => '',
+                               'distinct' => '',
+                               'fields' => "{$wpdb->posts}.ID",
+                               'limits' => '' );
+
+        if ( 'quick-search' == $this->mode ) {
+            $query_pieces['where'] .= '1=1 ';
+
+            foreach ( $execution as $keyword => $execdetails ) {
+                $query_pieces['where'] .= ' AND ( 1=0 ';
+
+                foreach ( $execdetails as $r ) {
+                    foreach ( array_keys( $query_pieces ) as $i ) {
+                        if ( ! isset( $r[ $i ] ) )
+                            continue;
+
+                        if ( 'where' == $i )
+                            $r[ $i ] = ' OR ' . $r[ $i ];
+
+                        $query_pieces[ $i ] .= ' ' . $r[ $i ] . ' ';
+                    }
+                }
+
+                $query_pieces['where'] .= ') ';
+            }
+        }
+
+        $query = sprintf( "SELECT %s %s FROM {$wpdb->posts} %s WHERE ({$wpdb->posts}.post_type = '%s' AND {$wpdb->posts}.post_status = '%s') AND %s GROUP BY {$wpdb->posts}.ID %s %s",
+                          $query_pieces['distinct'],
+                          $query_pieces['fields'],
+                          $query_pieces['join'],
+                          WPBDP_POST_TYPE,
+                          'publish',
+                          $query_pieces['where'],
+                          $query_pieces['orderby'],
+                          $query_pieces['limits'] );
+        wpbdp_debug_e($query);
+        $this->resultset = $wpdb->get_col( $query );
     }
 
-    public function prepare() {
-        do_action_ref_array( 'wpbdp_search_prepare', array( $this ) );
+    public function get_posts() {
+        $this->execute();
+        return $this->resultset;
+    }
+
+    public function join_alias( $table, $reuse = false ) {
+        if ( ! isset( $this->aliases[ $table ] ) )
+            $this->aliases[ $table ] = array();
+
+        $i = count( $this->aliases[ $table ] );
+        $alias = '';
+        $reused = false;
+
+        if ( $reuse && $i > 0 ) {
+            $alias = $this->aliases[ $table ][ $i - 1 ];
+            $reused = true;
+        } else {
+            $alias = $i > 0 ? 't_' . $i : $table;
+        }
+
+        $this->aliases[ $table ][] = $alias;
+
+        return array( $alias, $reused );
+    }
+
+    private function build_plan() {
+        $plan = array();
+
+        if ( 'quick-search' == $this->mode ) {
+            foreach ( $this->keywords as $k ) {
+                $plan[ $k ] = array();
+
+                foreach ( $this->fields as $f ) {
+                    $plan[ $k ][] = array( 'field' => $f->get_id(), 'keyword' => $k );
+                }
+            }
+        } elseif ( 'advanced' == $this->mode ) {
+        }
+
+        return $plan;
     }
 
 }
--- core/views/search.php
+++ core/views/search.php
@@ -6,7 +6,6 @@ class WPBDP__Views__Search extends WPBDP_NView {
         $listings_api = wpbdp_listings_api();
         $quick_search = false;
 
-        // Are we performing an advanced search or a quick search?
         if ( ! empty ( $_POST['q'] ) )
             $quick_search = true;
 
@@ -14,7 +13,7 @@ class WPBDP__Views__Search extends WPBDP_NView {
             $keywords = trim( $_POST['q'] );
             $location = ! empty( $_POST['location'] ) ? $_POST['location'] : '';
 
-            $results = $listings_api->quick_search_2( $keywords, $location );
+            $results = $listings_api->quick_search( $keywords, $location );
 
             wpbdp_debug_e( $results );
         }
