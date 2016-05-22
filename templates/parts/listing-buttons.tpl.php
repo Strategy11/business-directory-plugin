41d54eebac15e861c7914df850520d23564a4090
Jorge A. Torres <j@jorgetorres.co>
2016-05-22 18:51:55 -0500

    Add Edit Listing view

--- templates/parts/listing-buttons.tpl.php
+++ templates/parts/listing-buttons.tpl.php
@@ -1,10 +1,10 @@
 <div class="listing-actions cf">
 <?php if ($view == 'single'): ?>
     <?php if (wpbdp_user_can('edit', $listing_id)): ?>
-    <form action="<?php echo wpbdp_get_page_link('editlisting', $listing_id); ?>" method="POST"><input type="submit" name="" value="<?php _ex('Edit', 'templates', 'WPBDM'); ?>" class="button wpbdp-button edit-listing" /></form>
+    <form action="<?php echo wpbdp_url( 'edit_listing', $listing_id ); ?>" method="post"><input type="submit" name="" value="<?php _ex('Edit', 'templates', 'WPBDM'); ?>" class="button wpbdp-button edit-listing" /></form>
     <?php endif; ?>
     <?php if (wpbdp_user_can('upgrade-to-sticky', $listing_id)): ?>
-    <form action="<?php echo wpbdp_url( 'upgrade_listing', $listing_id); ?>" method="POST"><input type="submit" name="" value="<?php _ex('Upgrade Listing', 'templates', 'WPBDM'); ?>" class="button wpbdp-button upgrade-to-sticky" /></form>
+    <form action="<?php echo wpbdp_url( 'upgrade_listing', $listing_id); ?>" method="post"><input type="submit" name="" value="<?php _ex('Upgrade Listing', 'templates', 'WPBDM'); ?>" class="button wpbdp-button upgrade-to-sticky" /></form>
     <?php endif; ?>
     <?php if (wpbdp_user_can('delete', $listing_id)): ?>
     <form action="<?php echo wpbdp_url( 'delete_listing', $listing_id ); ?>" method="post"><input type="submit" name="" value="<?php _ex('Delete', 'templates', 'WPBDM'); ?>" class="button wpbdp-button delete-listing" data-confirmation-message="<?php _ex('Are you sure you wish to delete this listing?', 'templates', 'WPBDM'); ?>" /></form>
@@ -15,7 +15,7 @@
     <?php endif; ?>
 <?php elseif ($view == 'excerpt'): ?>
 <?php if (wpbdp_user_can('view', $listing_id)): ?><a class="wpbdp-button button view-listing" href="<?php the_permalink(); ?>"><?php _ex('View', 'templates', 'WPBDM'); ?></a><?php endif; ?>
-<?php if (wpbdp_user_can('edit', $listing_id)): ?><a class="wpbdp-button button edit-listing" href="<?php echo wpbdp_get_page_link('editlisting', $listing_id); ?>"><?php _ex('Edit', 'templates', 'WPBDM'); ?></a><?php endif; ?>
+<?php if (wpbdp_user_can('edit', $listing_id)): ?><a class="wpbdp-button button edit-listing" href="<?php echo wpbdp_url( 'edit_listing', $listing_id ); ?>"><?php _ex('Edit', 'templates', 'WPBDM'); ?></a><?php endif; ?>
 <?php if (wpbdp_user_can('delete', $listing_id)): ?><a class="wpbdp-button button delete-listing" href="<?php echo wpbdp_url( 'delete_listing', $listing_id ); ?>"><?php _ex('Delete', 'templates', 'WPBDM'); ?></a><?php endif; ?>
 
 <!--<?php if (wpbdp_user_can('view', $listing_id)): ?><input type="button" value="<?php _ex('View', 'templates', 'WPBDM'); ?>" class="button view-listing" onclick="window.location.href = '<?php the_permalink(); ?>' " /><?php endif; ?>-->
