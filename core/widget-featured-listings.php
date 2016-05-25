61c446c5df9ad000af29f325394fe397b9aea33d
Jorge A. Torres <j@jorgetorres.co>
2016-05-25 14:53:28 -0500

    Use correct DB prefix for a query inside the Featured Listings widget Closes #1996

--- core/widget-featured-listings.php
+++ core/widget-featured-listings.php
@@ -37,7 +37,7 @@ class WPBDP_FeaturedListingsWidget extends WPBDP_Listings_Widget {
         $q = $wpdb->prepare(
             "SELECT DISTINCT {$wpdb->posts}.ID FROM {$wpdb->posts} JOIN {$wpdb->postmeta} pm ON pm.post_id = {$wpdb->posts}.ID
              JOIN {$wpdb->prefix}wpbdp_listing_fees lf ON lf.listing_id = {$wpdb->posts}.ID
-             WHERE {$wpdb->posts}.post_status = %s AND wp_posts.post_type = %s AND ( lf.sticky = 1 OR ( pm.meta_key = %s AND pm.meta_value = %s ) )
+             WHERE {$wpdb->posts}.post_status = %s AND {$wpdb->posts}.post_type = %s AND ( lf.sticky = 1 OR ( pm.meta_key = %s AND pm.meta_value = %s ) )
              ORDER BY " . ( ( isset( $instance['random_order'] ) && $instance['random_order'] ) ? 'RAND' : $wpdb->posts . '.post_date' ) . 
             " LIMIT %d",
             'publish', WPBDP_POST_TYPE, '_wpbdp[sticky]', 'sticky', $instance['number_of_listings'] );
