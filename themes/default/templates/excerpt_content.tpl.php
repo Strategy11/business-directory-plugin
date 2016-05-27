e497581971beaaf1535f01f5a0ccf947510b6261
Jorge A. Torres <j@jorgetorres.co>
2016-05-27 11:38:17 -0500

    Fix incorrect check in default theme (#2031)

--- themes/default/templates/excerpt_content.tpl.php
+++ themes/default/templates/excerpt_content.tpl.php
@@ -8,16 +8,18 @@
     <?php endif; ?>
 
     <div class="listing-details">
+        <?php if ( $fields->t_address->value || $fields->t_zip->value ): ?>
         <div class="address-info">
-            <?php if ( $fields->t_address ): ?>
+            <?php if ( $fields->t_address->value ): ?>
                 <label><?php _ex( 'Address', 'themes/default', 'WPBDM' ); ?></label>
                 <span class="address"><?php echo $fields->t_address->value; ?></span>
             <?php endif; ?>
 
-            <?php if ( $fields->t_zip ): ?>
+            <?php if ( $fields->t_zip->value ): ?>
                 <br /><span class="zip-code"><?php echo $fields->t_zip->value; ?></span>
             <?php endif; ?>
         </div>
+        <?php endif; ?>
 
         <?php echo $fields->exclude('t_title,t_address,t_zip')->html; ?>
     </div>
