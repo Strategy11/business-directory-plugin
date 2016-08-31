f6c9e6bf421d4c9a9573d48ca2d55356af3917aa
Jorge A. Torres <j@jorgetorres.co>
2016-08-31 08:42:39 -0500

    Fix minor translation issue in 4.1.1 Related to #2257

--- templates/main-box.tpl.php
+++ templates/main-box.tpl.php
@@ -10,7 +10,7 @@
         <div class="box-col search-fields">
             <div class="box-row cols-<?php echo $no_cols; ?>">
                 <div class="box-col main-input">
-                    <input type="text" id="wpbdp-main-box-keyword-field" class="keywords-field" name="kw" placeholder="<?php esc_attr_e( _x( 'Find listings for <keywords>', 'main box', 'WPBDM' ) ); ?>" />
+                    <input type="text" id="wpbdp-main-box-keyword-field" class="keywords-field" name="kw" placeholder="<?php echo esc_attr( _x( 'Find listings for <keywords>', 'main box', 'WPBDM' ) ); ?>" />
                 </div>
                 <?php echo $extra_fields; ?>
             </div>
