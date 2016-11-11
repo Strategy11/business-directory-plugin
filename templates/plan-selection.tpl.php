ea47fe8b17dd9b5cde05dfd3a75a053dc4cb47fa
Jorge A. Torres <j@jorgetorres.co>
2016-11-11 11:03:04 -0500

    Update plan selection templates

--- templates/plan-selection.tpl.php
+++ templates/plan-selection.tpl.php
@@ -10,46 +10,6 @@ $selected = 0;
 ?>
 <div class="wpbdp-plan-selection-list">
     <?php foreach ( $plans as $plan ): ?>
-    <?php
-    $description = $plan->description ? wpautop( wp_kses_post( $plan->description ) ) : '';
-    $description = apply_filters( 'wpbdp_fee_selection_fee_description', $description, $plan );
-    $features = $plan->get_feature_list();
-    ?>
-    <div class="wpbdp-plan wpbdp-clearfix"
-         data-id="<?php echo $plan->id; ?>"
-         data-free-text="<?php echo esc_attr( wpbdp_currency_format( 0.0 ) ); ?>"
-         data-categories="<?php echo implode( ',', (array) $plan->supported_categories ); ?>"
-         data-pricing-model="<?php echo $plan->pricing_model; ?>"
-         data-amount="<?php echo $plan->amount; ?>"
-         data-amount-format="<?php echo esc_attr( wpbdp_currency_format( 'placeholder' ) ); ?>"
-         data-pricing-details="<?php echo esc_attr( json_encode( $plan->pricing_details ) ); ?>" >
-        <div class="wpbdp-plan-duration">
-            <span class="wpbdp-plan-duration-amount"><?php echo $plan->days; ?></span>
-            <span class="wpbdp-plan-duration-period"><?php _ex( 'days', 'plan selection', 'WPBDM' ); ?></span>
-        </div>
-        <div class="wpbdp-plan-details">
-            <div class="wpbdp-plan-label"><?php echo esc_html( apply_filters( 'wpbdp_category_fee_selection_label', $plan->label ) ); ?></div>
-
-            <?php if ( $description ): ?>
-            <div class="wpbdp-plan-description"><?php echo $description; ?></div>
-            <?php endif; ?>
-
-            <ul class="wpbdp-plan-feature-list">
-                <?php foreach ( $plan->get_feature_list() as $feature ): ?>
-                <li><?php echo $feature; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-        <div class="wpbdp-plan-price">
-            <label>
-                <input type="radio"
-                       id="wpbdp-plan-select-radio-<?php echo $plan->id; ?>"
-                       name="<?php echo $field_name; ?>"
-                       value="<?php echo $plan->id; ?>"
-                       <?php checked( absint( $plan->id ), absint( $selected ) ); ?> />
-                <span class="wpbdp-plan-price-amount"><?php echo wpbdp_currency_format( $plan->calculate_amount( $categories ) ); ?></span>
-            </label>
-        </div>
-    </div>
+        <?php echo wpbdp_render( 'plan-selection-plan', compact( 'plan', 'field_name', 'categories', 'selected' ) ); ?>
     <?php endforeach; ?>
 </div>
--- templates/submit-listing-plan-selection.tpl.php
+++ templates/submit-listing-plan-selection.tpl.php
@@ -1,9 +1,2 @@
 <?php echo $category_field->render(); ?>
 <?php echo wpbdp_render( 'plan-selection', array( 'plans' => $plans  ) ); ?>
-
-<?php if ( $allow_recurring && ! wpbdp_get_option( 'listing-renewal-auto-dontask' ) ): ?>
-<div class="make-charges-recurring-option">
-    <b><?php echo _x( 'Would you like to make your fee renew automatically at the end of the period?', 'submit', 'WPBDM' ); ?></b>
-    <input type="checkbox" name="autorenew_fees" value="autorenew" <?php echo wpbdp_getv( $_POST, 'autorenew_fees' ) == 'autorenew' ? 'checked="checked"' : ''; ?> />
-</div>
-<?php endif; ?>
