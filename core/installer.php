94e07e09806536b94a777af96e638a9a398f4630
Willington Vega <wvega@wvega.com>
2016-11-25 12:51:41 -0500

    Add upgrade routine to clean form field options.

--- core/installer.php
+++ core/installer.php
@@ -2,7 +2,7 @@
 
 class WPBDP_Installer {
 
-    const DB_VERSION = '14';
+    const DB_VERSION = '15';
 
     private $installed_version = null;
 
@@ -184,7 +184,7 @@ class WPBDP_Installer {
         if ( get_option( 'wpbdp-manual-upgrade-pending', false ) )
             return;
 
-        $upgrade_routines = array( '2.0', '2.1', '2.2', '2.3', '2.4', '2.5', '3.1', '3.2', '3.4', '3.5', '3.6', '3.7', '3.9', '4.0', '5', '6', '7', '8', '11', '12', '13' );
+        $upgrade_routines = array( '2.0', '2.1', '2.2', '2.3', '2.4', '2.5', '3.1', '3.2', '3.4', '3.5', '3.6', '3.7', '3.9', '4.0', '5', '6', '7', '8', '11', '12', '13', '15' );
 
         foreach ( $upgrade_routines as $v ) {
             if ( version_compare( $this->installed_version, $v ) < 0 ) {
@@ -929,6 +929,94 @@ class WPBDP_Installer {
              $f->save();
     }
 
+    public function upgrade_to_15() {
+        $form_fields = $this->get_list_form_fields();
+
+        foreach ( $form_fields as $form_field ) {
+            $options = array();
+
+            foreach ( $form_field->data( 'options' ) as $key => $value ) {
+                $options[ trim( $key ) ] = trim( $value );
+            }
+
+            $form_field->set_data( 'options', $options );
+            $form_field->save();
+        }
+
+        if ( $this->count_listing_meta_with_new_line_characters( array_keys( $form_fields ) ) ) {
+            $this->request_manual_upgrade( 'upgrade_to_15_fix_form_fields_data' );
+        }
+    }
+
+    private function get_list_form_fields() {
+        $form_fields = array();
+        $find_params = array(
+            'association' => 'meta',
+            'field_type' => array( 'select', 'checkbox', 'radio', 'multiselect' )
+        );
+
+        foreach ( wpbdp_get_form_fields( $find_params ) as $form_field ) {
+            $form_fields[ "_wpbdp[fields][" . $form_field->get_id() . "]" ] = $form_field;
+        }
+
+        return $form_fields;
+    }
+
+    private function count_listing_meta_with_new_line_characters( $meta_keys ) {
+        global $wpdb;
+
+        if ( ! $meta_keys ) {
+            return 0;
+        }
+
+        $sql = "SELECT COUNT(meta_id) FROM {$wpdb->postmeta} WHERE meta_key IN (%s) AND meta_value LIKE '%%\n%%'";
+        $sql = sprintf( $sql, "'" . implode( "', '", $meta_keys ) . "'" );
+
+        return intval( $wpdb->get_var( $sql ) );
+    }
+
+    public function upgrade_to_15_fix_form_fields_data() {
+        $form_fields = $this->get_list_form_fields();
+        $meta_entries = $this->get_listing_meta_with_new_line_characters( array_keys( $form_fields ) );
+
+        foreach ( $meta_entries as $meta_entry ) {
+            $meta_value = maybe_serialize( $meta_entry->meta_value );
+
+            if ( is_string( $meta_value ) ) {
+                $meta_value = explode( "\t", $meta_entry->meta_value );
+            }
+
+            $meta_value = array_map( 'trim', (array) $meta_value );
+
+            $form_fields[ $meta_entry->meta_key ]->store_value( $meta_entry->post_id, $meta_value );
+        }
+
+        $records_left = $this->count_listing_meta_with_new_line_characters( array_keys( $form_fields ) );
+
+        $message = _x( 'Cleaning up stored meta data for Checkbox, Radio and Select fields... (%d records left)', 'installer', 'WPBDM' );
+
+        return array(
+            'ok' => true,
+            'done' => $records_left == 0,
+            'status' => sprintf( $message, $records_left ),
+        );
+    }
+
+    private function get_listing_meta_with_new_line_characters( $meta_keys ) {
+        global $wpdb;
+
+        if ( ! $meta_keys ) {
+            return array();
+        }
+
+        $sql = "SELECT post_id, meta_id, meta_key, meta_value FROM {$wpdb->postmeta} ";
+        $sql.= "WHERE meta_key IN (%s) AND meta_value LIKE '%%\n%%' ";
+        $sql.= "LIMIT 50";
+
+        $sql = sprintf( $sql, "'" . implode( "', '", $meta_keys ) . "'" );
+
+        return $wpdb->get_results( $sql );
+    }
 }
 
 
