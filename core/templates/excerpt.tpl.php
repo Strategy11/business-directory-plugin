16237a1870e815e23e672efd3b0adefa6f966285
Willington Vega <wvega@wvega.com>
2016-11-30 14:08:02 -0500

    Improve responsive behaviour of list of listings in Tabbed Business theme.

--- core/templates/excerpt.tpl.php
+++ core/templates/excerpt.tpl.php
@@ -3,7 +3,7 @@ $__template__ = array( 'blocks' => array( 'before', 'after' ) );
 ?>
 
 <div id="wpbdp-listing-<?php echo $listing->get_id(); ?>"
-     class="wpbdp-listing wpbdp-excerpt excerpt wpbdp-listing-excerpt wpbdp-listing-<?php echo $listing->get_id(); ?> <?php echo $listing->get_sticky_status(); ?> <?php echo apply_filters( 'wpbdp_excerpt_view_css', '', $listing->get_id() ); ?> <?php echo $even_or_odd; ?>">
+     class="wpbdp-listing wpbdp-excerpt excerpt wpbdp-listing-excerpt wpbdp-listing-<?php echo $listing->get_id(); ?> <?php echo $listing->get_sticky_status(); ?> <?php echo apply_filters( 'wpbdp_excerpt_view_css', '', $listing->get_id() ); ?> <?php echo $even_or_odd; ?>" data-breakpoints='{"medium": [560,780], "large": [780,999999]}' data-breakpoints-class-prefix="wpbdp-listing-excerpt">
 
     <?php echo $blocks['before']; ?>
     <?php wpbdp_x_part( 'excerpt_content' ); ?>
