fdbf9d6db9da522ff7a1b320153dc7ad88f1e284
Jorge A. Torres <j@jorgetorres.co>
2016-12-09 15:26:37 -0500

    Make edit listing view refer to main submit listing view

--- core/views/edit_listing.php
+++ core/views/edit_listing.php
@@ -1,7 +1,13 @@
 <?php
 require_once( WPBDP_PATH . 'core/views/submit_listing.php' );
 
+
 class WPBDP__Views__Edit_Listing extends WPBDP__Views__Submit_Listing {
+
+    public function __construct( $args = null ) {
+        parent::__construct();
+        $this->editing = true;
+    }
 
 }
 
