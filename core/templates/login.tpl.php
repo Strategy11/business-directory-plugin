bf4b3b021f8df97c78f5b020fe44aabd266a377c
Jorge A. Torres <j@jorgetorres.co>
2016-12-09 21:24:38 -0500

    Add new login view

---
+++ core/templates/login.tpl.php
@@ -0,0 +1,57 @@
+<?php
+$show_message = isset( $show_message ) ? $show_message : true;
+?>
+
+<div id="wpbdp-login-view">
+
+    <?php if ( $show_message ): ?>
+    <?php echo wpbdp_render_msg(_x("You are not currently logged in. Please login or register first. When registering, you will receive an activation email. Be sure to check your spam if you don't see it in your email within 60 minutes.", 'templates', 'WPBDM')); ?>
+    <?php endif; ?>
+
+    <div class="wpbdp-login-options <?php echo $access_key_enabled ? 'options-2' : 'options-1'; ?>">
+
+    <div id="wpbdp-login-form" class="wpbdp-login-option">
+        <h4><?php _ex( 'Login', 'views:login', 'WPBDM' ); ?></h4>
+        <?php wp_login_form(); ?>
+    </div>
+
+    <?php if ( $access_key_enabled ): ?>
+    <div id="wpbdp-login-access-key-form" class="wpbdp-login-option">
+        <h4><?php _ex( '... or use an Access Key', 'views:login', 'WPBDM' ); ?></h4>
+        <p><?php _ex( 'Please enter your access key and e-mail address.', 'views:login', 'WPBDM' ); ?></p>
+
+        <form action="" method="post">
+            <input type="hidden" name="method" value="access_key" />
+            <p><input type="text" name="email" value="" placeholder="<?php _ex( 'E-Mail Address', 'views:login', 'WPBDM'); ?>" /></p>
+            <p><input type="text" name="access_key" value="" placeholder="<?php _ex( 'Access Key', 'views:login', 'WPBDM' ); ?>" /></p>
+            <p><input type="submit" value="<?php _ex( 'Use Access Key', 'views:login', 'WPBDM' ); ?>" /></p>
+            <p><a href="<?php echo esc_url( $request_access_key_url ); ?>"><?php _ex( 'Request access key?', 'views:login', 'WPBDM' ); ?></a></p>
+        </form>
+    </div>
+    <?php endif; ?>
+
+<?php
+/*$registration_url = trim( wpbdp_get_option( 'registration-url', '' ) );
+
+if ( ! $registration_url && get_option( 'users_can_register' ) ) {
+    if ( function_exists( 'wp_registration_url' ) ) {
+        $registration_url = wp_registration_url();
+    } else {
+        $registration_url = site_url( 'wp-login.php?action=register', 'login' );
+    }
+}
+
+$current_url = ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$registration_url = $registration_url ? add_query_arg( array( 'redirect_to' => urlencode( $current_url ) ), $registration_url ) : '';
+$lost_password_url = add_query_arg( 'redirect_to', urlencode( $current_url ), wp_lostpassword_url() );
+
+<p>
+    <?php if ( $registration_url ): ?>
+    <a href="<?php echo esc_url( $registration_url ); ?>"><?php _ex( 'Not yet registered?', 'templates', 'WPBDM' ); ?></a> | 
+    <?php endif; ?>
+    <a href="<?php echo esc_url( $lost_password_url ); ?>"><?php _ex( 'Lost your password?', 'templates', 'WPBDM' ); ?></a>
+        </p>
+ */
+?>
+
+</div>
---
+++ core/views/login.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @since next-release
+ */
+class WPBDP__Views__Login extends WPBDP__View {
+
+    public function dispatch() {
+        $redirect_to = ! empty( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : wp_get_referer();
+        $redirect_to = remove_query_arg( 'access_key_hash', $redirect_to );
+        $key_access_enabled = wpbdp_get_option( 'enable-key-access' );
+
+        if ( is_user_logged_in() )
+            return $this->_redirect( $redirect_to );
+
+        $login_url = trim( wpbdp_get_option( 'login-url' ) );
+
+        if ( $login_url )
+            return $this->_redirect( add_query_arg( 'redirect_to', $redirect_to, $login_url ) );
+
+        if ( ! empty( $_POST['method'] ) && 'access_key' == $_POST['method'] ) {
+            $email = trim( $_POST['email'] );
+            $key = trim( $_POST['access_key'] );
+
+            if ( WPBDP_Listing::validate_access_key( $key, $email ) ) {
+                $hash = sha1( AUTH_KEY . $key );
+                $redirect_to = add_query_arg( 'access_key_hash', $hash, $redirect_to );
+                $this->_redirect( $redirect_to );
+            } else {
+                $errors = array( _x( 'Please enter a valid e-mail/access key combination.', 'views:login', 'WPBDM' ) );
+            }
+        }
+
+        return $this->_render( 'login', array( 'access_key_enabled' => $key_access_enabled ) );
+    }
+
+}
