8fb8f5969c9a5a47427ffd74c5ec1ac56027ab3f
Jorge A. Torres <j@jorgetorres.co>
2016-12-19 19:27:04 -0500

    Add some utility functions to handle database table edits

--- core/class-utils.php
+++ core/class-utils.php
@@ -21,6 +21,40 @@ class WPBDP__Utils {
         uasort( $array,
                 create_function( '$x, $y', '$x_ = (array) $x; $y_ = (array) $y; return $x_["' . $prop . '"] - $y_["' . $prop . '"];' ) );
     }
+
+    /**
+     * @since next-release
+     */
+    public static function table_exists( $table ) {
+        global $wpdb;
+
+        $res = $wpdb->get_results( $wpdb->prepare( "SHOW TABLES LIKE %s", $table ) );
+        return count( $res ) > 0;
+    }
+
+    /**
+     * @since next-release
+     */
+    public static function table_has_col( $table, $col ) {
+        if ( ! self::table_exists( $table ) )
+            return false;
+
+        global $wpdb;
+        $columns = wp_filter_object_list( $wpdb->get_results( "DESCRIBE {$table}" ), null, null, 'Field' );
+        return in_array( $col, $columns, true );
+    }
+
+    /**
+     * @since next-release
+     */
+    public static function table_drop_col( $table, $col ) {
+        if ( ! self::table_has_col( $table, $col ) )
+            return false;
+
+        global $wpdb;
+        $wpdb->query( "ALTER TABLE {$table} DROP COLUMN {$col}" );
+    }
+
 }
 
 /**
