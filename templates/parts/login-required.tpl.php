b479ee6064702ac6a352625f3a644cc98b178bad
Willington Vega <wvega@wvega.com>
2016-12-30 16:52:31 -0500

    Update appearance of login form.

--- templates/parts/login-required.tpl.php
+++ templates/parts/login-required.tpl.php
@@ -1,11 +1,11 @@
 <?php
 $show_message = isset( $show_message ) ? $show_message : true;
 ?>
+
+<div class="wpbdp-login-form">
 <?php if ( $show_message ): ?>
 <?php echo wpbdp_render_msg(_x("You are not currently logged in. Please login or register first. When registering, you will receive an activation email. Be sure to check your spam if you don't see it in your email within 60 minutes.", 'templates', 'WPBDM')); ?>
 <?php endif; ?>
-
-<p></p>
 
 <h2><?php _ex('Login', 'templates', 'WPBDM'); ?></h2>
 <?php wp_login_form(); ?>
@@ -26,9 +26,10 @@ $registration_url = $registration_url ? add_query_arg( array( 'redirect_to' => u
 $lost_password_url = add_query_arg( 'redirect_to', urlencode( $current_url ), wp_lostpassword_url() );
 ?>
 
-<p>
+<p class="wpbdp-login-form-extra-links">
     <?php if ( $registration_url ): ?>
     <a href="<?php echo esc_url( $registration_url ); ?>"><?php _ex( 'Not yet registered?', 'templates', 'WPBDM' ); ?></a> | 
     <?php endif; ?>
     <a href="<?php echo esc_url( $lost_password_url ); ?>"><?php _ex( 'Lost your password?', 'templates', 'WPBDM' ); ?></a>
 </p>
+</div>
