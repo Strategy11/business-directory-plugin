bfb5f1cfa9cba4fd77519ddfab220762a454369a
Jorge A. Torres <j@jorgetorres.co>
2017-02-14 14:56:21 -0500

    Always display chosen categories on submit listing screen
    Related to #2604.

--- templates/submit-listing-plan-selection-complete.tpl.php
+++ templates/submit-listing-plan-selection-complete.tpl.php
@@ -2,6 +2,16 @@
 $plan = $listing->get_fee_plan()->fee;
 $categories = wp_get_post_terms( $listing->get_id(), WPBDP_CATEGORY_TAX, array( 'fields' => 'ids' ) );
 ?>
+
+<?php if ( $categories ): ?>
+    <ul class="category-list">
+    <?php foreach ( $categories as $cat_id ): ?>
+        <?php $category = get_term( $cat_id, WPBDP_CATEGORY_TAX ); ?>
+        <li><?php echo $category->name; ?></li>
+    <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
 <?php echo wpbdp_render( 'plan-selection-plan', array( 'plan' => $plan, 'categories' => $categories, 'display_only' => true, 'extra' ) ); ?>
 
 <div id="change-plan-link" class="wpbdp-clearfix">
