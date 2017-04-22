b299b30f5d8e5a65ea77d59609a82e847d0ab1e3
Jorge A. Torres <j@jorgetorres.co>
2017-04-21 21:19:29 -0500

    Remove header.tpl.php

--- includes/admin/admin-pages.php
+++ includes/admin/admin-pages.php
@@ -1,19 +1,44 @@
 <?php
 function wpbdp_admin_sidebar() {
-    return wpbdp_render_page(WPBDP_PATH . 'templates/admin/sidebar.tpl.php');
+    return wpbdp_render_page( WPBDP_PATH . 'templates/admin/sidebar.tpl.php' );
 }
 
-function wpbdp_admin_header($title_ = null, $id = null, $h2items =array(), $sidebar = true) {
-	global $title;
+function wpbdp_admin_header( $title_ = null, $id = null, $h2items = array(), $sidebar = true ) {
+    global $title;
 
-	if (!$title_) $title_ = $title;
-    return wpbdp_render_page( WPBDP_PATH . 'templates/admin/header.tpl.php', array( 'page_title' => $title_, 'page_id' => $id, 'h2items' => $h2items, 'sidebar' => $sidebar ) );
+    $title_ = ! $title_ ? $title : $title_;
+
+    $css_id = ! empty( $id ) ? 'wpbdp-admin-page-' . $id : '';
+    $css_class  = '';
+    $css_class .= ! empty( $id ) ? 'wpbdp-page-' . $id : 'wpbdp-page';
+
+    ob_start();
+?>
+<div class="wrap wpbdp-admin <?php echo $css_class; ?>" id="<?php echo $css_id; ?>">
+	<div id="icon-edit-pages" class="icon32"></div>
+		<h1>
+			<?php echo ! empty( $title_ ) ? $title_ : __( 'Business Directory Plugin', 'WPBDM' ); ?>
+
+            <?php if ( ! empty( $h2items ) ): ?>
+				<?php foreach ( $h2items as $item ): ?>
+					<a href="<?php echo $item[1]; ?>" class="add-new-h2"><?php echo $item[0]; ?></a>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</h1>
+		
+		<?php echo $sidebar = $sidebar ? wpbdp_admin_sidebar() : ''; ?>
+
+		<div class="wpbdp-admin-content <?php echo ! empty( $sidebar ) ? 'with-sidebar' : 'without-sidebar'; ?>">
+<?php
+    return ob_get_clean();
 }
 
 
-function wpbdp_admin_footer()
-{
-	$html = '<!--</div>--></div><br class="clear" /></div>';
-	return $html;
+function wpbdp_admin_footer() {
+    ob_start();
+?>
+</div><br class="clear" /></div>
+<?php
+	return ob_get_clean();
 }
 
