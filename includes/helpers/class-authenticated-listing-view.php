ed6b2654f6141a3e055b8dd5935c4d768bc0f4e1
Jorge A. Torres <j@jorgetorres.co>
2017-04-28 18:32:59 -0500

    Fix error in Authenticated View

--- includes/helpers/class-authenticated-listing-view.php
+++ includes/helpers/class-authenticated-listing-view.php
@@ -12,6 +12,9 @@ class WPBDP__Authenticated_Listing_View extends WPBDP__View {
         $user_id = intval( get_current_user_id() );
         $post = get_post( $this->listing->get_id() );
 
+        if ( 'WPBDP__Views__Submit_Listing' == get_class( $this ) && empty( $this->editing ) && ! wpbdp_get_option( 'require-login' ) )
+            return true;
+
         //if ( is_user_logged_in() && ( $this->listing->get_auth ) )
 
         $key_hash = ! empty( $_REQUEST['access_key_hash'] ) ? $_REQUEST['access_key_hash'] : '';
