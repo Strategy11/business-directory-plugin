5b759d4de65bc745b916b14013425c77e1569df0
Willington Vega <wvega@wvega.com>
2017-06-03 13:42:10 -0500

    Prevent PHP Warning in show_tag.php (#2844).

--- includes/views/show_tag.php
+++ includes/views/show_tag.php
@@ -8,14 +8,23 @@ class WPBDP__Views__Show_Tag extends WPBDP__View {
         wpbdp_push_query( $wp_query );
 
         $term = get_queried_object();
-        $term->is_tag = true;
 
-        $html = $this->_render( 'tag',
-                                array( 'title' => $term->name,
-                                       'term' => $term,
-                                       'query' => $wp_query,
-                                       'in_shortcode' => false ),
-                                'page' );
+        // TODO: figure out why get_queried_object would return
+        // something other thana taxonomy term when this method
+        // is executed.
+        if ( is_object( $term ) ) {
+            $term->is_tag = true;
+
+            $html = $this->_render( 'tag',
+                                    array( 'title' => $term->name,
+                                           'term' => $term,
+                                           'query' => $wp_query,
+                                           'in_shortcode' => false ),
+                                    'page' );
+        } else {
+            $html = '';
+        }
+
         wpbdp_pop_query();
 
         return $html;
