79a64282e440bfca896bea9a4d112f6eb4f7e215
Willington Vega <wvega@wvega.com>
2017-06-15 21:07:13 -0500

    Make WPBDP_Listing constructor public to allow testing.

--- includes/class-listing.php
+++ includes/class-listing.php
@@ -10,7 +10,7 @@ class WPBDP_Listing {
     private $id = 0;
     private $new = true;
 
-    private function __construct( $id ) {
+    public function __construct( $id ) {
         $this->id = intval( $id );
     }
 
