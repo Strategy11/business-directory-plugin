ae0b247ed4cd6fd324299617a78e6a2985f2e764
Willington Vega <wvega@wvega.com>
2017-07-20 18:03:20 -0500

    Replace 'Continue with listing submit' with 'Continue'.

--- templates/submit-listing/extra-sections.tpl.php
+++ templates/submit-listing/extra-sections.tpl.php
@@ -3,5 +3,5 @@
 <form id="wpbdp-listing-form-extra" class="wpbdp-listing-form" method="POST" action="" enctype="multipart/form-data">
 	<input type="hidden" name="_state" value="<?php echo $_state->id; ?>" />
     <?php echo $output; ?>
-    <input type="submit" name="continue-with-save" value="<?php _ex( 'Continue with listing submit', 'templates', 'WPBDM' ); ?> " class="submit" />  
+    <input type="submit" name="continue-with-save" value="<?php _ex( 'Continue', 'templates', 'WPBDM' ); ?> " class="submit" />  
 </form>
