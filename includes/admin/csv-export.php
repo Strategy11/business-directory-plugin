9d78db05c6cbc8146b8650e33ea140d86bff704f
Jorge A. Torres <j@jorgetorres.co>
2017-07-31 16:13:50 -0500

    Limit the CSV export AJAX callback to admins only

--- includes/admin/csv-export.php
+++ includes/admin/csv-export.php
@@ -28,6 +28,10 @@ class WPBDP_Admin_CSVExport {
     }
 
     public function ajax_csv_export() {
+        if ( ! current_user_can( 'administrator' ) ) {
+            exit();
+        }
+
         $error = '';
 
         try {
