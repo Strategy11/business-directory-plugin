345048ed95ebcc9dca7210c87d72d2209aa75f5e
Jorge A. Torres <j@jorgetorres.co>
2017-10-25 14:41:50 -0500

    Remove incorrect arg in delete_transient call

--- includes/admin/upgrades/migrations/migration-18_2.php
+++ includes/admin/upgrades/migrations/migration-18_2.php
@@ -3,7 +3,8 @@
 class WPBDP__Migrations__18_2 extends WPBDP__Migration {
 
     public function migrate() {
-        delete_site_transient( 'wpbdp_updates', null );
+        delete_site_transient( 'wpbdp_updates' );
+        delete_transient( 'wpbdp_updates' );
         set_site_transient( 'update_plugins', null );
     }
 
