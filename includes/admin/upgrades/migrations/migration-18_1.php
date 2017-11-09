fd517eca417ae3371300436e2c90ee2f6e5b7e6e
Jorge A. Torres <j@jorgetorres.co>
2017-11-09 08:57:51 -0500

    Prevent errors in migration 18.1

--- includes/admin/upgrades/migrations/migration-18_1.php
+++ includes/admin/upgrades/migrations/migration-18_1.php
@@ -6,7 +6,9 @@ class WPBDP__Migrations__18_1 extends WPBDP__Migration {
         global $wpdb;
 
         // wpbdp_payments: move from 'created_on' column to 'created_at'.
-        $wpdb->query( "UPDATE {$wpdb->prefix}wpbdp_payments SET created_at = FROM_UNIXTIME(UNIX_TIMESTAMP(created_on))" );
+        if ( wpbdp_column_exists( "{$wpdb->prefix}wpbdp_payments", 'created_on' ) ) {
+            $wpdb->query( "UPDATE {$wpdb->prefix}wpbdp_payments SET created_at = FROM_UNIXTIME(UNIX_TIMESTAMP(created_on))" );
+        }
     }
 
 }
