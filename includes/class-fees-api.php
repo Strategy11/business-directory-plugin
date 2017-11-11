a1d901af99f663cfb20e04e16e8e53f370f7c4f0
Jorge A. Torres <j@jorgetorres.co>
2017-11-11 18:12:40 -0500

    Automatically remove extra default "free" plans

--- includes/class-fees-api.php
+++ includes/class-fees-api.php
@@ -51,7 +51,9 @@ class WPBDP_Fees_API {
     private function setup_default_fees() {
         global $wpdb;
 
-        if ( 0 === intval( $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->prefix}wpbdp_plans WHERE tag = %s", 'free' ) ) ) ) {
+        $count = intval( $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM {$wpdb->prefix}wpbdp_plans WHERE tag = %s", 'free' ) ) );
+
+        if ( 0 === $count ) {
             // Add free fee to the DB.
             $wpdb->insert( $wpdb->prefix . 'wpbdp_plans',
                            array( 'id' => 0,
@@ -68,6 +70,15 @@ class WPBDP_Fees_API {
 
             // Update all "free fee" listings to use this.
             $wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->prefix}wpbdp_listings SET fee_id = %d WHERE fee_id = %d OR fee_id IS NULL", $fee_id, 0 ) );
+        } else if ( $count > 1 ) {
+            // Delete "extra" plans. This shouldn't happen, but sometimes it happens :/
+            $fee_ids  = $wpdb->get_col( $wpdb->prepare( "SELECT id FROM {$wpdb->prefix}wpbdp_plans WHERE tag = %s", 'free' ) );
+            $first_id = $fee_ids[0];
+
+            $fee_ids_str = implode( ',', $fee_ids );
+            $wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->prefix}wpbdp_listings SET fee_id = %d WHERE fee_id IN ({$fee_ids_str})", $first_id ) );
+
+            $wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->prefix}wpbdp_plans WHERE tag = %s AND id != %d", 'free', $first_id ) );
         }
     }
 
