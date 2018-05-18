827cfa394ae6ba33376630bd8ed495831b444cea
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-05-18 12:42:45 -0500

    Fix coding standards for Email Class.

--- includes/helpers/class-email.php
+++ includes/helpers/class-email.php
@@ -14,39 +14,37 @@
  */
 class WPBDP_Email {
 
-    public $headers = array();
-    public $subject = '';
+    public $headers      = array();
+    public $subject      = '';
     public $content_type = '';
-    public $from = '';
-    public $reply_to = '';
-    public $to = array();
-    public $cc = array();
-    public $bcc = array();
-
-    public $body = '';
-    public $html = '';
-    public $template = '';
-    public $boundary = '';
+    public $from         = '';
+    public $reply_to     = '';
+    public $to           = array();
+    public $cc           = array();
+    public $bcc          = array();
+    public $body         = '';
+    public $html         = '';
+    public $template     = '';
 
     public function __construct() {
         $this->content_type = wpbdp_get_option( 'listing-email-content-type', 'html' );
     }
 
-    public function wpbdp_email_config ( &$phpmailer ) {
+    public function wpbdp_email_config( &$phpmailer ) {
 
         $this->prepare_html();
 
-        if ( 'plain' == $this->content_type ) {
+        if ( 'plain' === $this->content_type ) {
             $phpmailer->Body = $phpmailer->normalizeBreaks( $phpmailer->html2text( $this->html ) );
             $phpmailer->isHTML( false );
         }
 
-        if ( 'html' == $this->content_type ) {
+        if ( 'html' === $this->content_type ) {
             $phpmailer->Body = $phpmailer->normalizeBreaks( $this->html );
             $phpmailer->isHTML( true );
         }
 
-        if ( 'both' == $this->content_type ) {
+        if ( 'both' === $this->content_type ) {
             $phpmailer->msgHTML( $this->html );
         } else {
             $phpmailer->AltBody = '';
@@ -57,14 +55,14 @@ class WPBDP_Email {
         $text = '';
 
         if ( ! $this->html ) {
-            $text = '<html>';
+            $text  = '<html>';
             $_text = $this->body ? $this->body : '';
-            $_text = str_ireplace(array("<br>", "<br/>", "<br />"), "\n", $_text);
+            $_text = str_ireplace( array( '<br>', '<br/>', '<br />' ), "\n", $_text );
             $text .= nl2br( $_text );
             $text .= '</html>';
         }
 
-        $this->html = $text ? $text : $this->html;
+        $this->html = ! empty( $text ) ? $text : $this->html;
     }
 
     private function get_headers() {
@@ -101,20 +99,25 @@ class WPBDP_Email {
 
     /**
      * Sends the email.
+     *
      * @param string $format allowed values are 'html', 'plain' or 'both'
      * @return boolean true on success, false otherwise
      */
-    public function send($format='both') {
+    public function send( $format = 'both' ) {
         $this->subject = preg_replace( '/[\n\r]/', '', strip_tags( html_entity_decode( $this->subject ) ) );
-        $this->from = preg_replace( '/[\n\r]/', '', $this->from ? $this->from : sprintf( '%s <%s>', get_option( 'blogname' ), get_option( 'admin_email' ) ) );
-        $this->to = preg_replace( '/[\n\r]/', '', $this->to );
+        $this->from    = preg_replace( '/[\n\r]/', '', $this->from ? $this->from : sprintf( '%s <%s>', get_option( 'blogname' ), get_option( 'admin_email' ) ) );
+        $this->to      = preg_replace( '/[\n\r]/', '', $this->to );
 
         if ( ! $this->to ) {
             return false;
         }
         if ( $this->template ) {
-            if ( $html_ = wpbdp_render( $this->template, array( 'subject' => $this->subject,
-                'body' => $this->html ) ) ) {
+            if ( $html_ = wpbdp_render(
+                $this->template, array(
+					'subject' => $this->subject,
+					'body'    => $this->html,
+                )
+            ) ) {
                 $this->html = $html_;
             }
         }
