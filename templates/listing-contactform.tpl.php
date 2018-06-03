93f2964bfb7e75f2ef7133275dced51f3d44b364
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-06-03 15:51:39 -0500

    fix code standards

--- templates/listing-contactform.tpl.php
+++ templates/listing-contactform.tpl.php
@@ -6,47 +6,51 @@
  */
 
 // phpcs:disable
-if ($validation_errors):
+if ( $validation_errors ) :
 ?>
 <div class="wpbdp-msg wpbdp-error">
     <ul>
-        <?php foreach($validation_errors as $error_msg): ?>
+        <?php foreach ( $validation_errors as $error_msg ) : ?>
             <li><?php echo $error_msg; ?></li>
         <?php endforeach; ?>
     </ul>
 </div>
 <?php endif; ?>
 
-<p><label><?php _ex('Listing Title: ', 'templates', 'WPBDM'); ?></label><?php echo get_the_title($listing_id); ?></p>
+<p><label><?php _ex( 'Listing Title: ', 'templates', 'WPBDM' ); ?></label><?php echo get_the_title( $listing_id ); ?></p>
 
 <form method="POST" action="<?php echo wpbdp_url( 'listing_contact', $listing_id ); ?>">
     <?php wp_nonce_field( 'contact-form-' . $listing_id ); ?>
 
-    <?php if ($current_user): ?>
+    <?php if ( $current_user ) : ?>
         <p>
-            <?php echo sprintf(_x('You are currently logged in as %s. Your message will be sent using your logged in contact email.', 'templates', 'WPBDM'),
-                               $current_user->user_login); ?>
+            <?php
+            echo sprintf(
+                _x( 'You are currently logged in as %s. Your message will be sent using your logged in contact email.', 'templates', 'WPBDM' ),
+                $current_user->user_login
+            );
+			?>
         </p>
-    <?php else: ?>
+    <?php else : ?>
             <p>
-               <label><?php _ex('Your Name', 'templates', 'WPBDM'); ?></label>
-               <input type="text" class="intextbox" name="commentauthorname" value="<?php echo esc_attr(wpbdp_getv($_POST, 'commentauthorname', '')); ?>" />
+               <label><?php _ex( 'Your Name', 'templates', 'WPBDM' ); ?></label>
+               <input type="text" class="intextbox" name="commentauthorname" value="<?php echo esc_attr( wpbdp_getv( $_POST, 'commentauthorname', '' ) ); ?>" />
             </p>
             <p>
-                <label><?php _ex("Your Email", 'templates', "WPBDM"); ?></label>
-                <input type="text" class="intextbox" name="commentauthoremail" value="<?php echo esc_attr(wpbdp_getv($_POST, 'commentauthoremail')); ?>" />
+                <label><?php _ex( 'Your Email', 'templates', 'WPBDM' ); ?></label>
+                <input type="text" class="intextbox" name="commentauthoremail" value="<?php echo esc_attr( wpbdp_getv( $_POST, 'commentauthoremail' ) ); ?>" />
             </p>
     <?php endif; ?>
 
-    <p><label><?php _ex("Message", 'templates', "WPBDM"); ?></label>
-       <textarea id="wpbdp-contact-form-message" name="commentauthormessage" rows="4" class="intextarea"><?php echo esc_textarea(wpbdp_getv($_POST, 'commentauthormessage', '')); ?></textarea>
+    <p><label><?php _ex( 'Message', 'templates', 'WPBDM' ); ?></label>
+       <textarea id="wpbdp-contact-form-message" name="commentauthormessage" rows="4" class="intextarea"><?php echo esc_textarea( wpbdp_getv( $_POST, 'commentauthormessage', '' ) ); ?></textarea>
     </p>
 
     <?php do_action( 'wpbdp_contact_form_extra_fields' ); ?>
 
-    <?php if ($recaptcha): ?>
+    <?php if ( $recaptcha ) : ?>
     <?php echo $recaptcha; ?>
     <?php endif; ?> 
 
-    <input type="submit" class="wpbdp-button wpbdp-submit submit" value="<?php _ex('Send', 'templates', 'WPBDM'); ?>" />
+    <input type="submit" class="wpbdp-button wpbdp-submit submit" value="<?php _ex( 'Send', 'templates', 'WPBDM' ); ?>" />
 </form>
