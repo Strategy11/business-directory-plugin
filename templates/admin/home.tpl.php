0fa5db4950ee8777147334f36e420b946e567533
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-07-05 11:23:23 -0500

    Fix coding standards

--- templates/admin/home.tpl.php
+++ templates/admin/home.tpl.php
@@ -1,22 +1,50 @@
-<?php echo wpbdp_admin_header(); ?>
+<?php
+/**
+ * BD home template
+ *
+ * @package WPBDP/Templates/Admin/Home
+ */
+
+// phpcs:disable
+
+echo wpbdp_admin_header();
+?>
 
 <div class="wpbdp-note welcome-message">
     <h4><?php printf( _x( 'Welcome to Business Directory Plugin. You are using %s.', 'admin home', 'WPBDM' ), '<span class="version">' . wpbdp_get_version() . '</span>' ); ?></h4>
-    <p><?php _ex( 'Thanks for choosing us.  There\'s a lot you probably want to get done, so let\'s jump right in!',
-                  'admin home',
-                  'WPBDM' ); ?></p>
+    <p>
+    <?php
+    _ex(
+        'Thanks for choosing us.  There\'s a lot you probably want to get done, so let\'s jump right in!',
+        'admin home',
+        'WPBDM'
+    );
+					?>
+    </p>
     <ul>
         <li>
-            <?php echo str_replace( '<a>', '<a href="https://businessdirectoryplugin.com/knowledge-base/" target="_blank" rel="noopener">',
-                                    _x( 'Our complete documentation is <a>here</a> which we encourage you to use while setting things up.', 'admin home', 'WPBDM' ) ); ?>
+            <?php
+            echo str_replace(
+                '<a>', '<a href="https://businessdirectoryplugin.com/knowledge-base/" target="_blank" rel="noopener">',
+                _x( 'Our complete documentation is <a>here</a> which we encourage you to use while setting things up.', 'admin home', 'WPBDM' )
+            );
+                                    ?>
         <li>
-            <?php echo str_replace( '<a>', '<a href="https://businessdirectoryplugin.com/article-categories/getting-started/" target="_blank" rel="noopener">',
-                                    _x( 'We have some quick-start scenarios that you will find useful regarding setup and configuration <a>here</a>.', 'admin home', 'WPBDM' ) ); ?>
+            <?php
+            echo str_replace(
+                '<a>', '<a href="https://businessdirectoryplugin.com/article-categories/getting-started/" target="_blank" rel="noopener">',
+                _x( 'We have some quick-start scenarios that you will find useful regarding setup and configuration <a>here</a>.', 'admin home', 'WPBDM' )
+            );
+                                    ?>
         </li>
         <li>
-            <?php echo str_replace( '<a>', '<a href="http://businessdirectoryplugin.com/support-forum/" target="_blank" rel="noopener">',
-                                    _x( 'If you have questions, please post a comment on <a>support forum</a> and we\'ll answer it within 24 hours most days.', 'admin home', 'WPBDM' ) ); ?>
-
+            <?php
+            echo str_replace(
+                '<a>', '<a href="http://businessdirectoryplugin.com/support-forum/" target="_blank" rel="noopener">',
+                _x( 'If you have questions, please post a comment on <a>support forum</a> and we\'ll answer it within 24 hours most days.', 'admin home', 'WPBDM' )
+            );
+                                    ?>
+        </li>
     </ul>
 </div>
 
@@ -33,9 +61,9 @@
     <li class="clear"></li>
 
 
-    <?php if ( wpbdp_get_option( 'payments-on' ) ): ?>
+    <?php if ( wpbdp_get_option( 'payments-on' ) ) : ?>
     <li>
-        <a href="<?php echo admin_url( sprintf( 'edit.php?post_type=%s&wpbdmfilter=unpaid', WPBDP_POST_TYPE ) ) ?>" class="button"><?php _e( 'Manage Paid Listings', 'WPBDM' ); ?></a>
+        <a href="<?php echo admin_url( sprintf( 'edit.php?post_type=%s&wpbdmfilter=unpaid', WPBDP_POST_TYPE ) ); ?>" class="button"><?php _e( 'Manage Paid Listings', 'WPBDM' ); ?></a>
     </li>
     <?php endif; ?>
 </ul>
