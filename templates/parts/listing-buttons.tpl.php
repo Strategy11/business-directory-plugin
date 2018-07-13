939a7c703b88e1e85b1e6d9ac94806259620fa80
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-07-13 18:15:05 -0500

    fix typo in listing buttons template (causing a bug). fixes #3818

--- templates/parts/listing-buttons.tpl.php
+++ templates/parts/listing-buttons.tpl.php
@@ -1,6 +1,15 @@
+<?php
+/**
+ * Listing Buttons template
+ *
+ * @package BDP/Templates/parts/Listing Buttons
+ */
+
+// phpcs:disable
+?>
 <div class="listing-actions cf">
-<?php if ( $view == 'single' ) : ?>
-    <?php if ( wpbdp_user_can( 'edit', $listing_id) ) : ?>
+<?php if ( 'single' === $view ) : ?>
+    <?php if ( wpbdp_user_can( 'edit', $listing_id ) ) : ?>
     <form action="<?php echo wpbdp_url( 'edit_listing', $listing_id ); ?>" method="post"><input type="submit" name="" value="<?php _ex( 'Edit', 'templates', 'WPBDM'); ?>" class="button wpbdp-button edit-listing" /></form>
     <?php endif; ?>
     <?php if ( wpbdp_get_option( 'enable-listing-flagging' ) && wpbdp_user_can( 'flagging', $listing_id ) ) : ?>
@@ -17,6 +26,6 @@
     <?php if ( wpbdp_user_can( 'view', $listing_id ) ) : ?><a class="wpbdp-button button view-listing" href="<?php the_permalink(); ?>" <?php if ( wpbdp_get_option( 'listing-link-in-new-tab' ) ): ?>target="_blank" rel="noopener" <?php endif; ?>><?php _ex('View', 'templates', 'WPBDM'); ?></a><?php endif; ?>
     <?php if ( wpbdp_user_can( 'edit', $listing_id ) ) : ?><a class="wpbdp-button button edit-listing" href="<?php echo wpbdp_url( 'edit_listing', $listing_id ); ?>"><?php _ex('Edit', 'templates', 'WPBDM'); ?></a><?php endif; ?>
     <?php if ( wpbdp_get_option( 'enable-listing-flagging' ) && wpbdp_user_can( 'flagging', $listing_id ) ): ?><a class="wpbdp-button button report-listing" href="<?php echo esc_url( wpbdp_url( 'flag_listing', $listing_id ) ); ?>"><?php echo apply_filters( 'wpbdp_listing_flagging_button_text', _x( 'Flag Listing', 'templates', 'WPBDM' ) ); ?></a><?php endif; ?>
-    <?php if ( wpbdp_user_can( 'delete', $listing_id ) ) : ?><a class="wpbdp-button button delete-listing" href="<?php echo wpbdp_url( 'delete_listing', $listing_id ); ?>"><?php _ex( 'Delete', 'templates', 'WPBD"M' ); ?></a><?php endif; ?>
+    <?php if ( wpbdp_user_can( 'delete', $listing_id ) ) : ?><a class="wpbdp-button button delete-listing" href="<?php echo wpbdp_url( 'delete_listing', $listing_id ); ?>"><?php _ex( 'Delete', 'templates', 'WPBDM' ); ?></a><?php endif; ?>
 <?php endif; ?>
 </div>
