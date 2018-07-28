818793bada72facabfe64afe07e66d42735f0cf5
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-07-28 14:20:42 -0500

    Update Plugin and Themes single_content templates to use default picture

--- themes/default/templates/single_content.tpl.php
+++ themes/default/templates/single_content.tpl.php
@@ -1,25 +1,33 @@
-<?php if ( $images->main ): ?>
-    <?php echo $images->main->html; ?>
+<?php
+/**
+ * Listing detail view rendering template
+ *
+ * @package BDP/Templates/Single Content
+ */
+
+// phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+?>
+<?php if ( $images->main || $images->thumbnail ) : ?>
+    <div class="main-image"><?php echo $images->main ? $images->main->html : $images->thumbnail->html; ?></div>
 <?php endif; ?>
 
 <div class="listing-details cf">
-    <?php foreach ( $fields->not( 'social' ) as $field ): ?>
+    <?php foreach ( $fields->not( 'social' ) as $field ) : ?>
         <?php echo $field->html; ?>
     <?php endforeach; ?>
 
     <?php $social_fields = $fields->filter( 'social' ); ?>
-    <?php if ( $social_fields ): ?>
-    <div class="social-fields cf"><?php echo $social_fields->html; ?></div>
+    <?php if ( $social_fields ) : ?>
+        <div class="social-fields cf"><?php echo $social_fields->html; ?></div>
     <?php endif; ?>
 </div>
 
-<?php if ( $images->extra ): ?>
-<div class="extra-images">
-    <ul>
-        <?php foreach ( $images->extra as $img ): ?>
-        <li><?php echo $img->html; ?></li>
-        <?php endforeach; ?>
-    </ul>
-</div>
+<?php if ( $images->extra ) : ?>
+    <div class="extra-images">
+        <ul>
+            <?php foreach ( $images->extra as $img ) : ?>
+                <li><?php echo $img->html; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 <?php endif; ?>
-
