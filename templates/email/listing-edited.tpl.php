c38abffdfa522fed87397ef1e1757691ad942503
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-08-04 14:47:01 -0500

    Minor edit email notification template fixes.

--- templates/email/listing-edited.tpl.php
+++ templates/email/listing-edited.tpl.php
@@ -1,20 +1,32 @@
 <?php
-    _ex( 'A listing in the directory has been edited recently. Listing details can be found below.', 'emails', 'WPBDM' );
+/**
+ * Listing Edited notification template
+ *
+ * @package BDP/Templates/Email/Listing Edited
+ */
+
+    echo esc_attr_x( 'A listing in the directory has been edited recently. Listing details can be found below.', 'emails', 'WPBDM' );
 ?>
 
 ----
 
-<?php _ex('ID', 'notify email', 'WPBDM'); ?>: <?php echo $listing->get_id(); ?>
+<?php echo esc_attr_x( 'ID', 'notify email', 'WPBDM' ); ?>: <?php echo esc_attr( $listing->get_id() ); ?>
 
 
-<?php _ex('Title', 'notify email', 'WPBDM'); ?>: <?php echo $listing->get_title(); ?>
+<?php echo esc_attr_x( 'Title', 'notify email', 'WPBDM' ); ?>: <?php echo esc_attr( $listing->get_title() ); ?>
 
 
-<?php _ex('URL', 'notify email', 'WPBDM'); ?>: <?php echo $listing->is_published() ? $listing->get_permalink() : _x( '(not published yet)', 'notify email', 'WPBDM' ); ?>
+<?php echo esc_attr_x( 'URL', 'notify email', 'WPBDM' ); ?>: <?php echo $listing->is_published() ? esc_attr( $listing->get_permalink() ) : esc_attr_x( '(not published yet)', 'notify email', 'WPBDM' ); ?>
 
-<?php _ex( 'Admin URL', 'notify email', 'WPBDM' ); ?>: <?php echo get_edit_post_link( $listing->get_id() ); ?>
+<?php echo esc_attr_x( 'Admin URL', 'notify email', 'WPBDM' ); ?>: <?php echo esc_attr( wpbdp_get_edit_post_link( $listing->get_id() ) ); ?>
 
-<?php _ex('Categories', 'notify email', 'WPBDM'); ?>: <?php foreach ( $listing->get_categories() as $category ): ?><?php echo $category->name; ?> / <?php endforeach; ?>
+<?php
+$categories = array();
+foreach ( $listing->get_categories() as $category ) :
+    $categories[] = $category->name;
+endforeach;
+?>
+<?php echo esc_attr( _nx( 'Category', 'Categories', count( $listing->get_categories() ), 'notify email', 'WPBDM' ) ); ?>: <?php echo esc_attr( implode( ' / ', $categories ) ); ?>
 
 
-<?php _ex('Posted By', 'notify email', 'WPBDM'); ?>: <?php echo $listing->get_author_meta( 'user_login' ); ?> (<?php echo $listing->get_author_meta( 'user_email' ); ?>)
+<?php echo esc_attr_x( 'Posted By', 'notify email', 'WPBDM' ); ?>: <?php echo esc_attr( $listing->get_author_meta( 'user_login' ) ); ?> (<?php echo esc_attr( $listing->get_author_meta( 'user_email' ) ); ?>)
