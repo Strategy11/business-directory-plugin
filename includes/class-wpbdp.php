e1924322933f2e6abf4420e8b0b8eefaaf916fc7
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-09-21 14:49:37 -0500

    Bump pkugin version to 5.3.3dev1

--- includes/class-wpbdp.php
+++ includes/class-wpbdp.php
@@ -19,7 +19,7 @@ final class WPBDP {
     }
 
     private function setup_constants() {
-        define( 'WPBDP_VERSION', '5.3.2.1' );
+        define( 'WPBDP_VERSION', '5.3.3dev1' );
 
         define( 'WPBDP_PATH', wp_normalize_path( plugin_dir_path( WPBDP_PLUGIN_FILE ) ) );
         define( 'WPBDP_INC', trailingslashit( WPBDP_PATH . 'includes' ) );
