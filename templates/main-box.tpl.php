3bc81830d295f18b08931062d510008c8f1f806e
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-11-02 22:50:50 -0500

    fix coding standards in main-box.tpl.php file

--- templates/main-box.tpl.php
+++ templates/main-box.tpl.php
@@ -5,16 +5,16 @@
  * @package BDP/Templates/Main Box
  */
 
-// phpcs:disable
+// phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
 ?>
 <div id="wpbdp-main-box" class="wpbdp-main-box" data-breakpoints='{"tiny": [0,360], "small": [360,560], "medium": [560,710], "large": [710,999999]}' data-breakpoints-class-prefix="wpbdp-main-box">
 
-<?php if ( wpbdp_get_option( 'show-search-listings' ) ): ?>
+<?php if ( wpbdp_get_option( 'show-search-listings' ) ) : ?>
 <div class="main-fields box-row cols-2 cf">
     <form action="<?php echo esc_url( $search_url ); ?>" method="get">
         <input type="hidden" name="wpbdp_view" value="search" />
         <?php echo $hidden_fields; ?>
-        <?php if ( ! wpbdp_rewrite_on() ): ?>
+        <?php if ( ! wpbdp_rewrite_on() ) : ?>
         <input type="hidden" name="page_id" value="<?php echo wpbdp_get_page_id(); ?>" />
         <?php endif; ?>
         <div class="box-col search-fields">
@@ -26,8 +26,8 @@
             </div>
         </div>
         <div class="box-col submit-btn">
-            <input type="submit" value="<?php _ex( 'Find Listings', 'main box', 'WPBDM' ); ?>" /><br />
-            <a class="advanced-search-link" href="<?php echo $search_url; ?>"><?php _ex( 'Advanced Search', 'main box', 'WPBDM' ); ?></a>
+            <input type="submit" value="<?php echo esc_attr_x( 'Find Listings', 'main box', 'WPBDM' ); ?>" /><br />
+            <a class="advanced-search-link" href="<?php echo $search_url; ?>"><?php echo esc_attr_x( 'Advanced Search', 'main box', 'WPBDM' ); ?></a>
         </div>
     </form>
 </div>
@@ -35,7 +35,8 @@
 <div class="box-row separator"></div>
 <?php endif; ?>
 
-<?php if ( $main_links = wpbdp_the_main_links( $buttons ) ): ?>
+<?php $main_links = wpbdp_main_links( $buttons ); ?>
+<?php if ( $main_links ) : ?>
 <div class="box-row"><?php echo $main_links; ?></div>
 <?php endif; ?>
 
