851ef7434702281246670a898dfbbf7a92bc8082
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-11-14 15:13:02 -0500

    Fix coding standards in class-listing-search.php

--- includes/helpers/class-listing-search.php
+++ includes/helpers/class-listing-search.php
@@ -13,22 +13,23 @@
  */
 class WPBDP__Listing_Search {
 
-    private $tree = array();
+    private $tree             = array();
     private $original_request = array();
-    private $parts = array();
-    public $aliases = array();
-    private $query_template = '';
-    private $query = '';
-    private $results = null;
+    private $parts            = array();
+    public $aliases           = array();
+    private $query_template   = '';
+    private $query            = '';
+    private $results          = null;
 
 
     public function __construct( $tree, $original_request = array() ) {
-        $this->tree = $tree;
+        $this->tree             = $tree;
         $this->original_request = $original_request;
 
         // If the tree has no head, assume 'and'.
-        if ( ! isset( $this->tree[0] ) || ! is_string( $this->tree[0] ) )
+        if ( ! isset( $this->tree[0] ) || ! is_string( $this->tree[0] ) ) {
             array_unshift( $this->tree, 'and' );
+        }
     }
 
     public function terms_for_field( $field ) {
@@ -37,8 +38,9 @@ class WPBDP__Listing_Search {
         $result = array();
 
         foreach ( $this->parts as $p ) {
-            if ( $field == $p[0] )
+            if ( $field == $p[0] ) {
                 $result[] = $p[1];
+            }
         }
 
         return $result;
@@ -75,16 +77,18 @@ class WPBDP__Listing_Search {
         $this->query_template = $this->_traverse_tree( $this->tree );
 
         // Build query.
-        $query_pieces = array( 'where' => $this->query_template,
-                               'join' => '',
-                               'orderby' => '',
-                               'distinct' => '',
-                               'fields' => "{$wpdb->posts}.ID",
-                               'limits' => '' );
+        $query_pieces = array(
+			'where'    => $this->query_template,
+			'join'     => '',
+			'orderby'  => '',
+			'distinct' => '',
+			'fields'   => "{$wpdb->posts}.ID",
+			'limits'   => '',
+		);
 
-        foreach ( $this->parts as $key => $data )  {
+        foreach ( $this->parts as $key => $data ) {
             $field = wpbdp_get_form_field( $data[0] );
-            $res = $field->configure_search( $data[1], $this );
+            $res   = $field->configure_search( $data[1], $this );
 
             if ( ! empty( $res['where'] ) ) {
                 $query_pieces['where'] = str_replace( '%' . $key . '%', $res['where'], $query_pieces['where'] );
@@ -95,8 +99,9 @@ class WPBDP__Listing_Search {
             }
 
             foreach ( $res as $k => $v ) {
-                if ( 'where' == $k )
+                if ( 'where' == $k ) {
                     continue;
+                }
 
                 $query_pieces[ $k ] .= ' ' . $v . ' ';
             }
@@ -109,15 +114,17 @@ class WPBDP__Listing_Search {
 
         $query_pieces = apply_filters_ref_array( 'wpbdp_search_query_pieces', array( $query_pieces, $this ) );
 
-        $this->query = sprintf( "SELECT %s %s FROM {$wpdb->posts} %s WHERE ({$wpdb->posts}.post_type = '%s' AND {$wpdb->posts}.post_status = '%s') AND %s GROUP BY {$wpdb->posts}.ID %s %s",
-                                $query_pieces['distinct'],
-                                $query_pieces['fields'],
-                                $query_pieces['join'],
-                                WPBDP_POST_TYPE,
-                                'publish',
-                                $query_pieces['where'],
-                                $query_pieces['orderby'],
-                                $query_pieces['limits'] );
+        $this->query = sprintf(
+            "SELECT %s %s FROM {$wpdb->posts} %s WHERE ({$wpdb->posts}.post_type = '%s' AND {$wpdb->posts}.post_status = '%s') AND %s GROUP BY {$wpdb->posts}.ID %s %s",
+            $query_pieces['distinct'],
+            $query_pieces['fields'],
+            $query_pieces['join'],
+            WPBDP_POST_TYPE,
+            'publish',
+            $query_pieces['where'],
+            $query_pieces['orderby'],
+            $query_pieces['limits']
+        );
         // wpbdp_debug_e($this->query);
         $this->results = $wpdb->get_col( $this->query );
     }
@@ -126,13 +133,14 @@ class WPBDP__Listing_Search {
         if ( is_array( $tree ) && 2 == count( $tree ) && is_numeric( $tree[0] ) ) {
             $key = md5( serialize( $tree ) );
 
-            if ( ! isset( $this->parts[ $key ] ) )
+            if ( ! isset( $this->parts[ $key ] ) ) {
                 $this->parts[ $key ] = $tree;
+            }
 
             return '%' . $key . '%';
         }
 
-        $res = '';
+        $res  = '';
         $head = $tree[0];
         $args = array_slice( $tree, 1 );
 
@@ -150,15 +158,16 @@ class WPBDP__Listing_Search {
     }
 
     public function join_alias( $table, $reuse = false ) {
-        if ( ! isset( $this->aliases[ $table ] ) )
+        if ( ! isset( $this->aliases[ $table ] ) ) {
             $this->aliases[ $table ] = array();
+        }
 
-        $i = count( $this->aliases[ $table ] );
-        $alias = '';
+        $i      = count( $this->aliases[ $table ] );
+        $alias  = '';
         $reused = false;
 
         if ( $reuse && $i > 0 ) {
-            $alias = $this->aliases[ $table ][ $i - 1 ];
+            $alias  = $this->aliases[ $table ][ $i - 1 ];
             $reused = true;
         } else {
             $alias = $i > 0 ? $table . '_t_' . $i : $table;
@@ -181,7 +190,7 @@ class WPBDP__Listing_Search {
             if ( wpbdp_get_option( 'quick-search-enable-performance-tricks' ) ) {
                 $request['kw'] = array( trim( $request['kw'] ) );
             } else {
-                $request['kw'] = explode( ' ',  trim( $request['kw'] ) );
+                $request['kw'] = explode( ' ', trim( $request['kw'] ) );
             }
 
             $fields = array();
@@ -210,8 +219,9 @@ class WPBDP__Listing_Search {
             $res[] = 'and';
 
             foreach ( $request['listingfields'] as $field_id => $term ) {
-                if ( ! $term )
+                if ( ! $term ) {
                     continue;
+                }
 
                 $res[] = array( $field_id, $term );
             }
@@ -235,7 +245,7 @@ class WPBDP__Listing_Search {
     }
 
     public static function tree_remove_field( $tree, $field, $term = null ) {
-        $field = is_object( $field ) ? $field->get_id() : absint( $field );
+        $field  = is_object( $field ) ? $field->get_id() : absint( $field );
         $result = array();
 
         foreach ( $tree as $t ) {
