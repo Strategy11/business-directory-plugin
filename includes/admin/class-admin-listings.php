9ec4fd7e0680940f74d09edc85e34666473eff53
Diego Giraldo <diego.dgiraldo36@gmail.com>
2018-11-21 15:28:59 -0500

    Bump Plugin version to 5.3.5dev3

--- includes/admin/class-admin-listings.php
+++ includes/admin/class-admin-listings.php
@@ -497,10 +497,10 @@ class WPBDP_Admin_Listings {
 
         $listing = wpbdp_get_listing( $post_id );
 
-        $thumbnail_id = $_POST['_thumbnail_id'] ? $_POST['_thumbnail_id'] : ( $_POST['thumbnail_id'] ? $_POST['thumbnail_id'] : 0 );
+        $thumbnail_id = intval( $_POST['_thumbnail_id'] ) > 0 ? intval( $_POST['_thumbnail_id'] ) : ( ! empty( $_POST['thumbnail_id'] ) ?  intval( ['thumbnail_id'] ) : 0 );
 
         // Update image information.
-        if ( $thumbnail_id ) {
+        if ( $thumbnail_id > 0 ) {
             $listing->set_thumbnail_id( $thumbnail_id );
             $listing->set_images( $thumbnail_id, true );
         }
