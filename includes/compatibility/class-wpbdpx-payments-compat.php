a8b3f97881dafbcae31bcaa34de525b27dc44e5e
Diego Giraldo <diego.dgiraldo36@gmail.com>
2019-01-13 14:58:17 -0500

    Fix payment object retrieving in WPBDP__WPBDPX_Payments_Compat class

--- includes/compatibility/class-wpbdpx-payments-compat.php
+++ includes/compatibility/class-wpbdpx-payments-compat.php
@@ -1,5 +1,10 @@
-<?php
+<?php // phpcs:disable
 
+/**
+ * Class WPBDP__WPBDPX_Payments_Compat
+ *
+ * @SuppressWarnings(PHPMD)
+ */
 class WPBDP__WPBDPX_Payments_Compat {
 
     private $gateways;
@@ -11,33 +16,36 @@ class WPBDP__WPBDPX_Payments_Compat {
 
     public function dispatch() {
         $action = isset( $_GET['action'] ) ? trim( $_GET['action'] ) : '';
-        $payment = isset( $_GET['payment_id'] ) ? WPBDP_Payment::get( intval( $_GET['payment_id'] ) ) : null;
+        $payment = isset( $_GET['payment_id'] ) ? wpbdp_get_payment( intval( $_GET['payment_id'] ) ) : null;
         $gid = isset( $_GET['gid'] ) ? trim( $_GET['gid'] ) : '';
 
-        if ( ! in_array( $action, array( 'postback', 'process', 'notify', 'return', 'cancel', 'ins' ) ) || ( ! $payment && ! $gid ) )
+        if ( ! in_array( $action, array( 'postback', 'process', 'notify', 'return', 'cancel', 'ins' ) ) || ( ! $payment && ! $gid ) ) {
             return;
+        }
 
         unset( $_GET['action'] );
 
-        if ( $gid )
+        if ( $gid ) {
             unset( $_GET['gid'] );
+        }
 
         $gateway_id = $payment ? $payment->gateway : $gid;
         $gateway = $this->gateways->get( $gateway_id );
 
-        if ( ! $gateway )
+        if ( ! $gateway ) {
             return;
+        }
 
         switch ( $gateway ) {
-        case '2checkout':
-            $_POST['wpbdp_payment_id']    = $_REQUEST['merchant_order_id'];
-            $_GET['wpbdp_payment_id']     = $_REQUEST['merchant_order_id'];
-            $_REQUEST['wpbdp_payment_id'] = $_REQUEST['merchant_order_id'];
-            break;
-        case 'paypal':
-            break;
-        case 'stripe':
-            break;
+            case '2checkout':
+                $_POST['wpbdp_payment_id']    = $_REQUEST['merchant_order_id'];
+                $_GET['wpbdp_payment_id']     = $_REQUEST['merchant_order_id'];
+                $_REQUEST['wpbdp_payment_id'] = $_REQUEST['merchant_order_id'];
+                break;
+            case 'paypal':
+                break;
+            case 'stripe':
+                break;
         }
 
         $gateway->process_postback();
@@ -45,8 +53,4 @@ class WPBDP__WPBDPX_Payments_Compat {
     }
 
 }
-    //     // if ( ! $payment )
-    //     //     $this->gateways[ $gateway_id ]->process_generic( $action );
-    //     // else
-    //     //     $this->gateways[ $gateway_id ]->process( $payment, $action );
-    // }
+
