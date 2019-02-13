e0d15e87fd5565097dfbaabeb56d23de3f7ea60c
Diego Giraldo <diego.dgiraldo36@gmail.com>
2019-02-13 16:12:16 -0500

    Fix coding standards in class-view.php

--- includes/class-view.php
+++ includes/class-view.php
@@ -9,14 +9,16 @@
 // phpcs:disable
 /**
  * Class WPBDP__View
+ *
  * @SuppressWarnings(PHPMD)
  */
 class WPBDP__View {
 
     public function __construct( $args = null ) {
         if ( is_array( $args ) ) {
-            foreach ( $args as $k => $v )
+            foreach ( $args as $k => $v ) {
                 $this->{$k} = $v;
+            }
         }
     }
 
@@ -35,62 +37,66 @@ class WPBDP__View {
     //
     // API for views. {
     //
-
-    protected final function _http_404() {
+    final protected function _http_404() {
         status_header( 404 );
         nocache_headers();
 
-        if ( $template_404 = get_404_template() )
-            include( $template_404 );
+        if ( $template_404 = get_404_template() ) {
+            include $template_404;
+        }
 
         exit;
     }
 
-    protected final function _redirect( $url ) {
+    final protected function _redirect( $url ) {
         wp_redirect( $url );
         exit;
     }
 
-    protected final function _render() {
+    final protected function _render() {
         $args = func_get_args();
         return call_user_func_array( 'wpbdp_x_render', $args );
     }
 
-    protected final function _render_page() {
+    final protected function _render_page() {
         $args = func_get_args();
         return call_user_func_array( 'wpbdp_x_render_page', $args );
     }
 
-    protected final function _auth_required( $args = array() ) {
+    final protected function _auth_required( $args = array() ) {
         $defaults = array(
-            'test' => '',
-            'login_url' => wpbdp_url( 'login' ),
+            'test'                => '',
+            'login_url'           => wpbdp_url( 'login' ),
             'redirect_on_failure' => true,
-            'wpbdp_view' => '',
+            'wpbdp_view'          => '',
             'redirect_query_args' => array(),
         );
-        $args = wp_parse_args( $args, $defaults );
+        $args     = wp_parse_args( $args, $defaults );
         extract( $args );
 
-        if ( ! $test && method_exists( $this, 'authenticate' ) )
+        if ( ! $test && method_exists( $this, 'authenticate' ) ) {
             $test = array( $this, 'authenticate' );
+        }
 
-        if ( is_callable( $test ) )
+        if ( is_callable( $test ) ) {
             $passes = call_user_func( $test );
-        elseif ( 'administrator' == $test )
+        } elseif ( 'administrator' == $test ) {
             $passes = current_user_can( 'administrator' );
-        else
-            $passes = is_user_logged_in();
+        } else {
+			$passes = is_user_logged_in();
+        }
 
-        if ( $passes )
+        if ( $passes ) {
             return;
+        }
 
-        if ( is_user_logged_in() )
+        if ( is_user_logged_in() ) {
             $redirect_on_failure = false;
+        }
 
         if ( $redirect_on_failure ) {
             $query_args['redirect_to'] = urlencode( $wpbdp_view ? wpbdp_url( $wpbdp_view ) : apply_filters( 'the_permalink', get_permalink() ) );
-            $login_url = add_query_arg( $redirect_query_args, $login_url );
+            $login_url                 = add_query_arg( $redirect_query_args, $login_url );
 
             return $this->_redirect( $login_url );
         } else {
