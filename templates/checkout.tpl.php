f03462483dc1709be97c2c95dc2eef7fef854136
Diego Giraldo <diego.dgiraldo36@gmail.com>
2019-03-13 12:30:17 -0500

    Fix coding standards in Checkout.tpl.php file

--- templates/checkout.tpl.php
+++ templates/checkout.tpl.php
@@ -1,5 +1,14 @@
-<?php //phpcs:disable ?>
-<h2><?php _ex( 'Checkout', 'checkout', 'WPBDM'); ?></h2>
+<?php
+/**
+ * Checkout template
+ *
+ * @package BDP/Templates/Checkout
+ */
+
+// phpcs:disable
+?>
+
+<h2><?php _ex( 'Checkout', 'checkout', 'WPBDM' ); ?></h2>
 
 <div class="wpbdp-payment-invoice">
     <?php echo $invoice; ?>
@@ -13,21 +22,21 @@
     <?php echo $checkout_form_top; ?>
 
     <div class="wpbdp-checkout-errors wpbdp-checkout-section">
-        <?php if ( ! empty( $errors ) ): ?>
-            <?php foreach ( $errors as $error ): ?>
+        <?php if ( ! empty( $errors ) ) : ?>
+            <?php foreach ( $errors as $error ) : ?>
             <div class="wpbdp-msg error wpbdp-checkout-error"><?php echo $error; ?></div>
             <?php endforeach; ?>
         <?php endif; ?>
         
         <?php if ( $payment->has_item_type( 'discount_code' ) && $payment->has_item_type( 'recurring_plan' ) && 0.0 == $payment->amount ) : ?>
-            <div class="wpbdp-msg notice"><?php _ex( "Recurring fee plans require a payment method to renew your listing at the end of the term.", 'checkout', 'WPBDM' ); ?></div>
+            <div class="wpbdp-msg notice"><?php _ex( 'Recurring fee plans require a payment method to renew your listing at the end of the term.', 'checkout', 'WPBDM' ); ?></div>
         <?php endif; ?>
     </div>
 
 
     <div class="wpbdp-checkout-gateway-selection wpbdp-checkout-section">
         <h3><?php _ex( 'Select a Payment Method', 'checkout', 'WPBDM' ); ?></h3>
-        <?php foreach ( wpbdp()->payment_gateways->get_available_gateways( array( 'currency_code' => $payment->currency_code ) ) as $gateway ): ?>
+        <?php foreach ( wpbdp()->payment_gateways->get_available_gateways( array( 'currency_code' => $payment->currency_code ) ) as $gateway ) : ?>
         <label><input type="radio" name="gateway" value="<?php echo $gateway->get_id(); ?>" <?php checked( $chosen_gateway->get_id(), $gateway->get_id() ); ?>/> <?php echo $gateway->get_logo(); ?></label>
         <?php endforeach; ?>
         <div class="wpbdp-checkout-submit wpbdp-no-js"><input type="submit" value="<?php _ex( 'Next', 'checkout', 'WPBDM' ); ?>" /></div>
