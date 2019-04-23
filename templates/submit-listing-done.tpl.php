c694e66364300cf609c0bb869b3597c5d1bbfe29
Diego Giraldo <diego.dgiraldo36@gmail.com>
2019-04-23 16:52:44 -0500

    Include "Thank you for payment message" in submit confirmation page.

--- templates/submit-listing-done.tpl.php
+++ templates/submit-listing-done.tpl.php
@@ -1,23 +1,34 @@
-<h3><?php _ex( 'Submission Received', 'templates', 'WPBDM' ); ?></h3>
+<?php
+/**
+ * Submit Listing Done Template
+ *
+ * @package WPBDP/Templates
+ */
 
-<?php if ( ! $editing ): ?>
-    <p><?php _ex( 'Your listing has been submitted.', 'templates', 'WPBDM' ); ?></p>
-    <?php if ( $payment && $payment->amount > 0.0 ): ?>
-    <div id="wpbdp-checkout-confirmation-receipt">
-        <?php echo wpbdp()->payments->render_receipt( $payment ); ?>
-    </div>
+?>
+<h3><?php echo esc_html_x( 'Submission Received', 'templates', 'WPBDM' ); ?></h3>
+
+<?php if ( ! $editing ) : ?>
+    <p><?php echo esc_html_x( 'Your listing has been submitted.', 'templates', 'WPBDM' ); ?></p>
+    <?php if ( $payment && $payment->amount > 0.0 ) : ?>
+        <p>
+        <?php echo esc_html( wpbdp_get_option( 'payment-message' ) ); ?>
+        </p>
+        <div id="wpbdp-checkout-confirmation-receipt">
+            <?php echo wpbdp()->payments->render_receipt( $payment ); ?>
+        </div>
     <?php endif; ?>
-<?php else: ?>
-    <p><?php _ex('Your listing changes were saved.', 'templates', 'WPBDM'); ?></p>
+<?php else : ?>
+    <p><?php echo esc_html_x( 'Your listing changes were saved.', 'templates', 'WPBDM' ); ?></p>
 <?php endif; ?>
 
     <p>
-        <?php if ( 'publish' == get_post_status( $listing->get_id() ) ): ?>
-            <a href="<?php echo get_permalink( $listing->get_id() ); ?>"><?php _ex( 'Go to your listing', 'templates', 'WPBDM' ); ?></a> | 
-        <?php else: ?>
-            <?php _ex( 'Your listing requires admin approval. You\'ll be notified once your listing is approved.', 'templates', 'WPBDM' ); ?>
+        <?php if ( 'publish' === get_post_status( $listing->get_id() ) ) : ?>
+            <a href="<?php echo esc_attr( get_permalink( $listing->get_id() ) ); ?>"><?php echo esc_html_x( 'Go to your listing', 'templates', 'WPBDM' ); ?></a> | 
+        <?php else : ?>
+            <?php echo esc_html_x( 'Your listing requires admin approval. You\'ll be notified once your listing is approved.', 'templates', 'WPBDM' ); ?>
     </p>
     <p>
         <?php endif; ?>
-        <a href="<?php echo wpbdp_get_page_link( 'main' ); ?>"><?php _ex( 'Return to directory.', 'templates', 'WPBDM' ); ?></a>
+        <a href="<?php echo esc_attr( wpbdp_get_page_link( 'main' ) ); ?>"><?php echo esc_html_x( 'Return to directory.', 'templates', 'WPBDM' ); ?></a>
     </p>
