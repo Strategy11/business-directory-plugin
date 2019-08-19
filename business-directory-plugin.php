432cde6cba033a6f52d9e0e06dc194e7be5ec685
Diego Giraldo <diego.dgiraldo36@gmail.com>
2019-08-19 09:06:56 -0500

    Bump Plugin version to 5.5.10dev1

--- business-directory-plugin.php
+++ business-directory-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Business Directory Plugin
  * Plugin URI: https://www.businessdirectoryplugin.com
  * Description: Provides the ability to maintain a free or paid business directory on your WordPress powered site.
- * Version: 5.5.9
+ * Version: 5.5.10dev1
  * Author: D. Rodenbaugh
  * Author URI: https://businessdirectoryplugin.com
  * Text Domain: WPBDM
