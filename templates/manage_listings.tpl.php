452b63f2f1bd265652aee319cb9f1baeb5494c9c
Diego Giraldo <diego.dgiraldo36@gmail.com>
2020-08-02 10:47:58 -0500

    Process "Manage Listings" query directly on manage_listings.tpl.php

--- templates/manage_listings.tpl.php
+++ templates/manage_listings.tpl.php
@@ -1,10 +1,26 @@
 <div id="wpbdp-manage-listings-page" class="wpbdp-manage-listings-page businessdirectory-manage-listings businessdirectory wpbdp-page">
-    <?php if ( $query-> have_posts() ): ?>
-        <p><?php _ex("Your current listings are shown below. To edit a listing click the edit button. To delete a listing click the delete button.", 'templates', "WPBDM"); ?></p>
-        <?php echo wpbdp_x_part( 'listings' ); ?>
-    <?php else: ?>
+    <?php if ( ! $query->have_posts() ): ?>
         <p><?php _ex('You do not currently have any listings in the directory.', 'templates', 'WPBDM'); ?></p>
         <?php echo sprintf('<a href="%s">%s</a>.', wpbdp_get_page_link('main'),
-                           _x('Return to directory', 'templates', 'WPBDM')); ?>     
+                           _x('Return to directory', 'templates', 'WPBDM')); ?> 
+    <?php else: ?>
+        <p><?php _ex("Your current listings are shown below. To edit a listing click the edit button. To delete a listing click the delete button.", 'templates', "WPBDM"); ?></p>
+        <?php
+        while ( $query->have_posts() ) :
+            $query->the_post();
+            echo WPBDP_Listing_Display_Helper::excerpt();
+        endwhile;
+        ?>
+
+        <div class="wpbdp-pagination">
+        <?php
+        if ( function_exists( 'wp_pagenavi' ) ) :
+            wp_pagenavi( array( 'query' => $query ) );
+        else :
+        ?>
+            <span class="prev"><?php previous_posts_link( _x( '&laquo; Previous ', 'templates', 'WPBDM' ) ); ?></span>
+            <span class="next"><?php next_posts_link( _x( 'Next &raquo;', 'templates', 'WPBDM' ), $query->max_num_pages ); ?></span>
+        <?php endif; ?>
+        </div>
     <?php endif; ?>
 </div>
