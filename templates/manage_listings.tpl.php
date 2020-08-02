8b3ee2835d413da3542059fa5cf0ffc5c68c011f
Diego Giraldo <diego.dgiraldo36@gmail.com>
2020-08-02 10:50:34 -0500

    Fix coding standards in manage_listings.tpl.php

--- templates/manage_listings.tpl.php
+++ templates/manage_listings.tpl.php
@@ -1,23 +1,35 @@
+<?php
+/**
+ * Manage Listings rendering template
+ *
+ * @package BDP/templates/Manage Listings
+ */
+
+?>
 <div id="wpbdp-manage-listings-page" class="wpbdp-manage-listings-page businessdirectory-manage-listings businessdirectory wpbdp-page">
-    <?php if ( ! $query->have_posts() ): ?>
-        <p><?php _ex('You do not currently have any listings in the directory.', 'templates', 'WPBDM'); ?></p>
-        <?php echo sprintf('<a href="%s">%s</a>.', wpbdp_get_page_link('main'),
-                           _x('Return to directory', 'templates', 'WPBDM')); ?> 
-    <?php else: ?>
-        <p><?php _ex("Your current listings are shown below. To edit a listing click the edit button. To delete a listing click the delete button.", 'templates', "WPBDM"); ?></p>
+    <?php if ( ! $query->have_posts() ) : ?>
+        <p><?php echo esc_html_x( 'You do not currently have any listings in the directory.', 'templates', 'WPBDM' ); ?></p>
+        <?php
+        echo sprintf(
+            '<a href="%s">%s</a>.',
+            esc_attr( wpbdp_get_page_link( 'main' ) ),
+            esc_html_x( 'Return to directory', 'templates', 'WPBDM' )
+        );
+        ?>
+    <?php else : ?>
+        <p><?php echo esc_html_x( 'Your current listings are shown below. To edit a listing click the edit button. To delete a listing click the delete button.', 'templates', 'WPBDM' ); ?></p>
         <?php
         while ( $query->have_posts() ) :
             $query->the_post();
+            // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
             echo WPBDP_Listing_Display_Helper::excerpt();
         endwhile;
         ?>
 
         <div class="wpbdp-pagination">
-        <?php
-        if ( function_exists( 'wp_pagenavi' ) ) :
-            wp_pagenavi( array( 'query' => $query ) );
-        else :
-        ?>
+        <?php if ( function_exists( 'wp_pagenavi' ) ) : ?>
+            <?php wp_pagenavi( array( 'query' => $query ) ); ?>
+        <?php else : ?>
             <span class="prev"><?php previous_posts_link( _x( '&laquo; Previous ', 'templates', 'WPBDM' ) ); ?></span>
             <span class="next"><?php next_posts_link( _x( 'Next &raquo;', 'templates', 'WPBDM' ), $query->max_num_pages ); ?></span>
         <?php endif; ?>
