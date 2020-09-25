5a098ce2a13d524fabf584b38cd9d3682aae6367
Steph <steph@strategy11.com>
2020-09-25 09:27:09 -0600

    copy php warning fix into master https://github.com/Strategy11/BusinessDirectoryPlugin/commit/68684e07ccf98311b15c5998a6b710daace20b89

--- includes/fields/class-fieldtypes-url.php
+++ includes/fields/class-fieldtypes-url.php
@@ -139,6 +139,11 @@ class WPBDP_FieldTypes_URL extends WPBDP_Form_Field_Type {
     }
 
     public function render_field_inner( &$field, $value, $context, &$extra=null, $field_settings = array() ) {
+		if ( empty( $value ) ) {
+			// Set an empty array to avoid php warnings.
+			$value = array( '', '' );
+		}
+
         if ( $context == 'search' ) {
             global $wpbdp;
             return $wpbdp->formfields->get_field_type( 'textfield' )->render_field_inner( $field, $value[1], $context, $extra, $field_settings );
