56779ee3650d43b8033398912996312c8d1b4c0c
Steph <steph@strategy11.com>
2020-10-22 09:40:39 -0600

    merge in some changes from other branch Use &larr; and &rarr; for consistency

--- templates/deprecated/search.tpl.php
+++ templates/deprecated/search.tpl.php
@@ -3,32 +3,37 @@ $api = wpbdp_formfields_api();
 ?>
 <div id="wpbdp-search-page" class="wpbdp-search-page businessdirectory-search businessdirectory wpbdp-page">
     <div class="wpbdp-bar cf"><?php wpbdp_the_main_links(); ?></div>
-    <h2 class="title"><?php _ex('Search', 'search', 'business-directory-plugin' ); ?></h2>
+    <h2 class="title"><?php esc_html_e( 'Search', 'business-directory-plugin' ); ?></h2>
 
     <?php if ( 'none' == $search_form_position || 'above' == $search_form_position ): ?>
-    <?php echo $search_form; ?>
+        <?php echo $search_form; ?>
     <?php endif; ?>
 
     <!-- Results -->
     <?php if ($searching): ?>    
-        <h3><?php _ex('Search Results', 'search', 'business-directory-plugin' ); ?></h3>    
+        <h3><?php esc_html_e( 'Search Results', 'business-directory-plugin' ); ?></h3>
 
         <?php do_action( 'wpbdp_before_search_results' ); ?>
         <div class="search-results">
         <?php if (have_posts()): ?>
-            <?php echo wpbdp_render('businessdirectory-listings'); ?>
+            <?php wpbdp_render( 'businessdirectory-listings', array( 'echo' => true ) ); ?>
         <?php else: ?>
-            <?php _ex("No listings found.", 'templates', "WPBDM"); ?>
+            <?php esc_html_e( 'No listings found.', 'business-directory-plugin' ); ?>
             <br />
-            <?php echo sprintf('<a href="%s">%s</a>.', wpbdp_get_page_link('main'),
-                               _x('Return to directory', 'templates', 'business-directory-plugin' )); ?>    
+            <?php
+            printf(
+                '<a href="%s">%s</a>.',
+                esc_url( wpbdp_get_page_link( 'main' ) ),
+                esc_html__( 'Return to directory', 'business-directory-plugin' )
+            );
+            ?>
         <?php endif; ?>
         </div>
         <?php do_action( 'wpbdp_after_search_results' ); ?>
     <?php endif; ?>
 
     <?php if ( 'below' == $search_form_position ): ?>
-    <?php echo $search_form; ?>
+        <?php echo $search_form; ?>
     <?php endif; ?>
 
 </div>
