558f197549256da504f53f789312406503d04ed8
Diego Giraldo <diego.dgiraldo36@gmail.com>
2020-11-03 15:18:18 -0500

    Bump plugin version to 5.7.7dev1

--- includes/class-wpbdp.php
+++ includes/class-wpbdp.php
@@ -20,7 +20,7 @@ final class WPBDP {
     }
 
     private function setup_constants() {
-        define( 'WPBDP_VERSION', '5.7.6' );
+        define( 'WPBDP_VERSION', '5.7.7dev1' );
 
         define( 'WPBDP_PATH', wp_normalize_path( plugin_dir_path( WPBDP_PLUGIN_FILE ) ) );
         define( 'WPBDP_INC', trailingslashit( WPBDP_PATH . 'includes' ) );
