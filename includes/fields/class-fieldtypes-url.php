36abcdb841d98f3856b3ca78da461de9324b2e7c
Steph <steph@strategy11.com>
2020-11-05 13:30:39 -0700

    merge in security changes in another file

--- includes/fields/class-fieldtypes-url.php
+++ includes/fields/class-fieldtypes-url.php
@@ -5,17 +5,13 @@
  * @package WPBDP/Views/Includes/Fields/URL
  */
 
-// phpcs:disable
-
 /**
  * Class WPBDP_FieldTypes_URL
- * 
- * @SuppressWarnings(PHPMD)
  */
 class WPBDP_FieldTypes_URL extends WPBDP_Form_Field_Type {
 
     public function __construct() {
-        parent::__construct( _x( 'URL Field', 'form-fields api', 'business-directory-plugin' ) );
+        parent::__construct( __( 'URL Field', 'business-directory-plugin' ) );
         add_filter( 'wpbdp_form_field_css_classes', array( $this, 'css_classes' ), 10, 3 );
     }
 
@@ -33,10 +29,10 @@ class WPBDP_FieldTypes_URL extends WPBDP_Form_Field_Type {
 
         $settings = array();
 
-        $settings['new-window'][] = _x( 'Open link in a new window?', 'form-fields admin', 'business-directory-plugin' );
+        $settings['new-window'][] = esc_html__( 'Open link in a new window?', 'business-directory-plugin' );
         $settings['new-window'][] = '<input type="checkbox" value="1" name="field[x_open_in_new_window]" ' . ( $field && $field->data( 'open_in_new_window' ) ? ' checked="checked"' : '' ) . ' />';
 
-        $settings['nofollow'][] = _x( 'Use rel="nofollow" when displaying the link?', 'form-fields admin', 'business-directory-plugin' );
+        $settings['nofollow'][] = esc_html__( 'Use rel="nofollow" when displaying the link?', 'business-directory-plugin' );
         $settings['nofollow'][] = '<input type="checkbox" value="1" name="field[x_use_nofollow]" ' . ( $field && $field->data( 'use_nofollow' ) ? ' checked="checked"' : '' ) . ' />';
 
         return self::render_admin_settings( $settings );
@@ -85,7 +81,7 @@ class WPBDP_FieldTypes_URL extends WPBDP_Form_Field_Type {
                         $field->data( 'use_nofollow' ) == true ? 'nofollow': '',
                         $field->data( 'open_in_new_window' ) == true ? '"_blank" rel="noopener"' : '"_self"',
                         esc_attr( ! empty( $value[1] ) ? $value[1] : $value[0] ),
-                        esc_attr( ! empty( $value[1] ) ? $value[1] : $value[0] ) );
+                        esc_html( ! empty( $value[1] ) ? $value[1] : $value[0] ) );
     }
 
     public function get_field_plain_value( &$field, $post_id ) {
@@ -111,15 +107,15 @@ class WPBDP_FieldTypes_URL extends WPBDP_Form_Field_Type {
             return sprintf( '%s,%s', $value[0], $value[1] );
         }
 
-        return is_array( $value ) ? $value[0] : $value ;
+        return is_array( $value ) ? $value[0] : $value;
     }
 
     public function convert_input( &$field, $input ) {
         if ( $input === null )
             return array( '', '' );
 
-        $url = trim( is_array( $input ) ? $input[0] : $input );
-        $text = trim( is_array( $input ) ? $input[1] : '' );
+        $url = trim( sanitize_text_field( is_array( $input ) ? $input[0] : $input ) );
+        $text = trim( is_array( $input ) ? sanitize_text_field( $input[1] ) : '' );
 
         if ( $url && ! parse_url( $url, PHP_URL_SCHEME ) )
             $url = 'http://' . $url;
@@ -153,30 +149,30 @@ class WPBDP_FieldTypes_URL extends WPBDP_Form_Field_Type {
         $html .= '<div class="wpbdp-url-field-col wpbdp-half">';
         $html .= sprintf(
             '<label for="%s"><span class="sublabel">%s</span></label>',
-            'wpbdp-field-' . $field->get_id() . '-url',
+            'wpbdp-field-' . esc_attr( $field->get_id() ) . '-url',
             _x( 'URL:', 'form-fields api', 'business-directory-plugin' )
         );
         $html .= sprintf( '<input type="text" id="%s" name="%s" value="%s" />',
-                          'wpbdp-field-' . $field->get_id(),
-                          'listingfields[' . $field->get_id() . '][0]',
+                          'wpbdp-field-' . esc_attr( $field->get_id() ),
+                          'listingfields[' . esc_attr( $field->get_id() ) . '][0]',
                           esc_attr( $value[0] ) );
         $html .= '</div>';
 
         $html .= '<div class="wpbdp-url-field-col wpbdp-half">';
         $html .= sprintf(
             '<label for="%s"><span class="sublabel">%s</span></label>',
-            'wpbdp-field-' . $field->get_id() . '-title',
-            _x( 'Link Text (optional):','form-fields api', 'business-directory-plugin' )
+            'wpbdp-field-' . esc_attr( $field->get_id() ) . '-title',
+            esc_html__( 'Link Text (optional)', 'business-directory-plugin' ) . ':'
         );
         $html .= sprintf( '<input type="text" id="%s" name="%s" value="%s" placeholder="" />',
-                          'wpbdp-field-' . $field->get_id() . '-title',
-                          'listingfields[' . $field->get_id() . '][1]',
+                          'wpbdp-field-' . esc_attr( $field->get_id() ) . '-title',
+                          'listingfields[' . esc_attr( $field->get_id() )    . '][1]',
                           esc_attr( $value[1] ) );
         $html .= '</div>';
 
-        if ( 'submit' === $context ) {
-            $html = '<div class="wpbdp-grid">' . $html . '</div>';
-        }
+		if ( 'submit' === $context ) {
+			$html = '<div class="wpbdp-grid">' . $html . '</div>';
+		}
 
         return $html;
     }
