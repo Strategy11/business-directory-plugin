c066a396c303b89b63088085cf56df3b238b2ad1
Diego Giraldo <diego.dgiraldo36@gmail.com>
2020-11-10 17:29:20 -0500

    more changes from security branch

--- templates/submit-listing-plan-selection.tpl.php
+++ templates/submit-listing-plan-selection.tpl.php
@@ -10,12 +10,12 @@ $single_plan = ! ( count( $plans ) > 1 );
 <div class="wpbdp-category-selection-with-tip">
     <?php if ( $editing ) : ?>
     <div class="wpbdp-msg tip">
-    <?php
-    esc_html_e(
-        'You can\'t change the plan your listing is on but you can modify the categories it appears in, using the field below. Details about the plan are shown for completeness.',
-        'business-directory-plugin'
-    );
-    ?>
+        <?php
+        esc_html_e(
+            'You can\'t change the plan your listing is on but you can modify the categories it appears in, using the field below. Details about the plan are shown for completeness.',
+            'business-directory-plugin'
+        );
+        ?>
     </div>
     <?php endif; ?>
 
@@ -39,20 +39,21 @@ $single_plan = ! ( count( $plans ) > 1 );
     <input type="hidden" name="listing_plan" value="<?php echo esc_attr( $_submit->fixed_plan_id ); ?>" />
     <input type="hidden" name="skip_plan_selection" value="1" />
 
-    <div class="wpbdp-msg tip"><?php _ex( 'Your plan\'s details:', 'submit', 'business-directory-plugin' ); ?></div>
+    <div class="wpbdp-msg tip"><?php esc_html_e( 'Your plan details:', 'business-directory-plugin' ); ?></div>
     <div class="wpbdp-plan-selection-wrapper" data-breakpoints='{"tiny": [0,410], "small": [410,560], "medium": [560,710], "large": [710,999999]}' data-breakpoints-class-prefix="wpbdp-size">
         <div class="wpbdp-plan-selection">
             <div class="wpbdp-plan-selection-list">
                 <?php
-                echo wpbdp_render(
+                wpbdp_render(
                     'plan-selection-plan', array(
-						'plan'         => wpbdp_get_fee_plan( $selected_plan ),
-						'categories'   => $selected_categories,
-						'display_only' => true,
-						'extra',
+                        'plan'         => wpbdp_get_fee_plan( $selected_plan ),
+                        'categories'   => $selected_categories,
+                        'display_only' => true,
+                        'echo'         => true,
+                        'extra',
                     )
                 );
-?>
+                ?>
             </div>
         </div>
     </div>
@@ -67,7 +68,7 @@ $single_plan = ! ( count( $plans ) > 1 );
                 wpbdp_render(
                     'plan-selection',
                     array(
-						'plans'    => $plans,
+                        'plans'    => $plans,
                         'selected' => ( ! empty( $selected_plan ) ? $selected_plan : 0 ),
                         'echo'     => true,
                     )
@@ -79,14 +80,14 @@ $single_plan = ! ( count( $plans ) > 1 );
             <?php
             wpbdp_render(
                 'plan-selection-plan', array(
-					'plan'         => wpbdp_get_fee_plan( $selected_plan ),
-                    'categories'   => $selected_categories,
+                    'plan'         => wpbdp_get_fee_plan( $selected_plan ),
+                    'categories'   => array(),
                     'display_only' => true,
                     'echo'         => true,
                     'extra',
                 )
             );
-?>
+            ?>
         </div>
         <?php endif; ?>
     </div>
