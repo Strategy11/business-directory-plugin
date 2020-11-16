6be21beb73e6a2f135e78587ec49e670a13f99ff
Steph <steph@strategy11.com>
2020-11-16 16:11:12 -0700

    fix php syntax error on php 5.6

--- templates/listings.tpl.php
+++ templates/listings.tpl.php
@@ -8,7 +8,7 @@
 wpbdp_the_listing_sort_options();
 ?>
 
-<div id="wpbdp-listings-list" class="listings wpbdp-listings-list list wpbdp-grid <?php echo esc_attr( apply_filters( 'wpbdp_listings_class', '', ) ); ?>">
+<div id="wpbdp-listings-list" class="listings wpbdp-listings-list list wpbdp-grid <?php echo esc_attr( apply_filters( 'wpbdp_listings_class', '' ) ); ?>">
     <?php if ( ! $query->have_posts() ): ?>
         <span class="no-listings">
             <?php esc_html_e( 'No listings found.', 'business-directory-plugin' ); ?>
