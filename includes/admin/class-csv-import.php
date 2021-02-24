a615dce83a5de6e4c991c5f1ae72a637c68b3698
Steph <steph@strategy11.com>
2021-02-23 17:06:31 -0700

    phpcs

--- includes/admin/class-csv-import.php
+++ includes/admin/class-csv-import.php
@@ -769,7 +769,7 @@ class WPBDP_CSV_Import {
                     }
 
                     if ( 'category' == $field->get_association() ) {
-                        $decoded_value  = html_entity_decode( $value );
+                        $decoded_value  = $value;
                         $csv_categories = array_map( 'trim', explode( $this->settings['category-separator'], $decoded_value ) );
 
                         foreach ( $csv_categories as $csv_category_ ) {
