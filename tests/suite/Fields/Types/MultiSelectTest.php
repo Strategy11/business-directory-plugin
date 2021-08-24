854f6806fabc1524244c84caf8ee239732261c33
Paul Kevin <paultitude@gmail.com>
2021-08-24 20:47:30 +0300

    Multiselector test

--- tests/suite/Fields/Types/MultiSelectTest.php
+++ tests/suite/Fields/Types/MultiSelectTest.php
@@ -12,6 +12,35 @@ class MultiSelectTest extends TestCase {
 
 
 	public function test_render_html_options() {
+		$context            = 'whatever';
+		$empty_option_label = 'Empty Option Label';
 
+		$options = array(
+			'first'  => 'First Select',
+			'second' => 'Second Select',
+		);
+
+		$field = Phake::mock( 'WPBDP_Form_Field' );
+
+		Phake::when( $field )->get_association->thenReturn( 'not-category' );
+		Phake::when( $field )->data( 'show_emprty_option' )->thenReturn( true );
+		Phake::when( $field )->data( 'empty_option_label', Phake::ignoreRemaining() )->thenReturn( $empty_option_label );
+		Phake::when( $field )->data( 'options', Phake::ignoreRemaining() )->thenReturn( $options );
+		Phake::when( $field )->set_data( 'options', $options );
+
+		$type = new WPBDP_FieldTypes_MultiSelect();
+
+		// Execution
+		// SEt selected items
+		$output = $type->render_field_inner( $field, array( 'first', 'second' ), $context );
+
+		// Verification
+		$this->assertContains( $empty_option_label, $output );
+		$this->assertContains( 'multiple="multiple"', $output );
+
+		// There should be 2 selected occurences
+		$selected = substr_count( $output, 'selected="selected"' );
+
+		$this->assertEquals( 2, $selected );
 	}
 }
