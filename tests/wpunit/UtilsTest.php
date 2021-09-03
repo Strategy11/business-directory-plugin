a43465049e7a8eace9295c85608f5b59010a501a
Paul Kevin <paultitude@gmail.com>
2021-09-03 17:37:02 +0300

    Fix composer requirements, xml configuration and migrate first class to wp unit

---
+++ tests/wpunit/UtilsTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package WPBDP\Tests\Plugin
+ */
+
+use WPBDP_Utils;
+
+/**
+ * Unit tests for Utils class.
+ */
+class UtilsTest extends \Codeception\Test\Unit {
+		
+	/**
+     * @var \WpunitTester
+     */
+    protected $tester;
+
+
+	/**
+	 * @since 5.2.1
+	 */
+	public function testSortByProperty() {
+		$data = array(
+			'a' => array(
+				'order' => 5,
+			),
+			'b' => array(
+				'order' => 2,
+			),
+			'c' => array(
+				'order' => 10,
+			),
+		);
+
+		WPBDP_Utils::sort_by_property( $data, 'order' );
+		$this->tester->wantToTest("Utils Sort Order");
+		$this->assertEquals( array( 'b', 'a', 'c' ), array_keys( $data ) );
+	}
+}
