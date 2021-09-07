11ce8575fec0067b5f59602554a5bcc8e0787d48
Paul Kevin <paultitude@gmail.com>
2021-09-07 13:42:08 +0300

    Working on directory test. Run into an issue where field names are not consistent

--- tests/acceptance/directory/DirectoryCrudCest.php
+++ tests/acceptance/directory/DirectoryCrudCest.php
@@ -8,6 +8,7 @@ class DirectoryCrudCept {
     public function _before( AcceptanceTester $I ) {
 		$I->wantTo('log in to site');
 		$I->loginAsAdmin();
+		$I->amOnPage( '/wp-admin/edit.php?post_type=wpbdp_listing' );
     }
 
     public function _after( AcceptanceTester $I ) {
@@ -15,7 +16,9 @@ class DirectoryCrudCept {
     }
 
     public function createDirectory( AcceptanceTester $I ) {
-
+		$I->click( 'Add New Listing' );
+		$I->fillField( 'post_title', 'Sample Test Listing' );
+		//Fields do not have proper names. Very hard to determine
     }
 
     public function editDirectory( AcceptanceTester $I ) {
