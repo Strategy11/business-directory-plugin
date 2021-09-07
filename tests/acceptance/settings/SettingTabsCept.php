ab51015b0601f776b70d09d08660f292331fcd6a
Paul Kevin <paultitude@gmail.com>
2021-09-07 18:48:50 +0300

    Acceptance test to check features are avialble on free version settings tabs

--- tests/acceptance/settings/SettingTabsCept.php
+++ tests/acceptance/settings/SettingTabsCept.php
@@ -8,6 +8,7 @@ class SettingTabsCest {
     public function _before( AcceptanceTester $I ) {
 		$I->wantTo('log in to site');
 		$I->loginAsAdmin();
+		$I->amOnPage( '/wp-admin/admin.php?page=wpbdp_settings' );
     }
 
     public function _after( AcceptanceTester $I ) {
@@ -15,8 +16,6 @@ class SettingTabsCest {
 
     // tests
     public function tryToTestSettings( AcceptanceTester $I ) {
-		
-		$I->amOnPage( '/wp-admin/admin.php?page=wpbdp_settings' );
         $I->see( 'Directory Settings', 'h1' );
 		$I->click( 'Listings' );
 		$I->seeCheckboxIsChecked('#wpbdp-settings-listing-renewal');
@@ -30,4 +29,29 @@ class SettingTabsCest {
 		$I->see('Settings saved.');
 		$I->dontSeeCheckboxIsChecked('#listing-link-in-new-tab');
     }
+
+	public function tryToTestSettingsSearchTab( AcceptanceTester $I ) {
+		$I->click('Searching');
+		$I->see( 'Display advanced search form', 'label' );
+	}
+
+	public function tryToTestSettingsCateoryTab( AcceptanceTester $I ) {
+		$I->click('Categories');
+		$I->see( 'Show listings under categories on main page?', 'label' );
+	}
+
+	public function tryToTestSettingsContactTab( AcceptanceTester $I ) {
+		$I->click('Contact Form');
+		$I->see( 'Include listing contact form on listing pages?', 'label' );
+	}
+
+	public function tryToTestSettingsButtonsTab( AcceptanceTester $I ) {
+		$I->click('Buttons');
+		$I->see( 'Include button to report listings?', 'label' );
+	}
+
+	public function tryToTestSettingsSortingTab( AcceptanceTester $I ) {
+		$I->click('Sorting');
+		$I->see( 'Order directory listings by', 'label' );
+	}
 }
