f7348f8b69e57b698a4061c87074de873bf5edad
Paul Kevin <paultitude@gmail.com>
2021-09-09 13:05:09 +0300

    Remove configuration

--- templates/admin/form-fields-label-icon.tpl.php
+++ templates/admin/form-fields-label-icon.tpl.php
@@ -8,7 +8,7 @@
 ?>
 
 <?php // TODO: Remove style attribute. ?>
-<select id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $name ); ?>" class="<?php echo esc_attr( implode( ' ', $class ) ); ?>" data-configuration="<?php echo esc_attr( wp_json_encode( $configuration ) ); ?>" data-nonce="<?php echo esc_attr( $nonce ); ?>" style="width: 100%">
+<select id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $name ); ?>" class="<?php echo esc_attr( implode( ' ', $class ) ); ?>" style="width: 100%">
     <?php if ( $default ) : ?>
         <option value=""><?php echo esc_html( $default ); ?></option>
     <?php endif; ?>
