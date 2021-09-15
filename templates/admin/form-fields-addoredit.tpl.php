1c5eae09baf995cd965e319296b27185ae6a0ba9
Paul Kevin <paultitude@gmail.com>
2021-09-15 13:40:27 +0300

    Add action for fields

--- templates/admin/form-fields-addoredit.tpl.php
+++ templates/admin/form-fields-addoredit.tpl.php
@@ -264,18 +264,24 @@ wpbdp_admin_notices();
                     </label>
                 </td>
             </tr>
-			<tr class="<?php echo in_array( 'nolabel', $hidden_fields, true ) ? 'wpbdp-hidden' : ''; ?>">
-                <th scope="row">
-                    <label> <?php _ex( 'Hide this field\'s label?', 'form-fields admin', 'business-directory-plugin' ); ?></label>
-                </th>
-                <td>
-                    <label>
-                        <input name="field[display_flags][]"
-                               value="nolabel"
-                               type="checkbox" <?php echo $field->has_display_flag( 'nolabel' ) ? 'checked="checked"' : ''; ?>/> <?php _ex( 'Hide this field\'s label when displaying it.', 'form-fields admin', 'business-directory-plugin' ); ?>
-                    </label>
-                </td>
-            </tr>
+            <?php
+			if ( has_action( 'wpbdp_admin_listing_field_section_visibility' ) ) {
+				do_action( 'wpbdp_admin_listing_field_section_visibility', $field, $hidden_fields );
+			} else {
+				?>
+				<tr class="<?php echo in_array( 'nolabel', $hidden_fields, true ) ? 'wpbdp-hidden' : ''; ?>">
+					<th scope="row">
+						<label> <?php _ex( 'Hide this field\'s label?', 'form-fields admin', 'business-directory-plugin' ); ?></label>
+					</th>
+					<td>
+						<label>
+							<input name="field[display_flags][]"
+								value="nolabel"
+								type="checkbox" <?php echo $field->has_display_flag( 'nolabel' ) ? 'checked="checked"' : ''; ?>/> <?php _ex( 'Hide this field\'s label when displaying it.', 'form-fields admin', 'business-directory-plugin' ); ?>
+						</label>
+					</td>
+				</tr>
+			<?php } ?>
     </table>
 
     <!-- display options -->
