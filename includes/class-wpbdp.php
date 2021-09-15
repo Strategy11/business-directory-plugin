571793c65980be9259bb0705c837c31ef891288a
Steph <steph@strategy11.com>
2021-09-15 15:32:41 -0600

    prepare for release

--- includes/class-wpbdp.php
+++ includes/class-wpbdp.php
@@ -23,7 +23,7 @@ final class WPBDP {
     }
 
     private function setup_constants() {
-        define( 'WPBDP_VERSION', '5.13.3' );
+        define( 'WPBDP_VERSION', '5.13.4' );
 
         define( 'WPBDP_PATH', wp_normalize_path( plugin_dir_path( WPBDP_PLUGIN_FILE ) ) );
         define( 'WPBDP_INC', trailingslashit( WPBDP_PATH . 'includes' ) );
