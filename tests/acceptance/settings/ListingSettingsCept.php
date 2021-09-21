deda5aa19492023a31db46be41dd97b2d81948b0
Paul Kevin <paultitude@gmail.com>
2021-09-21 17:11:36 +0300

    Text field test and fix some formatting

--- tests/acceptance/settings/ListingSettingsCept.php
+++ tests/acceptance/settings/ListingSettingsCept.php
@@ -5,18 +5,18 @@
  */
 class ListingSettingsCest {
 	
-    public function _before( AcceptanceTester $I ) {
+	public function _before( AcceptanceTester $I ) {
 		$I->wantTo('log in to site');
 		$I->loginAsAdmin();
-    }
+	}
 
-    public function _after( AcceptanceTester $I ) {
-    }
+	public function _after( AcceptanceTester $I ) {
+	}
 
     // tests
-    public function tryToTestListingSettings( AcceptanceTester $I ) {
+	public function tryToTestListingSettings( AcceptanceTester $I ) {
 		
 		$I->amOnPage( '/wp-admin/admin.php?page=wpbdp_settings&tab=listings' );
-        $I->see( 'Directory Settings', 'h1' );
-    }
+		$I->see( 'Directory Settings', 'h1' );
+	}
 }
--- tests/acceptance/settings/PaymentSettingsCept.php
+++ tests/acceptance/settings/PaymentSettingsCept.php
@@ -5,24 +5,24 @@
  */
 class PaymentSettingsCest {
 	
-    public function _before( AcceptanceTester $I ) {
+	public function _before( AcceptanceTester $I ) {
 		$I->wantTo('log in to site');
 		$I->loginAsAdmin();
     }
 
-    public function _after( AcceptanceTester $I ) {
+	public function _after( AcceptanceTester $I ) {
     }
 
     // tests
-    public function tryToTestPaymentSettings( AcceptanceTester $I ) {
+	public function tryToTestPaymentSettings( AcceptanceTester $I ) {
 		
 		$I->amOnPage( '/wp-admin/admin.php?page=wpbdp_settings&tab=payment' );
-        $I->see( 'Directory Settings', 'h1' );
-        $I->dontSeeCheckboxIsChecked('#payments-on');
-        $I->checkOption('#payments-on');
-        $I->see( 'Put payment gateways in test mode?', 'label' );
-        $I->click('Save Changes');
-        $I->reloadPage();
-        $I->seeCheckboxIsChecked('#payments-on');
+		$I->see( 'Directory Settings', 'h1' );
+		$I->dontSeeCheckboxIsChecked('#payments-on');
+		$I->checkOption('#payments-on');
+		$I->see( 'Put payment gateways in test mode?', 'label' );
+		$I->click('Save Changes');
+		$I->reloadPage();
+		$I->seeCheckboxIsChecked('#payments-on');
     }
 }
