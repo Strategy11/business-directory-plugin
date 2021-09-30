0f9db7ce89ec036dc8f8281d1f5b6d5a04ee011f
Paul Kevin <paultitude@gmail.com>
2021-09-30 13:22:57 +0300

    Simplify file checks

--- includes/class-dispatcher.php
+++ includes/class-dispatcher.php
@@ -139,7 +139,6 @@ class WPBDP__Dispatcher {
                             'class-' . $class_view_name . '.php',
                         );
         $find = array( '.php', 'class' );
-        $replace = array( '', '' );
         foreach ( $this->get_view_locations() as $dir ) {
             foreach ( $filenames as $f ) {
                 $path = wp_normalize_path( WPBDP_FS::join( $dir, $f ) );
@@ -149,8 +148,7 @@ class WPBDP__Dispatcher {
 
                 // Views that start with class- in the file name. Addons may not have this hence the condition.
                 if ( strpos( $f, 'class' ) !== false ) {
-                    $f = str_replace( '-', '_', $f );
-                    $classname = 'WPBDP__Views_' . implode( '_', array_map( 'ucfirst', explode( '_', str_replace( $find, $replace, $f ) ) ) );
+                    $classname = 'WPBDP__Views_' . implode( '_', array_map( 'ucfirst', explode( '-', str_replace( $find, '', $f ) ) ) );
                 } else {
                     $classname = 'WPBDP__Views__' . implode( '_', array_map( 'ucfirst', explode( '_', str_replace( '.php', '', $f ) ) ) );
                 }
