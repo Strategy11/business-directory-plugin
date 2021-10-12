6ff5484a82e7977a0337cf0fff2440e61baab6d9
Steph <steph@strategy11.com>
2021-10-12 09:21:49 -0600

    move esc to last

--- includes/widgets/class-listings-widget.php
+++ includes/widgets/class-listings-widget.php
@@ -186,12 +186,12 @@ class WPBDP_Listings_Widget extends WP_Widget {
 		if ( $args['show_images'] ) {
 			$img_size = 'medium';
 			$img_id = $listing->get_thumbnail_id();
-			$permalink = esc_url( $listing->get_permalink() );
+			$permalink = $listing->get_permalink();
 			if ( $img_id ) {
-				$image_link = '<a href="' . $permalink . '">' . wp_kses_post( wp_get_attachment_image( $img_id, $img_size, false, array( 'class' => 'listing-image' ) ) ). '</a>';
+				$image_link = '<a href="' . esc_url( $permalink ) . '">' . wp_kses_post( wp_get_attachment_image( $img_id, $img_size, false, array( 'class' => 'listing-image' ) ) ). '</a>';
 			} elseif ( $args['default_image'] ) {
 				$class      = "attachment-$img_size size-$img_size listing-image";
-				$image_link = '<a href="' . $permalink . '"><img src="' . wp_kses_post( $args['coming_soon_image'] ) . '" class="' . $class . '" /></a>';
+				$image_link = '<a href="' . esc_url( $permalink ) . '"><img src="' . wp_kses_post( $args['coming_soon_image'] ) . '" class="' . esc_attr( $class ) . '" /></a>';
 			} else {
 				// For image spacing.
 				$image_link = '<span></span>';
