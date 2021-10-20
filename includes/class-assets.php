dcf47fd9c38955aa3667e201f21d72dcc2f5009f
Paul Kevin <paultitude@gmail.com>
2021-10-20 21:25:06 +0300

    `force` param return

--- includes/class-assets.php
+++ includes/class-assets.php
@@ -258,9 +258,6 @@ class WPBDP__Assets {
 	public function enqueue_admin_scripts( $force = false ) {
 		global $wpbdp;
 
-        if ( is_string( $force ) ) {
-            $force = false;
-        }
 		if ( ! $force && ! $wpbdp->is_bd_page() ) {
 			return;
 		}
