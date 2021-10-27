8b7e5fcc5df1c4f4ecc7054551e991983f76a382
Paul Kevin <paultitude@gmail.com>
2021-10-27 14:00:14 +0300

    Version number change of what was previously changed

--- includes/admin/class-admin-listings.php
+++ includes/admin/class-admin-listings.php
@@ -1,3 +1,3 @@
 <?php
-_deprecated_file( basename( __FILE__ ), '5.15', null, 'This file can be found in includes/admin/controllers/class-admin-listings.php' );
+_deprecated_file( basename( __FILE__ ), '5.14', null, 'This file can be found in includes/admin/controllers/class-admin-listings.php' );
 require_once WPBDP_INC . 'admin/controllers/class-admin-listings.php';
