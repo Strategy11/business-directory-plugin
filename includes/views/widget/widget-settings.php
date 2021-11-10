64e52896d528b0fb2666b763088f8bc4e016a8fd
Steph <steph@strategy11.com>
2021-11-10 13:47:15 -0700

    a bit more readability

--- includes/views/widget/widget-settings.php
+++ includes/views/widget/widget-settings.php
@@ -5,11 +5,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <p>
-	<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'business-directory-plugin' ); ?></label>
+	<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">
+		<?php esc_html_e( 'Title:', 'business-directory-plugin' ); ?>
+	</label>
 	<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $this->get_field_value( $instance, 'title' ) ); ?>" />
 </p>
 <p>
-	<label for="<?php echo esc_attr( $this->get_field_id( 'number_of_listings' ) ); ?>"><?php esc_html_e( 'Number of listings to display:', 'business-directory-plugin' ); ?></label>
+	<label for="<?php echo esc_attr( $this->get_field_id( 'number_of_listings' ) ); ?>">
+		<?php esc_html_e( 'Number of listings to display:', 'business-directory-plugin' ); ?>
+	</label>
 	<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'number_of_listings' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number_of_listings' ) ); ?>" type="text" value="<?php echo esc_attr( $this->get_field_value( $instance, 'number_of_listings' ) ); ?>" />
 </p>
 <?php $this->_form( $instance ); ?>
@@ -19,7 +23,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php $class_name = $this->get_field_value( $instance, 'show_images' ) ? '' : 'hidden'; ?>
 	<h4><?php esc_html_e( 'Thumbnails', 'business-directory-plugin' ); ?></h4>
 	<p>
-		<input id="<?php echo esc_attr( $this->get_field_id( 'show_images' ) ); ?>" class="wpbdp-toggle-images" name="<?php echo esc_attr( $this->get_field_name( 'show_images' ) ); ?>" type="checkbox" value="1" <?php checked( $this->get_field_value( $instance, 'show_images' ), true ); ?> /> <label for="<?php echo esc_attr( $this->get_field_id( 'show_images' ) ); ?>"><?php esc_html_e( 'Show thumbnails', 'business-directory-plugin' ); ?></label>
+		<input id="<?php echo esc_attr( $this->get_field_id( 'show_images' ) ); ?>" class="wpbdp-toggle-images" name="<?php echo esc_attr( $this->get_field_name( 'show_images' ) ); ?>" type="checkbox" value="1" <?php checked( $this->get_field_value( $instance, 'show_images' ), true ); ?> />
+		<label for="<?php echo esc_attr( $this->get_field_id( 'show_images' ) ); ?>">
+			<?php esc_html_e( 'Show thumbnails', 'business-directory-plugin' ); ?>
+		</label>
 	</p>
 	<div class="thumbnail-width-config <?php echo esc_attr( $class_name ); ?>">
 		<p>
@@ -46,22 +53,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'thumbnail_desktop' ) ); ?>">
 				<?php esc_html_e( 'Thumbnail Position (Desktop):', 'business-directory-plugin' ); ?>
-			</label><br/>
+			</label>
+			<br/>
 			<select name="<?php echo esc_attr( $this->get_field_name( 'thumbnail_desktop' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'thumbnail_desktop' ) ); ?>">
-				<option value="left" <?php selected( $this->get_field_value( $instance, 'thumbnail_desktop' ), 'left' ); ?>><?php esc_html_e( 'Left', 'business-directory-plugin' ); ?></option>
-				<option value="right" <?php selected( $this->get_field_value( $instance, 'thumbnail_desktop' ), 'right' ); ?>><?php esc_html_e( 'Right', 'business-directory-plugin' ); ?></option>
-				<option value="above" <?php selected( $this->get_field_value( $instance, 'thumbnail_desktop' ), 'above' ); ?>><?php esc_html_e( 'Top', 'business-directory-plugin' ); ?></option>
+				<option value="left" <?php selected( $this->get_field_value( $instance, 'thumbnail_desktop' ), 'left' ); ?>>
+					<?php esc_html_e( 'Left', 'business-directory-plugin' ); ?>
+				</option>
+				<option value="right" <?php selected( $this->get_field_value( $instance, 'thumbnail_desktop' ), 'right' ); ?>>
+					<?php esc_html_e( 'Right', 'business-directory-plugin' ); ?>
+				</option>
+				<option value="above" <?php selected( $this->get_field_value( $instance, 'thumbnail_desktop' ), 'above' ); ?>>
+					<?php esc_html_e( 'Top', 'business-directory-plugin' ); ?>
+				</option>
 			</select>
 		</p>
 
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'thumbnail_mobile' ) ); ?>">
 				<?php esc_html_e( 'Thumbnail Position (Mobile):', 'business-directory-plugin' ); ?>
-			</label><br/>
+			</label>
+			<br/>
 			<select name="<?php echo esc_attr( $this->get_field_name( 'thumbnail_mobile' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'thumbnail_mobile' ) ); ?>">
-				<option value="left" <?php selected( $this->get_field_value( $instance, 'thumbnail_mobile' ), 'left' ); ?>><?php esc_html_e( 'Left', 'business-directory-plugin' ); ?></option>
-				<option value="right" <?php selected( $this->get_field_value( $instance, 'thumbnail_mobile' ), 'right' ); ?>><?php esc_html_e( 'Right', 'business-directory-plugin' ); ?></option>
-				<option value="above" <?php selected( $this->get_field_value( $instance, 'thumbnail_mobile' ), 'above' ); ?>><?php esc_html_e( 'Top', 'business-directory-plugin' ); ?></option>
+				<option value="left" <?php selected( $this->get_field_value( $instance, 'thumbnail_mobile' ), 'left' ); ?>>
+					<?php esc_html_e( 'Left', 'business-directory-plugin' ); ?>
+				</option>
+				<option value="right" <?php selected( $this->get_field_value( $instance, 'thumbnail_mobile' ), 'right' ); ?>>
+					<?php esc_html_e( 'Right', 'business-directory-plugin' ); ?>
+				</option>
+				<option value="above" <?php selected( $this->get_field_value( $instance, 'thumbnail_mobile' ), 'above' ); ?>>
+					<?php esc_html_e( 'Top', 'business-directory-plugin' ); ?>
+				</option>
 			</select>
 		</p>
 	</div>
