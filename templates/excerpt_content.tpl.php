6bd595f3ae3e0ceb55ebba5e8d576710699eefd2
Paul Kevin <paultitude@gmail.com>
2021-11-22 21:35:47 +0300

    Move excerpt repetitive template to single file and include

--- templates/excerpt_content.tpl.php
+++ templates/excerpt_content.tpl.php
@@ -1,16 +1,2 @@
-<?php if ( $images->thumbnail ) : ?>
-    <?php echo $images->thumbnail->html; ?>
-<?php endif; ?>
-
-<div class="listing-details">
-    <?php foreach ( $fields->not( 'social' ) as $field ) : ?>
-        <?php echo $field->html; ?>
-    <?php endforeach; ?>
-
-    <?php
-    $social = $fields->filter( 'social' );
-    ?>
-	<?php if ( $social && $social->html ) : ?>
-    <div class="social-fields cf"><?php echo $social->html; ?></div>
-    <?php endif; ?>
-</div>
+<?php
+wpbdp_x_part( 'parts/excerpt-details' );
--- themes/default/templates/excerpt_content.tpl.php
+++ themes/default/templates/excerpt_content.tpl.php
@@ -12,20 +12,5 @@
 </div>
 
 <div class="excerpt-content">
-	<?php if ( $images->thumbnail ): ?>
-		<?php echo $images->thumbnail->html; ?>
-	<?php endif; ?>
-
-	<div class="listing-details">
-		<?php foreach ( $fields->not( 'social' ) as $field ) : ?>
-			<?php echo $field->html; ?>
-		<?php endforeach; ?>
-
-		<?php
-			$social = $fields->filter( 'social' );
-		?>
-		<?php if ( $social && $social->html ) : ?>
-			<div class="social-fields cf"><?php echo $social->html; ?></div>
-		<?php endif; ?>
-	</div>
+	<?php wpbdp_x_part( 'parts/excerpt-details' ); ?>
 </div>
