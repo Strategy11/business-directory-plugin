27ee14078257a48e22d4aeb23d174dc9313ab177
Paul Kevin <paultitude@gmail.com>
2021-11-24 14:57:54 +0300

    Revert parts change

--- templates/excerpt_content.tpl.php
+++ templates/excerpt_content.tpl.php
@@ -1,2 +1,16 @@
-<?php
-wpbdp_x_part( 'parts/excerpt-details' );
+<?php if ( $images->thumbnail ) : ?>
+    <?php echo $images->thumbnail->html; ?>
+<?php endif; ?>
+
+<div class="listing-details">
+    <?php foreach ( $fields->not( 'social' ) as $field ) : ?>
+        <?php echo $field->html; ?>
+    <?php endforeach; ?>
+
+    <?php
+    $social = $fields->filter( 'social' );
+    ?>
+	<?php if ( $social && $social->html ) : ?>
+    <div class="social-fields cf"><?php echo $social->html; ?></div>
+    <?php endif; ?>
+</div>
