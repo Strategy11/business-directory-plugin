1b5e8c08b883a627d9daf6c655ab364af6fdfe2f
Paul Kevin <paultitude@gmail.com>
2021-12-09 11:08:21 +0300

    Fix slash and work on listings settings

---
+++ tests/wpunit/Settings/ListingSettingsTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Includes tests for the Settings
+ */
+
+namespace Settings;
+
+use WPBDP\Tests\WPUnitTestCase;
+
+/**
+ * Tests for the Listings settings
+ */
+class ListingSettingsTest extends WPUnitTestCase {
+
+	/**
+	 * @var \WpunitTester
+	 */
+	protected $tester;
+
+	public function testListingSettings() {
+
+	}
+}
