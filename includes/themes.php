b75c85d80a698faf0bad541fa07297a114dd2f19
Paul Kevin <paultitude@gmail.com>
2021-12-22 12:58:43 +0300

    Tabs

--- includes/themes.php
+++ includes/themes.php
@@ -613,7 +613,7 @@ class WPBDP_Themes {
 
     private function render_template_file( $template_id, $path, $vars = array() ) {
         if ( ! $path ) {
-            wpbdp_log( 'Invalid template path for template: "' . $template_id . '"' );
+			wpbdp_log( 'Invalid template path for template: "' . $template_id . '"' );
 			return '';
         }
 
