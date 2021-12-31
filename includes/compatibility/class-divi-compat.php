46dfa128ab5ff25d422df846d4df070d85e45b51
Paul Kevin <paultitude@gmail.com>
2021-12-31 09:02:45 +0300

    Only check the param

--- includes/compatibility/class-divi-compat.php
+++ includes/compatibility/class-divi-compat.php
@@ -13,22 +13,17 @@ class WPBDP_Divi_Compat {
 	/**
 	 * Check if Divi builder is being used.
 	 *
-	 * @param int $post_id The post id. If not passed, defaults to the current post id.
-	 *
 	 * @since x.x
 	 *
 	 * @return bool
 	 */
-	public static function divi_builder_is_active( $post_id = false ) {
+	public static function divi_builder_is_active() {
 		if ( ! class_exists( 'ET_Builder_Plugin' ) ) {
 			return false;
 		}
 		if ( isset( $_GET['et_fb'] ) ) {
 			return true;
 		}
-		if ( ! $post_id ) {
-			$post_id = get_the_ID();
-		}
-		return ( 'on' === get_post_meta( $post_id, '_et_pb_use_builder', true ) );
+		return false;
 	}
 }
