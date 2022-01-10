49eb42f08c13779fecf859d267b13efbda356a3c
Paul Kevin <paultitude@gmail.com>
2022-01-10 19:01:34 +0300

    Form fields buttons
    Update button code to match new button format

--- templates/admin/form-fields.tpl.php
+++ templates/admin/form-fields.tpl.php
@@ -1,12 +1,17 @@
 <?php
     $buttons = array(
-        _x( 'Add New Form Field', 'form-fields admin', 'business-directory-plugin' ) =>
-            wp_nonce_url(
-				admin_url( 'admin.php?page=wpbdp_admin_formfields&action=addfield' ),
-				'editfield'
-            ),
-        __( 'Preview Form', 'business-directory-plugin' )      => admin_url( 'admin.php?page=wpbdp_admin_formfields&action=previewform' ),
-        __( 'Manage Theme Tags', 'business-directory-plugin' ) => admin_url( 'admin.php?page=wpbdp_admin_formfields&action=updatetags' ),
+		'addfield'    => array(
+			'label' => __( 'Add New Form Field', 'business-directory-plugin' ),
+			'url'   => wp_nonce_url( admin_url( 'admin.php?page=wpbdp_admin_formfields&action=addfield' ), 'editfield' ),
+		),
+		'previewform' => array(
+			'label' => __( 'Preview Form', 'business-directory-plugin' ),
+			'url'   => admin_url( 'admin.php?page=wpbdp_admin_formfields&action=previewform' ),
+		),
+		'updatetags'  => array(
+			'label' => __( 'Manage Theme Tags', 'business-directory-plugin' ),
+			'url'   => admin_url( 'admin.php?page=wpbdp_admin_formfields&action=updatetags' ),
+		),
     );
 
 	WPBDP_Admin_Pages::show_tabs(
