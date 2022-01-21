9cd3d5fbbbae0f309d3bfada49f0b9d5a87ac84e
Paul Kevin <paultitude@gmail.com>
2022-01-21 17:11:07 +0300

    Item check
    Return check for name and trim white spaces from beginning or end

--- includes/licensing.php
+++ includes/licensing.php
@@ -995,7 +995,7 @@ class WPBDP_Licensing {
             $item_key = $item['item_type'] . '-' . $item['id'];
 
             foreach ( $body as $item_information ) {
-				if ( $item_information->slug !== $item['id'] ) {
+				if ( trim( $item_information->name ) !== trim( $item['name'] ) ) {
 					continue;
 				}
 
