9af05dac9b22fe04308fda7976fca249c7ebd927
Paul Kevin <paultitude@gmail.com>
2022-02-17 11:56:37 +0300

    Other text updates

--- includes/admin/controllers/class-admin-fees.php
+++ includes/admin/controllers/class-admin-fees.php
@@ -112,11 +112,11 @@ class WPBDP__Admin__Fees extends WPBDP__Admin__Controller {
 
 		if ( ! is_wp_error( $result ) ) {
 			if ( 'insert' === $mode ) {
-				wpbdp_admin_message( __( 'Fee plan added.', 'business-directory-plugin' ) );
+				wpbdp_admin_message( __( 'Plan added.', 'business-directory-plugin' ) );
 			} elseif ( $images_changed ) {
 				$this->show_update_listing_msg( $fee );
 			} else {
-				wpbdp_admin_message( __( 'Fee plan updated.', 'business-directory-plugin' ) );
+				wpbdp_admin_message( __( 'Plan updated.', 'business-directory-plugin' ) );
 			}
 		} else {
 			foreach ( $result->get_error_messages() as $msg ) {
@@ -131,7 +131,7 @@ class WPBDP__Admin__Fees extends WPBDP__Admin__Controller {
 	 * @since 5.15.3
 	 */
 	private function show_update_listing_msg( $fee ) {
-		$message = __( 'Fee plan updated.', 'business-directory-plugin' );
+		$message = __( 'Plan updated.', 'business-directory-plugin' );
 
 		$total_listings = $fee->count_listings();
 		if ( ! $total_listings ) {
@@ -173,11 +173,11 @@ class WPBDP__Admin__Fees extends WPBDP__Admin__Controller {
 		$fee     = wpbdp_get_fee_plan( $plan_id );
 		$res     = new WPBDP_Ajax_Response();
 		if ( ! $fee ) {
-			$res->send_error( __( 'Fee plan not found.', 'business-directory-plugin' ) );
+			$res->send_error( __( 'Plan not found.', 'business-directory-plugin' ) );
 		}
 
 		$this->update_listing_images( $fee );
-		$res->set_message( __( 'Fee plan listings updated.', 'business-directory-plugin' ) );
+		$res->set_message( __( 'Plan listings updated.', 'business-directory-plugin' ) );
 		$res->send();
 	}
 
