d9442a9706773b6fb7b43c8e12706805caec33eb
Paul Kevin <paultitude@gmail.com>
2022-02-18 10:48:34 +0300

    Fix css to hide title

--- themes/default/templates/excerpt_content.tpl.php
+++ themes/default/templates/excerpt_content.tpl.php
@@ -6,6 +6,10 @@
  */
 
 ?>
+<div class="listing-title">
+    <?php echo $fields->t_title->value; ?>
+</div>
+
 <div class="excerpt-content wpbdp-hide-title">
 	<?php include WPBDP_PATH . 'templates/excerpt_content.tpl.php'; ?>
 </div>
