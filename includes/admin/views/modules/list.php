44e7db11981d445be1737a727a310e7fa7634446
Steph <steph@strategy11.com>
2022-03-02 13:46:49 -0700

    prepare the modules and themes pages

--- includes/admin/views/modules/list.php
+++ includes/admin/views/modules/list.php
@@ -30,51 +30,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 			}
 		?>
 			<div class="wpbdp-card plugin-card-<?php echo esc_attr( $slug ); ?> wpbdp-no-thumb wpbdp-addon-<?php echo esc_attr( $addon['status']['type'] ); ?>">
-				<div class="plugin-card-top">
-					<?php if ( strtotime( $addon['released'] ) > strtotime( '-90 days' ) ) { ?>
+					<?php if ( strtotime( $addon['released'] ) > strtotime( '-90 days' ) ) : ?>
 						<div class="wpbdp-ribbon">
-							<span>New</span>
+							<span><?php esc_attr_e( 'New', 'business-directory-plugin' ); ?></span>
 						</div>
-					<?php } ?>
-					<h2>
-						<?php echo esc_html( $addon['title'] ); ?>
-					</h2>
-					<p>
+					<?php endif; ?>
+					<div class="wpbdp-grid">
+						<div class="wpbdp8">
+							<div class="wpbdp-grid">
+								<span class="wpbdp3 wpbdp-card-module-icon wpbdp-admin-module-icon ">
+									<img src="<?php echo esc_attr( $addon['icons']['1x'] )?>" alt="" />
+								</span>
+								<div class="wpbdp9">
+									<h2 class="wpbdp-plugin-card-title">
+										<?php echo esc_html( str_replace( ' Module', '', $addon['title'] ) ); ?>
+									</h2>
+									<p class="wpbdp-addon-status">
+										<?php echo esc_html( $addon['status']['label'] ); ?>
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="wpbdp4 wpbdp-right">
+							<?php
+							$passing = array(
+								'addon'         => $addon,
+								'license_type'  => ! empty( $license_type ) ? $license_type : false,
+								'plan_required' => 'plan_required',
+								'upgrade_link'  => $pricing,
+							);
+							WPBDP_Show_Modules::show_conditional_action_button( $passing );
+							?>
+						</div>
+					</div>
+
+					<div class="wpbdp-plugin-card-details">
 						<?php echo esc_html( $addon['excerpt'] ); ?>
 						<?php $show_docs = isset( $addon['docs'] ) && ! empty( $addon['docs'] ) && $addon['installed']; ?>
 						<?php if ( $show_docs ) { ?>
-							<br/><a href="<?php echo esc_url( $addon['docs'] ); ?>" target="_blank" aria-label="<?php esc_attr_e( 'View Docs', 'business-directory-plugin' ); ?>">
-								<?php esc_html_e( 'View Docs', 'business-directory-plugin' ); ?>
-							</a>
+							<div class="wpbdp-plugin-card-docs">
+								<a href="<?php echo esc_url( $addon['docs'] ); ?>" target="_blank" aria-label="<?php esc_attr_e( 'View Docs', 'business-directory-plugin' ); ?>">
+									<?php esc_html_e( 'View Docs', 'business-directory-plugin' ); ?>
+								</a>
+							</div>
 						<?php } ?>
-					</p>
+					</div>
 					<?php
 					if ( ! $show_docs ) {
 						// $plan_required = FrmFormsHelper::get_plan_required( $addon );
 						// FrmFormsHelper::show_plan_required( $plan_required, $pricing . '&utm_content=' . $addon['slug'] );
 					}
 					?>
-				</div>
-				<div class="plugin-card-bottom">
-					<span class="addon-status">
-						<?php
-						printf(
-							/* translators: %s: Status name */
-							esc_html__( 'Status: %s', 'business-directory-plugin' ),
-							'<span class="addon-status-label">' . esc_html( $addon['status']['label'] ) . '</span>'
-						);
-						?>
-					</span>
-					<?php
-					$passing = array(
-						'addon'         => $addon,
-						'license_type'  => ! empty( $license_type ) ? $license_type : false,
-						'plan_required' => 'plan_required',
-						'upgrade_link'  => $pricing,
-					);
-					WPBDP_Show_Modules::show_conditional_action_button( $passing );
-					?>
-				</div>
 			</div>
 		<?php } ?>
 	</div>
