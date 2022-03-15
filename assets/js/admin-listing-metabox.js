155621fb36d19763f0aac219d1b7ab1123ea6428
Paul Kevin <paultitude@gmail.com>
2022-03-15 15:31:39 +0300

    Fix `Today` button click event

--- assets/js/admin-listing-metabox.js
+++ assets/js/admin-listing-metabox.js
@@ -20,6 +20,27 @@ jQuery( function( $ ) {
         }, 1 );
     };
 
+    $.datepicker._gotoToday = function(id) {
+        var target = $(id);
+        var inst = this._getInst(target[0]);
+        if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
+                inst.selectedDay = inst.currentDay;
+                inst.drawMonth = inst.selectedMonth = inst.currentMonth;
+                inst.drawYear = inst.selectedYear = inst.currentYear;
+        }
+        else {
+                var date = new Date();
+                inst.selectedDay = date.getDate();
+                inst.drawMonth = inst.selectedMonth = date.getMonth();
+                inst.drawYear = inst.selectedYear = date.getFullYear();
+                // the below two lines are new
+                this._setDateDatepicker(target, date);
+                this._selectDate(id, this._getDateDatepicker(target));
+        }
+        this._notifyChange(inst);
+        this._adjustDate(target);
+    }
+
     $( '#wpbdp-listing-metabox-plan-info input[name="listing_plan[expiration_date]"]' ).datepicker({
         dateFormat: 'yy-mm-dd',
         showButtonPanel: true,
