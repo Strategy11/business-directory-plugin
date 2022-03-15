3f98ded11fb701dbd9c5f9a585e3a7357e11092d
Paul Kevin <paultitude@gmail.com>
2022-03-15 16:56:51 +0300

    Date picker CSS

--- assets/js/wpbdp.js
+++ assets/js/wpbdp.js
@@ -113,26 +113,6 @@ jQuery(function( $ ) {
     var form_fields = {
         init: function() {
             var t = this;
-			$.datepicker._gotoToday = function(id) {
-				var target = $(id);
-				var inst = this._getInst(target[0]);
-				if (this._get(inst, 'gotoCurrent') && inst.currentDay) {
-						inst.selectedDay = inst.currentDay;
-						inst.drawMonth = inst.selectedMonth = inst.currentMonth;
-						inst.drawYear = inst.selectedYear = inst.currentYear;
-				}
-				else {
-						var date = new Date();
-						inst.selectedDay = date.getDate();
-						inst.drawMonth = inst.selectedMonth = date.getMonth();
-						inst.drawYear = inst.selectedYear = date.getFullYear();
-						// the below two lines are new
-						this._setDateDatepicker(target, date);
-						this._selectDate(id, this._getDateDatepicker(target));
-				}
-				this._notifyChange(inst);
-				this._adjustDate(target);
-			}
             $( '.wpbdp-form-field-type-date' ).each(function(i, v) {
                 t.configure_date_picker( $(v).find( 'input' ) );
             });
