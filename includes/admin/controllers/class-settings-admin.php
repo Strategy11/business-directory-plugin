762f2b3796fc1617b81b5974791c4ceb0a256a04
Steph <steph@strategy11.com>
2022-03-16 15:29:46 -0600

    update toggle to match design

--- includes/admin/controllers/class-settings-admin.php
+++ includes/admin/controllers/class-settings-admin.php
@@ -257,7 +257,7 @@ class WPBDP__Settings_Admin {
 		echo '<input type="hidden" name="wpbdp_settings[' . esc_attr( $setting['id'] ) . ']" value="0" />';
 
 		echo '<label>';
-		echo '<span class="wpbd-toggle">';
+		echo '<span class="wpbd-toggle-cont">';
 		$this->checkbox_input_html( $setting, $value );
 		echo '<span class="wpbd-toggle-slider"></span>';
 		echo '</span>';
