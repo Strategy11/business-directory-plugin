463666d9640ed6a5d3d03a87ace3740cf4249731
Paul Kevin <paultitude@gmail.com>
2022-03-21 17:59:01 +0300

    Set up a few PHPStan rules and fix afew errors

---
+++ stubs.php
@@ -0,0 +1,21 @@
+<?php
+
+define( 'OBJECT', 'OBJECT' );
+define( 'OBJECT_K', 'OBJECT_K' );
+define( 'ARRAY_A', 'ARRAY_A' );
+
+define( 'WPBDP_VERSION', '5.18' );
+
+define( 'WPBDP_PATH', '/' );
+define( 'WPBDP_INC', 'includes' );
+define( 'WPBDP_TEMPLATES_PATH', 'templates' );
+
+define( 'WPBDP_POST_TYPE', 'wpbdp_listing' );
+define( 'WPBDP_CATEGORY_TAX', 'wpbdp_category' );
+define( 'WPBDP_TAGS_TAX', 'wpbdp_tag' );
+
+//Plugin url
+define( 'WPBDP_URL', '/' );
+
+//Assets url
+define( 'WPBDP_ASSETS_URL', 'assets/' );
