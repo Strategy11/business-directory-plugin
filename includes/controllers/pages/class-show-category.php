1981af7ab6b3344095992984f7ce726998a85b88
stephywells <steph@strategy11.com>
2022-06-06 12:12:41 -0600

    Fix https://github.com/Strategy11/BusinessDirectoryPlugin/issues/5084 Fixes https://github.com/Strategy11/BusinessDirectoryPlugin/issues/5084 This updates the page title for taxonomy pages to avoid output for a listing

--- includes/controllers/pages/class-show-category.php
+++ includes/controllers/pages/class-show-category.php
@@ -17,6 +17,10 @@ class WPBDP__Views__Show_Category extends WPBDP__View {
         $html = '';
 
         if ( is_object( $term ) ) {
+
+			add_filter( 'the_title', array( &$this, 'set_tax_title' ) );
+			add_filter( 'post_thumbnail_html', array( &$this, 'remove_tax_thumbnail' ) );
+
             $term->is_tag = false;
 
             $html = $this->_render(
@@ -35,17 +39,39 @@ class WPBDP__Views__Show_Category extends WPBDP__View {
 
         wpbdp_pop_query();
 
-        // if ( is_array( $category_id ) ) {
-        // $title = '';
-        // $category = null;
-        // } else {
-        // $category = get_term( $category_id, WPBDP_CATEGORY_TAX );
-        // $title = esc_attr( $category->name );
-        //
-        // if ( $in_listings_shortcode )
-        // $title = '';
-        // }
         return $html;
     }
 
+    /**
+     * Since the category page thinks it's a normal post, override the global post.
+     * This would be better to change the category output, rather than using a "page".
+     * See WPBDP__Dispatcher.
+     *
+     * @since x.x
+	 * @return string
+     */
+    public function set_tax_title( $title ) {
+		if ( in_the_loop() ) {
+			return $title;
+		}
+
+		$term = get_queried_object();
+        return is_object( $term ) ? $term->name : '';
+    }
+
+	/**
+	 * Prevent a post thumbnail from showing on the page before the loop.
+	 *
+	 * @since x.x
+	 * @return string
+	 */
+	public function remove_tax_thumbnail( $thumbnail ) {
+		if ( in_the_loop() ) {
+			remove_filter( 'post_thumbnail_html', array( &$this, 'remove_tax_thumbnail' ) );
+
+			return $thumbnail;
+		}
+
+		return '';
+	}
 }
