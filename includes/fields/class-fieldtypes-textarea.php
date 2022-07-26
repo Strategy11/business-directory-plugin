9c43b49eecb209ebff8bbaa0270456fda1c20478
stephywells <steph@strategy11.com>
2022-07-26 14:59:14 -0600

    remove Jetpack workaround since it's no longer needed Jetpack sharing buttons used to show multiple times, but it no longer happens.

--- includes/fields/class-fieldtypes-textarea.php
+++ includes/fields/class-fieldtypes-textarea.php
@@ -325,16 +325,7 @@ class WPBDP_FieldTypes_TextArea extends WPBDP_Form_Field_Type {
 
         if ( 'content' == $field->get_association() ) {
             if ( $field->data( 'allow_filters' ) ) {
-                // Prevent Jetpack sharing from appearing twice. (#2039)
-                $jetpack_hack = has_filter( 'the_content', 'sharing_display' );
-
-                if ( $jetpack_hack )
-                    remove_filter( 'the_content', 'sharing_display', 19 );
-
                 $value = apply_filters( 'the_content', $value );
-
-                if ( $jetpack_hack )
-                    add_filter( 'the_content', 'sharing_display', 19 );
             } else {
 				$value = wpautop( $value );
 				if ( $field->data( 'allow_shortcodes' ) ) {
