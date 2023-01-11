6271ed0f8b1529e2941db2801366e13fed319bfd
Sherv Elmi <sherv.elmi@gmail.com>
2023-01-11 20:06:26 +0300

    Fix showing two admin menus for the editor role

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -515,9 +515,16 @@ if ( ! class_exists( 'WPBDP_Admin' ) ) {
 			$cpt_menu   = 'edit.php?post_type=' . WPBDP_POST_TYPE;
 			$admin_menu = $this->menu_id;
 
-			if ( isset( $submenu[ $cpt_menu ] ) && isset( $submenu[ $admin_menu ] ) ) {
+			$current_user = wp_get_current_user();
+			$roles        = $current_user instanceof WP_User ? $current_user->roles : [];
+			$role         = ! empty( $roles ) && is_array( $roles ) ? array_shift( $roles ) : '';
+
+			if ( isset( $submenu[ $cpt_menu ] ) && ( isset( $submenu[ $admin_menu ] ) || $role === 'editor' ) ) {
 				$this->change_menu_name( $submenu[ $cpt_menu ] );
-				$submenu[ $admin_menu ] = array_merge( $submenu[ $cpt_menu ], $submenu[ $admin_menu ] );
+
+				$submenu[ $admin_menu ] = isset( $submenu[ $admin_menu ] ) ?
+					array_merge( $submenu[ $cpt_menu ], $submenu[ $admin_menu ] ) :
+					$submenu[ $cpt_menu ];
 			}
 		}
 
