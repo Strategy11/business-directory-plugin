e7a9c28d051eb0f79fde53ff126095c3bfbc979c
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2023-08-31 20:59:34 +0300

    move functions from /debugging.php

--- includes/debugging.php
+++ includes/debugging.php
@@ -219,21 +219,3 @@ class WPBDP_Debugging {
 		self::add_debug_msg( $msg, sprintf( 'log-%s', $type ), debug_backtrace() );
 	}
 }
-
-function wpbdp_log( $msg, $type = 'info' ) {
-	call_user_func( array( 'WPBDP_Debugging', 'log' ), $msg, $type );
-}
-
-function wpbdp_log_deprecated() {
-	wpbdp_log( 'Deprecated function called.', 'deprecated' );
-}
-
-function wpbdp_debug() {
-	$args = func_get_args();
-	call_user_func_array( array( 'WPBDP_Debugging', 'debug' ), $args );
-}
-
-function wpbdp_debug_e() {
-	$args = func_get_args();
-	call_user_func_array( array( 'WPBDP_Debugging', 'debug_e' ), $args );
-}
