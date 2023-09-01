55172367b606839a538142d8d0360e194f36c090
mostafa-hisham <mostafa.hisham.mahmoud@gmail.com>
2023-09-01 17:51:48 +0300

    fix phpcs submit-listing-done.tpl

--- templates/submit-listing-done.tpl.php
+++ templates/submit-listing-done.tpl.php
@@ -28,7 +28,7 @@
 	<p>
 		<?php if ( 'publish' === get_post_status( $listing->get_id() ) ) : ?>
 			<a href="<?php echo esc_url( get_permalink( $listing->get_id() ) ); ?>"><?php echo esc_html_x( 'Go to your listing', 'templates', 'business-directory-plugin' ); ?></a> |
-		<?php elseif ( ! current_user_can( 'administrator' ) ) : ?>
+		<?php elseif ( ! current_user_is_admin() ) : ?>
 			<?php echo esc_html_x( 'Your listing requires admin approval. You\'ll be notified once your listing is approved.', 'templates', 'business-directory-plugin' ); ?>
 	</p>
 	<p>
