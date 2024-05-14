de9e755f4ab5241cacebb55b613963ab7e3704f2
Meshsons <meshsons@gmail.com>
2024-05-15 02:33:08 +0600

    AddtoAny Share button issue fixed

--- themes/default/templates/single_content.tpl.php
+++ themes/default/templates/single_content.tpl.php
@@ -11,11 +11,17 @@
 <?php endif; ?>
 
 <div class="listing-details cf<?php echo esc_attr( ( $images->main || $images->thumbnail ) ? '' : ' wpbdp-no-thumb' ); ?>">
-    <?php foreach ( $fields->not( 'social' ) as $field ) : ?>
+	<?php if( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) && get_option( 'addtoany_options', array() ) ['position'] !== null && get_option( 'addtoany_options', array() ) ['position'] == 'both' ||  get_option( 'addtoany_options', array() ) ['position'] == 'top') {
+		ADDTOANY_SHARE_SAVE_KIT();
+	} ?>
+	<?php foreach ( $fields->not( 'social' ) as $field ) : ?>
         <?php echo $field->html; ?>
     <?php endforeach; ?>
 
 	<?php wpbdp_x_part( 'parts/listing-socials' ); ?>
+	<?php if( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) && get_option( 'addtoany_options', array() ) ['position'] !== null && get_option( 'addtoany_options', array() ) ['position'] == 'both' ||  get_option( 'addtoany_options', array() ) ['position'] == 'bottom') {
+		ADDTOANY_SHARE_SAVE_KIT();
+	} ?>
 </div>
 
 <?php wpbdp_x_part( 'parts/listing-images' ); ?>
