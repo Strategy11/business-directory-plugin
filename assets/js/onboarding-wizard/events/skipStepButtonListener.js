e08bb58a1eb3d11fab13370d8653c88e10fd1e29
Sherv <sherv.elmi@gmail.com>
2024-10-21 20:16:51 +0300

    Add FF JS files to BD

---
+++ assets/js/onboarding-wizard/events/skipStepButtonListener.js
@@ -0,0 +1,37 @@
+/**
+ * External dependencies
+ */
+import { onClickPreventDefault } from 'core/utils';
+
+/**
+ * Internal dependencies
+ */
+import { getElements } from '../elements';
+import { navigateToNextStep } from '../utils';
+
+/**
+ * Manages event handling for the "Skip" step button.
+ *
+ * @return {void}
+ */
+function addSkipStepButtonEvents() {
+	const { skipStepButtons } = getElements();
+
+	// Attach click event listeners to each skip buttons
+	skipStepButtons.forEach( skipButton => {
+		onClickPreventDefault( skipButton, onSkipStepButtonClick );
+	});
+}
+
+/**
+ * Handles the click event on a "Skip" step button.
+ *
+ * @private
+ * @param {Event} event The click event object.
+ * @return {void}
+ */
+const onSkipStepButtonClick = () => {
+	navigateToNextStep();
+};
+
+export default addSkipStepButtonEvents;
---
+++ assets/js/onboarding-wizard/shared/constants.js
@@ -0,0 +1,13 @@
+export const { INITIAL_STEP, proIsIncluded } =  window.frmOnboardingWizardVars;
+
+export const PREFIX = 'frm-onboarding';
+export const WELCOME_STEP_ID = `${PREFIX}-welcome-step`;
+
+export const STEPS = {
+	INITIAL: INITIAL_STEP,
+	DEFAULT_EMAIL_ADDRESS: 'default-email-address',
+	INSTALL_FORMIDABLE_PRO: 'install-formidable-pro',
+	LICENSE_MANAGEMENT: 'license-management',
+	INSTALL_ADDONS: 'install-addons',
+	SUCCESS: 'success'
+};
