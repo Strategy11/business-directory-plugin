97d7d9fd19d223c54af8fdca4a038fd87ceb695b
Sherv <sherv.elmi@gmail.com>
2024-11-04 15:24:13 +0300

    Update shared

--- assets/js/onboarding-wizard/shared/constants.js
+++ assets/js/onboarding-wizard/shared/constants.js
@@ -1,13 +1,8 @@
-export const { INITIAL_STEP, proIsIncluded } =  window.frmOnboardingWizardVars;
+export const { INITIAL_STEP } = window.wpbdpOnboardingWizardVars;
 
 export const PREFIX = 'wpbdp-onboarding';
-export const WELCOME_STEP_ID = `${PREFIX}-welcome-step`;
 
 export const STEPS = {
 	INITIAL: INITIAL_STEP,
-	DEFAULT_EMAIL_ADDRESS: 'default-email-address',
-	INSTALL_FORMIDABLE_PRO: 'install-formidable-pro',
-	LICENSE_MANAGEMENT: 'license-management',
-	INSTALL_ADDONS: 'install-addons',
-	SUCCESS: 'success'
+	SUCCESS: 'success',
 };
