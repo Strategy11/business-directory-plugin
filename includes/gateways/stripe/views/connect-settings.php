0241bfb540aba70c0e359ae8625ba1be2a96618c
Mike Letellier <mike@crabcyb.org>
2024-12-12 11:04:15 -0400

    Fix up PHP formatting

--- includes/gateways/stripe/views/connect-settings.php
+++ includes/gateways/stripe/views/connect-settings.php
@@ -7,12 +7,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<span style="margin-bottom: 5px;min-width:40px;display: inline-block;">
 		<b style="color: var(--medium-grey);"><?php echo esc_html( $title ); ?></b>
 	</span>
-	<?php if ( $account_id ) { ?>
-		<?php if ( $connected ) { ?>
-			<?php WPBDP_App_Helper::icon_by_class( 'wpbdpfont wpbdp-checkmark-icon', array( 'aria-hidden' => 'true', 'style' => 'color: #1da867' ) ); ?>
-			<?php esc_html_e( 'Connected', 'business-directory-plugin' ); ?>
-			&nbsp; &nbsp;
-		<?php } else { ?>
+	<?php
+		if ( $account_id ) {
+			if ( $connected ) {
+				WPBDP_App_Helper::icon_by_class(
+					'wpbdpfont wpbdp-checkmark-icon',
+					array(
+						'aria-hidden' => 'true',
+						'style'       => 'color: #1da867',
+					)
+				);
+				echo '&nbsp;';
+				esc_html_e( 'Connected', 'business-directory-plugin' );
+				echo '&nbsp;&nbsp;';
+			} else {
+				?>
 			<strong>
 				<span class="wpbdp-nope" style="color:#B94A48">&#10008;</span>
 				<?php esc_html_e( 'Not connected!', 'business-directory-plugin' ); ?>
@@ -23,13 +32,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 				<?php esc_html_e( 'Finish Stripe Setup', 'business-directory-plugin' ); ?>
 			</a>
 			or
-		<?php } ?>
+		<?php
+			}
+		?>
 		<a id="wpbdp_disconnect_stripe" href="#" style="font-size:13px"><?php esc_html_e( 'Disconnect', 'business-directory-plugin' ); ?></a>
-	<?php } else { ?>
+	<?php
+		} else {
+	?>
 		<br/>
 		<a id="wpbdp_connect_with_oauth" class="button-primary wpbdp-button-primary">
 			<?php WPBDPStrpConnectHelper::stripe_icon(); ?> &nbsp;
 			<?php esc_html_e( 'Connect to Stripe', 'business-directory-plugin' ); ?>
 		</a>
-	<?php } ?>
+	<?php
+		}
+	?>
 </div>
