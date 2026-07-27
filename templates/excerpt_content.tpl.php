f26055e2957fdb98217a974c55c7f7efc3e4aee6
Sorin Marta <sorinmarta0@gmail.com>
2026-07-27 11:17:05 +0300

    Fix ZIP Code label in excerpt when street address is empty Group address fields in excerpt only when street Address has a value so zip-only listings show their own field label.

--- templates/excerpt_content.tpl.php
+++ templates/excerpt_content.tpl.php
@@ -5,15 +5,20 @@
 <div class="listing-details<?php echo esc_attr( $images->thumbnail ? '' : ' wpbdp-no-thumb' ); ?>">
 	<?php foreach ( $fields->not( 'social' ) as $field ) : ?>
 		<?php
-		$address = array( 'address', 'address2', 'city', 'state', 'country', 'zip' );
-		if ( in_array( $field->tag, $address ) ) :
-			if ( empty( $skip_address ) && $field->html ) :
-				$skip_address = $address;
-				?>
+		$address    = array( 'address', 'address2', 'city', 'state', 'country', 'zip' );
+		$has_street = ! empty( $fields->t_address->value );
+		if ( in_array( $field->tag, $address, true ) ) :
+			if ( $has_street ) :
+				if ( empty( $skip_address ) && $field->html ) :
+					$skip_address = $address;
+					?>
 		<div class="address-info wpbdp-field-display wpbdp-field wpbdp-field-value">
 				<?php echo wp_kses_post( $fields->_h_address_label ); ?>
 			<div><?php echo wp_kses_post( $fields->_h_address ); ?></div>
 		</div>
+				<?php endif; ?>
+			<?php elseif ( $field->html ) : ?>
+			<?php echo $field->html; // phpcs:ignore WordPress.Security.EscapeOutput ?>
 			<?php endif; ?>
 		<?php else : ?>
 			<?php echo $field->html; // phpcs:ignore WordPress.Security.EscapeOutput ?>
